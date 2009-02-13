eb9bf4068fea6ccfb89c41efc3274e8d7cfc2ac0
Peter Bastian <peter@dune-project.org>
2009-02-13 22:22:43 +0000

    Pk elements in 2d added (ie triangles only)
    Imported from SVN: r59 at trunk

---
+++ dune/pdelab/finiteelementmap/pk2dfem.hh
@@ -0,0 +1,58 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_Pk2DFEM_HH
+#define DUNE_PDELAB_Pk2DFEM_HH
+
+#include<dune/finiteelements/pk2d.hh>
+#include"finiteelementmap.hh"
+
+namespace Dune {
+  namespace PDELab {
+
+	//! wrap up element from local functions
+    //! \ingroup FiniteElementMap
+
+    template<typename GV, typename D, typename R, unsigned int k>
+    class Pk2DLocalFiniteElementMap : 
+	  public LocalFiniteElementMapInterface<LocalFiniteElementMapTraits< Dune::Pk2DLocalFiniteElement<D,R,k> >, 
+											Pk2DLocalFiniteElementMap<GV,D,R,k> >,
+      public Countable
+    {
+      typedef Dune::Pk2DLocalFiniteElement<D,R,k> FE;
+      typedef typename GV::IndexSet IndexSet;
+     
+    public:
+	  //! \brief export type of the signature
+	  typedef LocalFiniteElementMapTraits<FE> Traits;  
+
+	  //! \brief Use when Imp has a standard constructor
+	  Pk2DLocalFiniteElementMap (const GV& gv_) : is(gv_.indexSet())
+	  {
+        // create all variants 
+        for (int i=0; i<8; i++)
+          variant[i] = FE(i);
+      }
+
+	  //! \brief get local basis functions for entity
+	  template<class EntityType>
+	  const typename Traits::LocalFiniteElementType& find (const EntityType& e) const
+	  {
+        unsigned int n0,n1,n2;
+        n0 = is.template subIndex<2>(e,0);
+        n1 = is.template subIndex<2>(e,1);
+        n2 = is.template subIndex<2>(e,2);
+        unsigned int j=0;
+        if (n1>n2) j += 1;
+        if (n0>n2) j += 2;
+        if (n0>n1) j += 4;
+		return variant[j];
+	  }
+
+	private:
+      FE variant[8];
+      const IndexSet& is;
+    };
+
+  }
+}
+
+#endif
