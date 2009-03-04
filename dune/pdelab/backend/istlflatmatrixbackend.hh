ef676f899ff7ec9e16f7b495a7b06d0431a08e31
Peter Bastian <peter@dune-project.org>
2009-03-04 10:15:12 +0000

    added two directories
    Imported from SVN: r78 at trunk

---
+++ dune/pdelab/backend/istlflatmatrixbackend.hh
@@ -0,0 +1,193 @@
+#ifndef DUNE_ISTLFLATMATRIXBACKEND_HH
+#define DUNE_ISTLFLATMATRIXBACKEND_HH
+
+#include<utility>
+#include<vector>
+#include<set>
+
+#include<dune/common/fmatrix.hh>
+#include<dune/istl/bvector.hh>
+#include<dune/istl/bcrsmatrix.hh>
+
+#include"../operatorspace.hh"
+
+// ISTL backend for FlatLinearOperatorSpace
+template<int ROWBLOCKSIZE, int COLBLOCKSIZE>
+class ISTLFlatBCRSMatrixBackend
+{
+public:
+  //! The size type
+  typedef typename Dune::BCRSMatrix< Dune::FieldMatrix<float,1,1> >::size_type size_type;
+
+  // container construction
+  template<typename T, typename E>
+  class Matrix : public Dune::BCRSMatrix< Dune::FieldMatrix<E,ROWBLOCKSIZE,COLBLOCKSIZE> >
+  {
+    typedef Dune::FieldMatrix<E,ROWBLOCKSIZE,COLBLOCKSIZE> M;
+	typedef Dune::BCRSMatrix<M> BaseT;
+    typedef typename Dune::BCRSMatrix<M>::size_type size_type;
+
+  public:
+	typedef E ElementType;
+
+    friend class ISTLFlatBCRSMatrixBackend;
+
+	Matrix (const T& t) 
+      : BaseT(t.rfs().globaldimension()/ROWBLOCKSIZE,t.dfs().globaldimension()/COLBLOCKSIZE,
+              Dune::BCRSMatrix<M>::random) 
+    {
+      Pattern pattern(t.rfs().globaldimension(),t.dfs().globaldimension());
+      t.fill_pattern(pattern);
+
+      // first dummy code: build full matrix
+      for (int i=0; i<t.rfs().globaldimension(); ++i)
+        this->setrowsize(i,pattern[i].size());
+      this->endrowsizes();
+
+      for (int i=0; i<t.rfs().globaldimension(); ++i)
+        {
+          for (typename std::set<size_type>::iterator it=pattern[i].begin(); 
+               it!=pattern[i].end(); ++it)
+            this->addindex(i,*it);
+        }
+      this->endindices();
+
+      // code for full matrix
+//       for (int i=0; i<t.rfs().globaldimension(); ++i)
+//         this->setrowsize(i,t.dfs().globaldimension());
+//       this->endrowsizes();
+
+//       for (int i=0; i<t.rfs().globaldimension(); ++i)
+//         for (int j=0; j<t.dfs().globaldimension(); ++j)
+//           this->addindex(i,j);
+//       this->endindices();
+
+    }
+
+    Matrix& operator= (const E& x)
+    {
+      BaseT::operator=(x);
+      return *this;
+    }
+
+    // for debugging
+    BaseT& base ()
+    {
+      return *this;
+    }
+
+    const BaseT& base () const
+    {
+      return *this;
+    }
+  };
+
+  // extract type of container element 
+  template<class C>
+  struct Value
+  {
+	typedef typename C::field_type Type;
+  };
+
+  // type to store sparsity pattern of block indices
+  class Pattern : public std::vector< std::set<size_type> >
+  {
+    typedef std::vector< std::set<size_type> > BaseT;
+  public:
+    Pattern (size_type m_, size_type n_)
+    {
+      this->resize(m_);
+    }
+
+    void add_link (size_type i, size_type j)
+    {
+      (*this)[i/ROWBLOCKSIZE].insert(j/COLBLOCKSIZE);
+    }
+  };
+
+  //==========================
+  // individual element access
+  //==========================
+
+
+  // get const_reference to container element
+  // note: this method does not depend on T!
+  template<typename C>
+  static const typename C::field_type& const_access (const C& c, size_type i, size_type j)
+  {
+	return c[i/ROWBLOCKSIZE][j/COLBLOCKSIZE][i%ROWBLOCKSIZE][j%COLBLOCKSIZE];
+  }
+
+  // get non const_reference to container element 
+  // note: this method does not depend on T!
+  template<typename C>
+  static typename C::field_type& access (C& c, size_type i, size_type j)
+  {
+	return c[i/ROWBLOCKSIZE][j/COLBLOCKSIZE][i%ROWBLOCKSIZE][j%COLBLOCKSIZE];
+  }
+
+
+  //=================
+  // submatrix access
+  //=================
+
+
+  // read a submatrix given by global indices
+  // we can assume C to be std::vector
+  template<typename C, typename RI, typename CI, typename T>
+  static void read (const C& c, 
+                    const std::vector<RI>& row_index, const std::vector<CI>& col_index, 
+                    LocalMatrix<T>& submatrix)
+  {
+    submatrix.resize(row_index.size(),col_index.size());
+    for (int j=0; j<col_index.size(); j++)
+      for (int i=0; i<row_index.size(); i++)
+        {
+          int I = row_index[i];
+          int J = col_index[j];
+          submatrix(i,j) = c[I/ROWBLOCKSIZE][J/COLBLOCKSIZE][I%ROWBLOCKSIZE][J%COLBLOCKSIZE];
+        }
+  }
+
+  // write a submatrix given by global indices
+  // we can assume C to be std::vector
+  template<typename C, typename RI, typename CI, typename T>
+  static void write (const std::vector<RI>& row_index, const std::vector<CI>& col_index, 
+                     const LocalMatrix<T>& submatrix, C& c)
+  {
+    for (int j=0; j<col_index.size(); j++)
+      for (int i=0; i<row_index.size(); i++)
+        {
+          int I = row_index[i];
+          int J = col_index[j];
+          c[I/ROWBLOCKSIZE][J/COLBLOCKSIZE][I%ROWBLOCKSIZE][J%COLBLOCKSIZE] = submatrix(i,j);
+        }
+  }
+
+  // write a submatrix given by global indices
+  // we can assume C to be std::vector
+  template<typename C, typename RI, typename CI, typename T>
+  static void add (const std::vector<RI>& row_index, const std::vector<CI>& col_index, 
+                   const LocalMatrix<T>& submatrix, C& c)
+  {
+    for (int j=0; j<col_index.size(); j++)
+      for (int i=0; i<row_index.size(); i++)
+        {
+          int I = row_index[i];
+          int J = col_index[j];
+          c[I/ROWBLOCKSIZE][J/COLBLOCKSIZE][I%ROWBLOCKSIZE][J%COLBLOCKSIZE] += submatrix(i,j);
+        }
+  }
+
+  // clear one row of the matrix
+  template<typename C, typename RI>
+  static void clear_row (RI i, C& c)
+  {
+    typedef typename C::ColIterator coliterator;
+    for (coliterator j=c[i].begin(); j!=c[i].end(); ++j)
+      for (int jj=0; jj<COLBLOCKSIZE; jj++)
+        (*j)[i%ROWBLOCKSIZE][jj] = 0;
+  }
+};
+
+#endif
---
+++ dune/pdelab/backend/istlflatvectorbackend.hh
@@ -0,0 +1,60 @@
+#ifndef DUNE_ISTLFLATVECTORBACKEND_HH
+#define DUNE_ISTLFLATVECTORBACKEND_HH
+
+#include<dune/common/fmatrix.hh>
+#include<dune/istl/bvector.hh>
+
+// ISTL backend for FlatFunctionSpace
+template<int BLOCKSIZE=1>
+class ISTLFlatVectorBackend
+{
+public:
+  // container construction
+  template<typename T, typename E>
+  class VectorContainer : public Dune::BlockVector< Dune::FieldVector<E,BLOCKSIZE> >
+  {
+	typedef Dune::BlockVector< Dune::FieldVector<E,BLOCKSIZE> > BaseT;
+  public:
+	typedef E ElementType;
+
+	VectorContainer (const T& t) : BaseT(t.globaldimension()/BLOCKSIZE) 
+	{}
+	VectorContainer (const T& t, const E& e) : BaseT(t.globaldimension()/BLOCKSIZE) 
+	{
+	  BaseT::operator=(e);
+	}
+	VectorContainer& operator= (const E& e)
+	{
+	  BaseT::operator=(e);
+	  return *this;
+	}
+  };
+
+  // extract type of container element 
+  template<class C>
+  struct Value
+  {
+	typedef typename C::block_type::block_type Type;
+  };
+
+  //! The size type
+  typedef typename Dune::BlockVector< Dune::FieldVector<float,BLOCKSIZE> >::size_type size_type;
+
+  // get const_reference to container element
+  // note: this method does not depend on T!
+  template<typename C>
+  static const typename C::field_type& const_access (const C& c, size_type i)
+  {
+	return c[i/BLOCKSIZE][i%BLOCKSIZE];
+  }
+
+  // get non const_reference to container element 
+  // note: this method does not depend on T!
+  template<typename C>
+  static typename C::field_type& access (C& c, size_type i)
+  {
+	return c[i/BLOCKSIZE][i%BLOCKSIZE];
+  }
+};
+
+#endif
