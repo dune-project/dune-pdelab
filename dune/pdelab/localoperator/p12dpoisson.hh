57d97d12b2a76392277c79ba293f7b384f1c2b13
Peter Bastian <peter@dune-project.org>
2009-03-04 12:37:47 +0000

    first version
    Imported from SVN: r84 at trunk

---
+++ dune/pdelab/localoperator/p12dpoisson.hh
@@ -0,0 +1,17 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_P12DPOISSON_HH
+#define DUNE_PDELAB_P12DPOISSON_HH
+
+#include<dune/common/exceptions.hh>
+
+#include"../common/geometrywrapper.hh"
+
+
+namespace Dune {
+  namespace PDELab {
+
+
+
+    //! \} group GridFunctionSpace
+  } // namespace PDELab
+} // namespace Dune
