71893052b20be8ce2c615bbbf81f221a7a3fd011
Jö Fahlke <joe@dune-project.org>
2009-04-07 18:10:58 +0000

    Monom shape functions
    Imported from SVN: r113 at trunk

---
+++ dune/pdelab/finiteelementmap/monomfem.hh
@@ -0,0 +1,29 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_MONOMFEM_HH
+#define DUNE_PDELAB_MONOMFEM_HH
+
+#include <dune/common/geometrytype.hh>
+
+#include<dune/finiteelements/monom.hh>
+#include"finiteelementmap.hh"
+
+namespace Dune {
+  namespace PDELab {
+
+	//! wrap up element from local functions
+    //! \ingroup FiniteElementMap
+	template<class D, class R, int d, int p>
+	class MonomLocalFiniteElementMap
+	  : public SimpleLocalFiniteElementMap< Dune::MonomLocalFiniteElement<D,R,d,p> >
+	{
+    public:
+      MonomLocalFiniteElementMap (Dune::GeometryType::BasicType basicType)
+        : SimpleLocalFiniteElementMap< Dune::MonomLocalFiniteElement<D,R,d,p> >(Dune::MonomLocalFiniteElement<D,R,d,p>(basicType))
+      {
+      }
+    };
+
+  }
+}
+
+#endif //DUNE_PDELAB_MONOMFEM_HH
