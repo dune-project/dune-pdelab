a622379e93d54c9f996f134c856f8cb490457f26
Jö Fahlke <joe@dune-project.org>
2009-07-31 11:48:46 +0000

    Finite element map for Rannacher-Turek elements
    Imported from SVN: r218 at trunk

---
+++ dune/pdelab/finiteelementmap/rannacher_turek2dfem.hh
@@ -0,0 +1,20 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_RANNACHER_TUREK2DFEM_HH
+#define DUNE_PDELAB_RANNACHER_TUREK2DFEM_HH
+
+#include<dune/finiteelements/rannacher_turek2d.hh>
+#include"finiteelementmap.hh"
+
+namespace Dune {
+  namespace PDELab {
+
+    //! wrap up element from local functions
+    //! \ingroup FiniteElementMap
+    template<class D, class R>
+    class RannacherTurek2DLocalFiniteElementMap
+      : public SimpleLocalFiniteElementMap<RannacherTurek2DLocalFiniteElement<D,R> >
+    {};
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_RANNACHER_TUREK2DFEM_HH
