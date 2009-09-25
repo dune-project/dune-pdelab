8132fb415d1bc2a47a72413242242e648ac013c6
Jö Fahlke <joe@dune-project.org>
2009-09-25 18:13:06 +0000

    implement l2difference in terms of the l2norm using the function adapters
    Imported from SVN: r274 at trunk

--- dune/pdelab/test/l2difference.hh
+++ dune/pdelab/test/l2difference.hh
@@ -7,43 +7,21 @@
 
 #include <dune/grid/common/quadraturerules.hh>
 
+#include "l2norm.hh"
 
 // Calculate the squared L2 differerence of two functions
 template<typename GV, typename U, typename V> 
 double l2difference2 (const GV & gv, const U& u, const V &v, int qorder=1)
 {
-  // constants and types
-  const int dim = GV::Grid::dimension;
-  typedef typename GV::Traits::template Codim<0>::Iterator ElementIterator;
-  typedef typename GV::Grid::ctype ct;
-  
-  // loop over grid view
-  double sum = 0.0;
-  for (ElementIterator eit = gv.template begin<0>();
-       eit!=gv.template end<0>(); ++eit)
-  {
-
-    Dune::GeometryType gt = eit->geometry().type();
-    const Dune::QuadratureRule<ct,dim>& 
-      rule = Dune::QuadratureRules<ct,dim>::rule(gt,qorder);
-
-    for (typename Dune::QuadratureRule<ct,dim>::const_iterator qit=rule.begin();
-         qit!=rule.end(); ++qit)
-    {
-      // evaluate the given grid functions at integration point
-      typename U::Traits::RangeType u_val;
-      u.evaluate(*eit,qit->position(),u_val);
-
-      typename V::Traits::RangeType v_val;
-      v.evaluate(*eit,qit->position(),v_val);
-
-      // accumulate error
-      v_val -= u_val;
-      sum += v_val.two_norm2()*qit->weight()*
-        eit->geometry().integrationElement(qit->position());
-    }
-  }
-  return sum;
+  return l2norm2
+    (Dune::PDELab::makePointwiseGridFunctionAdapter
+     (Dune::PDELab::PointwiseSumAdapterEngine(),
+      u,
+      Dune::PDELab::makePointwiseGridFunctionAdapter
+      (Dune::PDELab::makePointwiseScaleAdapterEngine
+      ((typename V::Traits::RangeFieldType)(-1)),
+       v)),
+     qorder);
 }
 
 // Calculate the L2 differerence of two functions
