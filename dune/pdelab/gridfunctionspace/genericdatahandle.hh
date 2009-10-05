81c6259bca1d97cbb2fac6721b714bfc00a214ac
Jö Fahlke <joe@dune-project.org>
2009-10-05 14:04:17 +0000

    make headercheck clean
    Imported from SVN: r294 at trunk

--- dune/pdelab/gridfunctionspace/genericdatahandle.hh
+++ dune/pdelab/gridfunctionspace/genericdatahandle.hh
@@ -1,9 +1,13 @@
-// -*- tab-width: 4; indent-tabs-mode: nil -*-
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_PDELAB_GENERICDATAHANDLE_HH
 #define DUNE_PDELAB_GENERICDATAHANDLE_HH
 
+#include <vector>
+
 #include<dune/common/exceptions.hh>
 #include<grid/common/datahandleif.hh>
+#include <dune/grid/common/gridenums.hh>
 
 namespace Dune {
   namespace PDELab {
