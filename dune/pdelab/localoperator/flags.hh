dc97eead8b1ad8698c31a8af4112742c17d33798
Jö Fahlke <joe@dune-project.org>
2009-10-21 15:11:45 +0000

    document flags
    Imported from SVN: r318 at trunk

--- dune/pdelab/localoperator/flags.hh
+++ dune/pdelab/localoperator/flags.hh
@@ -1,3 +1,5 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=8 sw=4 sts=4:
 #ifndef DUNE_PDELAB_LOCALOPERATOR_FLAGS_HH
 #define DUNE_PDELAB_LOCALOPERATOR_FLAGS_HH
 
@@ -5,24 +7,42 @@ namespace Dune
 {
     namespace PDELab
     {
+        //! \addtogroup LocalOperator
+        //! \ingroup PDELab
+        //! \{
+
+        //! Default flags for all local operators
         class LocalOperatorDefaultFlags
         {
         public:
             // pattern assembly flags
-            enum { doPatternVolume = false };
-            enum { doPatternSkeleton = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doPatternVolume = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doPatternSkeleton = false };
 
             // residual assembly flags
-            enum { doAlphaVolume = false };
-            enum { doAlphaVolumePostSkeleton = false };
-            enum { doAlphaSkeleton = false };
-            enum { doAlphaBoundary = false };
-            enum { doLambdaVolume = false };
-            enum { doLambdaVolumePostSkeleton = false };
-            enum { doLambdaSkeleton = false };
-            enum { doLambdaBoundary = false };
-            enum { doSkeletonTwoSided = false };
+            //! \brief Whether not to call the local operators alpha_volume()
+            //!        and jacobian_volume()
+            enum { /*! \hideinitializer */ doAlphaVolume = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doAlphaVolumePostSkeleton = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doAlphaSkeleton = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doAlphaBoundary = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doLambdaVolume = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doLambdaVolumePostSkeleton = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doLambdaSkeleton = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doLambdaBoundary = false };
+            //! \todo
+            enum { /*! \hideinitializer */ doSkeletonTwoSided = false };
         };
+        //! \} group LocalOperator
     }
 }
 
