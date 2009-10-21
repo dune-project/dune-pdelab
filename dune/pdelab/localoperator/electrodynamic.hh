44208f043e3adeff612eaf2850c3baab959badae
Jö Fahlke <joe@dune-project.org>
2009-10-21 15:12:12 +0000

    split out a localoperator for T
    Imported from SVN: r319 at trunk

--- dune/pdelab/localoperator/electrodynamic.hh
+++ dune/pdelab/localoperator/electrodynamic.hh
@@ -24,6 +24,98 @@ namespace Dune {
     //! \ingroup PDELab
     //! \{
 
+
+    //! Contruct matrix T for the Electrodynamic operator
+    /**
+     * Construct the matrix
+     * \f[
+     *    T_{ij}&=\int_\Omega\epsilon\mathbf N_i\cdot\mathbf N_jdV
+     * \f]
+     * which appears inside the Electrodynamic operator.
+     *
+     * \tparam Eps    Type of function to evaluate \f$\epsilon\f$
+     */
+    template<typename Eps>
+    class Electrodynamic_T
+      : public FullVolumePattern
+      , public LocalOperatorDefaultFlags
+    {
+    public:
+
+      // pattern assembly flags
+      enum { doPatternVolume = true };
+
+      //! Construct an Electrodynamic localoperator
+      /**
+       * \param eps_    Reference to function object to evaluate
+       * \param qorder_ Quadrature order to use.
+       *
+       * \note The references the the function objects should be valid for as
+       *       long as this localoperators residual() method is used.
+       */
+      Electrodynamic_T(const Eps &eps_, int qorder_ = 2)
+        : eps(eps_)
+        , qorder(qorder_)
+      {}
+
+      /**
+       * \note We support only Galerkin method lfsu==lfsv
+       */
+      template<typename EG, typename LFS, typename X, typename R>
+      void jacobian_volume (const EG& eg, const LFS& lfsu, const X& x,
+                            const LFS& lfsv, LocalMatrix<R>& mat) const
+      {
+        // domain and range field type
+        typedef typename LFS::Traits::LocalFiniteElementType::
+          Traits::LocalBasisType::Traits LBTraits;
+
+        typedef typename LBTraits::DomainFieldType DF;
+        typedef typename LBTraits::DomainType Domain;
+        static const unsigned dimD = LBTraits::dimDomain;
+
+        typedef typename LBTraits::RangeFieldType RF;
+        typedef typename LBTraits::RangeType Range;
+        static const unsigned dimR = LBTraits::dimRange;
+
+        // static checks
+        dune_static_assert(dimR == 3 || dimR == 2,
+                           "Works only in 2D or 3D");
+        dune_static_assert
+          ((Dune::is_same<typename EG::Geometry::ctype, DF>::value),
+           "Grids ctype and Finite Elements DomainFieldType must match");
+
+        // select quadrature rule
+        typedef Dune::QuadratureRule<DF,dimD> QR;
+        typedef Dune::QuadratureRules<DF,dimD> QRs;
+        Dune::GeometryType gt = eg.geometry().type();
+        const QR& rule = QRs::rule(gt,qorder);
+
+        // loop over quadrature points
+        for(typename QR::const_iterator it=rule.begin();
+            it!=rule.end(); ++it) {
+          // values of basefunctions
+          std::vector<Range> phi(lfsu.size());
+          lfsu.localFiniteElement().localBasis()
+            .evaluateFunctionGlobal(it->position(),phi,eg.geometry());
+
+          // calculate T
+          typename Eps::Traits::RangeType epsval;
+          eps.evaluate(eg.entity(), it->position(), epsval);
+
+          RF factor = it->weight()
+            * eg.geometry().integrationElement(it->position()) * epsval;
+
+          for(unsigned i = 0; i < lfsu.size(); ++i)
+            for(unsigned j = 0; j < lfsu.size(); ++j)
+              mat(i,j) += factor * (phi[i] * phi[j]);
+        }
+      }
+
+    private:
+      const Eps &eps;
+      const int qorder;
+    };
+
     //! A local operator for a simple electrodynamic wave equation
     /**
      * Solve the equation
@@ -113,9 +205,11 @@ namespace Dune {
 	public:
 
       // pattern assembly flags
+      //! \copydoc LocalOperatorDefaultFlags::doPatternVolume
       enum { doPatternVolume = true };
 
 	  // residual assembly flags
+      //! \copydoc LocalOperatorDefaultFlags::doAlphaVolume
       enum { doAlphaVolume = true };
 
       //! Construct an Electrodynamic localoperator
