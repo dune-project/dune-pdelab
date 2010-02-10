f1176de8636c49be89faf5fe1f49ecac85eda7e0
Jö Fahlke <joe@dune-project.org>
2010-02-10 12:30:30 +0000

    extend editor hints
    Imported from SVN: r385 at trunk

--- dune/pdelab/common/countingptr.hh
+++ dune/pdelab/common/countingptr.hh
@@ -1,4 +1,6 @@
-// -*- tab-width: 4; indent-tabs-mode: nil -*-
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
 #ifndef DUNE_PDELAB_COUNTINGPTR_HH
 #define DUNE_PDELAB_COUNTINGPTR_HH
 
