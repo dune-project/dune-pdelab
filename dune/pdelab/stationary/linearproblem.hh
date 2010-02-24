09afa3fa410c265b0ad7fb3fcbc9b60a990df240
Jö Fahlke <joe@dune-project.org>
2010-02-24 11:42:45 +0000

    make it possible to use the StationaryLinearProblemSolver for instationary linear problems as well.
    Imported from SVN: r430 at trunk

--- dune/pdelab/stationary/linearproblem.hh
+++ dune/pdelab/stationary/linearproblem.hh
@@ -21,33 +21,42 @@ namespace Dune {
     public:
 
       StationaryLinearProblemSolver (const GOS& gos_, V& x_, LS& ls_, typename V::ElementType reduction_)
-	: gos(gos_), ls(ls_), x(x_), reduction(reduction_)
+	: gos(gos_), ls(ls_), x(&x_), reduction(reduction_)
       {
       }
 
+      StationaryLinearProblemSolver (const GOS& gos_, LS& ls_, typename V::ElementType reduction_)
+	: gos(gos_), ls(ls_), x(0), reduction(reduction_)
+      {
+      }
+
+      void apply (V& x_) {
+        x = &x_;
+        apply();
+      }
       void apply ()
       {
 	// assemble matrix; optional: assemble only on demand!
 	M m(gos); 
 	m = 0.0;
-	gos.jacobian(x,m);
+	gos.jacobian(*x,m);
 
 	// assemble residual
 	W r(gos.testGridFunctionSpace(),0.0);
-	gos.residual(x,r);  // residual is additive
+        gos.residual(*x,r);  // residual is additive
 
 	// compute correction
 	V z(gos.trialGridFunctionSpace(),0.0);
 	ls.apply(m,z,r,reduction); // solver makes right hand side consistent
 
 	// and update
-	x -= z;
+        *x -= z;
       }
 
     private:
       const GOS& gos;
       LS& ls;
-      V& x;
+      V* x;
       typename V::ElementType reduction;
     };
 
