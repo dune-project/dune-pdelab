7c4e2562b0a086f1df651ee9e05b1e9ed4e65c07
Jö Fahlke <joe@dune-project.org>
2010-07-10 14:23:18 +0000

    Update editor hints.
    Imported from SVN: r545 at trunk

--- dune/pdelab/gridoperatorspace/gridoperatorspace.hh
+++ dune/pdelab/gridoperatorspace/gridoperatorspace.hh
@@ -1,4 +1,5 @@
-// -*- tab-width: 4; indent-tabs-mode: nil -*-
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_PDELAB_GRIDOPERATORSPACE_HH
 #define DUNE_PDELAB_GRIDOPERATORSPACE_HH
 
