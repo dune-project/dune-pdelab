6505f53a303703028456f27700fe07b8059da6a6
Jö Fahlke <joe@dune-project.org>
2010-10-04 11:19:21 +0000

    Global finite element map specifically for p1.
    Imported from SVN: r670 at trunk

---
+++ dune/pdelab/finiteelementmap/globalp1.hh
@@ -0,0 +1,45 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
+
+#ifndef DUNE_PDELAB_FINITEELEMENTMAP_GLOBALP1_HH
+#define DUNE_PDELAB_FINITEELEMENTMAP_GLOBALP1_HH
+
+#include <dune/localfunctions/lagrange/p1.hh>
+
+#include <dune/pdelab/finiteelement/simplewrapper.hh>
+#include <dune/pdelab/finiteelementmap/global.hh>
+
+namespace Dune {
+  namespace PDELab {
+
+    template<class Geometry, class RangeField>
+    class P1FiniteElementMap :
+      public GeometryFiniteElementMap
+        <SimpleLocalFiniteElementWrapperFactory<
+           P1LocalFiniteElement<typename Geometry::ctype,
+                                RangeField,
+                                Geometry::mydimension>,
+           Geometry> >
+    {
+      typedef P1LocalFiniteElement<typename Geometry::ctype,
+                                   RangeField,
+                                   Geometry::mydimension> LocalFE;
+      typedef SimpleLocalFiniteElementWrapperFactory<LocalFE, Geometry>
+        Factory;
+      typedef GeometryFiniteElementMap<Factory> Base;
+
+      static Factory& getFactory() {
+        static const LocalFE localFE;
+        static Factory factory(localFE);
+        return factory;
+      }
+
+    public:
+      P1FiniteElementMap() : Base(getFactory()) { }
+
+    };
+
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_FINITEELEMENTMAP_GLOBALP1_HH
