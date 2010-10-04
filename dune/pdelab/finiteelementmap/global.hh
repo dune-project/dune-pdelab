ac0e985549caa18b68cabd2780f07f27c6dce71b
Jö Fahlke <joe@dune-project.org>
2010-10-04 21:16:31 +0000

    Document GeometryFiniteElementMap.
    Imported from SVN: r687 at trunk

--- dune/pdelab/finiteelementmap/global.hh
+++ dune/pdelab/finiteelementmap/global.hh
@@ -10,6 +10,12 @@
 namespace Dune {
   namespace PDELab {
 
+    //! \brief Generic finite element map for global finite elements created
+    //!        with a geometry
+    /**
+     * \tparam Factory Finite element factory class that supports make(const
+     *                 Geometry&).
+     */
     template<class Factory>
     class GeometryFiniteElementMap :
       public Countable
@@ -19,9 +25,26 @@ namespace Dune {
     public:
       typedef FiniteElementMapTraits<typename Factory::FiniteElement> Traits;
 
+      //! construct GeometryFiniteElementMap
+      /**
+       * \param factory_ Reference to a factory object.
+       *
+       * \note The constructed finite element map object stores a reference to
+       *       to factory object.  The finite element map object becomes
+       *       invalid as soon the reference to the factory becomes invalid.
+       *       The only allowed operation on an invalid finite element map is
+       *       calling the destructor, all other operations result in
+       *       undefined behaviour.
+       */
       GeometryFiniteElementMap(Factory& factory_) : factory(factory_) {}
 
       //! Return finite element for the given entity.
+      /**
+       * \param e Grid element to create a finite element for.
+       *
+       * The returned value is valid for as long as both this finite element
+       * map as well as the reference to the grid element are valid.
+       */
       template<class Element>
       typename Traits::FiniteElementType find(const Element& e) const {
         return factory.make(e.geometry());
