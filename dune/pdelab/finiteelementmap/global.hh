9237575b47197d8123215f8a1e6eed2af3e5f4d9
Jö Fahlke <joe@dune-project.org>
2010-10-05 23:08:59 +0000

    Global finite element map for finite elements whose factory takes geometry + vertex order.
    Imported from SVN: r693 at trunk

--- dune/pdelab/finiteelementmap/global.hh
+++ dune/pdelab/finiteelementmap/global.hh
@@ -51,6 +51,54 @@ namespace Dune {
       }
     };
 
+    //! \brief Generic finite element map for global finite elements created
+    //!        with a geometry and a vertex ordering
+    /**
+     * \tparam FEFactory Finite element factory class that supports make(const
+     *                   Geometry&, const VertexOrdering&).
+     * \tparam VOFactory Factory to extract the vertex ordering for a given
+     *                   grid element.
+     */
+    template<class FEFactory, class VOFactory>
+    class GeometryVertexOrderFiniteElementMap :
+      public Countable
+    {
+      FEFactory& feFactory;
+      const VOFactory& voFactory;
+
+    public:
+      typedef FiniteElementMapTraits<typename FEFactory::FiniteElement> Traits;
+
+      //! construct GeometryFiniteElementMap
+      /**
+       * \param feFactory_ Reference to a finite element factory object.
+       * \param voFactory_ Reference to a vertex order factory object.
+       *
+       * \note The constructed finite element map object stores a reference to
+       *       to factory objects.  The finite element map object becomes
+       *       invalid as soon one of the references to the factories becomes
+       *       invalid.  The only allowed operation on an invalid finite
+       *       element map is calling the destructor, all other operations
+       *       result in undefined behaviour.
+       */
+      GeometryVertexOrderFiniteElementMap(FEFactory& feFactory_,
+                                          const VOFactory & voFactory_) :
+        feFactory(feFactory_), voFactory(voFactory_)
+      {}
+
+      //! Return finite element for the given entity.
+      /**
+       * \param e Grid element to create a finite element for.
+       *
+       * The returned value is valid for as long as both this finite element
+       * map as well as the reference to the grid element are valid.
+       */
+      template<class Element>
+      typename Traits::FiniteElementType find(const Element& e) const {
+        return feFactory.make(e.geometry(), voFactory.make(e));
+      }
+    };
+
   } // namespace PDELab
 } // namespace Dune
 
