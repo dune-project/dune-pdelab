b49430249f1cfa2dce676bf8c6c6a76addb16987
Jö Fahlke <joe@dune-project.org>
2010-10-08 14:43:09 +0000

    Properly document the memberfunctionadaptor.
    Imported from SVN: r709 at trunk

--- dune/pdelab/function/memberadaptor.hh
+++ dune/pdelab/function/memberadaptor.hh
@@ -13,11 +13,19 @@
 namespace Dune {
   namespace PDELab {
 
-    //! GridFunction returning a constant value everywhere
+    //! GridFunction implemented by a member function of some class
     /**
+     * \tparam Member   Member function pointer type.
+     * \tparam Class    Type of the class containing the member.
      * \tparam GV       The type of the GridView
      * \tparam RF       The type of the range field
-     * \tparam dimRange The dimension of the Range
+     * \tparam dimRange The dimension of the Range.
+     *
+     * The member function must support the signature
+     * \code
+(obj.*member)(const FieldVector<typename GV::ctype, GV::dimension> &xl,
+              FieldVector<RF, dimRange> &y) const;
+     * \endcode
      */
     template<class Member, class Class,
              class GV, class RF, std::size_t dimR = 1>
@@ -28,6 +36,7 @@ namespace Dune {
       >
     {
     public:
+      //! export traits class
       typedef GridFunctionTraits< GV, RF, dimR, FieldVector<RF, dimR> > Traits;
 
     private:
@@ -41,13 +50,14 @@ namespace Dune {
       const GV &gv;
 
     public:
-      //! Construct a Const GridFunction
+      //! Construct an adaptor object
       /**
-       * \param gv   The GridView to use.  It is passed as a reference to
-       *             AnalyticGridFunctionBase (look there for the requirements
-       *             of this argument).
-       * \param val_ The value tu return on evaluation.  This class stores a
-       *             copy of that value.
+       * \param obj_    Class object to call the member function on.
+       * \param member_ Member function pointer to the member to use.
+       * \param gv_     The GridView to use.
+       *
+       * This class store the \c obj_ and \c gv_ references internally and
+       * becomes invalid when they become invalid.
        */
       MemberFunctionToGridFunctionAdaptor(const Class &obj_, Member member_,
                                           const GV& gv_) :
@@ -61,9 +71,21 @@ namespace Dune {
         (obj.*member)(e, x, y);
       }
 
+      //! get reference to the internal gridview.
       const GV& getGridView() const { return gv; }
     };
 
+    //! easy construction of a MemberFunctionToGridFunctionAdaptor
+    /**
+     * \relates MemberFunctionToGridFunctionAdaptor
+     *
+     * \code
+#include <dune/pdelab/function/memberadaptor.hh>
+     * \endcode
+     *
+     * The only required template parameters are \c RF and \c dimRange, all
+     * other template parameters can be deduced automatically.
+     */
     template<class RF, std::size_t dimRange,
              class GV, class Class, class Member>
     MemberFunctionToGridFunctionAdaptor<Member, Class, GV, RF, dimRange>
