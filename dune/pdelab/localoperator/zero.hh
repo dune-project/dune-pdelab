a38682939972063a1bab1007fd0ae6e0fa1f3759
Jö Fahlke <joe@dune-project.org>
2010-10-20 12:00:44 +0000

    [ZeroLocalOperator] Do-nothing LOP, useful as a placeholder.
    Imported from SVN: r742 at trunk

---
+++ dune/pdelab/localoperator/zero.hh
@@ -0,0 +1,34 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
+
+#ifndef DUNE_PDELAB_LOCALOPERATOR_ZERO_HH
+#define DUNE_PDELAB_LOCALOPERATOR_ZERO_HH
+
+#include <dune/pdelab/localoperator/flags.hh>
+#include <dune/pdelab/localoperator/idefault.hh>
+
+namespace Dune {
+  namespace PDELab {
+    //! \addtogroup LocalOperator
+    //! \ingroup PDELab
+    //! \{
+
+    //! Do-nothing local operator
+    /**
+     * Thi local operator does nothing nothing.  It can be used as a
+     * placeholder where an (instationary) local operator is semantically
+     * required but not actually used.
+     *
+     * \tparam Time Type used for temporal values.
+     */
+    template<class Time>
+    class ZeroLocalOperator :
+      public LocalOperatorDefaultFlags,
+      public InstationaryLocalOperatorDefaultMethods<Time>
+    { };
+
+    //! \} group LocalOperator
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_LOCALOPERATOR_ZERO_HH
