8fb9ae21864578998a0cf3735c3369183c64e1f8
Felix Heimann <felix@dune-project.org>
2010-11-22 12:38:45 +0000

    Add a switch which checks whether the new grid operator space should be used. Default is to use the old one.
    Imported from SVN: r837 at trunk

---
+++ dune/pdelab/gridoperatorspace/gridoperatorspace.hh
@@ -0,0 +1,5 @@
+#ifndef USE_NEW_GRIDOPERATORSPACE
+#include <dune/pdelab/gridoperatorspace/old_gridoperatorspace.hh>
+#else
+#include <dune/pdelab/gridoperatorspace/new_gridoperatorspace.hh>
+#endif
