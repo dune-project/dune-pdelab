d9fc2ff05b05a95343fc9a455a8e58cd048dd138
Christian Engwer <christi@dune-project.org>
2010-11-26 21:47:59 +0000

    add P-adaptive DG-Monom FiniteElementMaps
    Imported from SVN: r848 at trunk

---
+++ dune/pdelab/finiteelementmap/variablemonomfem.hh
@@ -0,0 +1,92 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_VARIABLEMONOMFEM_HH
+#define DUNE_PDELAB_VARIABLEMONOMFEM_HH
+
+#include <dune/common/geometrytype.hh>
+
+#include <dune/localfunctions/common/virtualwrappers.hh>
+#include <dune/localfunctions/monom.hh>
+#include <dune/common/array.hh>
+#include <dune/common/shared_ptr.hh>
+#include "finiteelementmap.hh"
+
+namespace Dune {
+  namespace PDELab {
+
+    namespace {
+      template<class D, class R, int d, int p>
+      struct InitVariableMonomLocalFiniteElementMap
+      {
+        template<typename C>
+        static void init(C & c)
+        {
+          typedef Dune::MonomLocalFiniteElement<D,R,d,p> LFE;
+          typedef typename C::value_type ptr;
+          c[p] = ptr(new LocalFiniteElementVirtualImp<LFE>(LFE(Dune::GeometryType::cube)));
+          
+          InitVariableMonomLocalFiniteElementMap<D,R,d,p-1>::init(c);
+        }
+      };
+      template<class D, class R, int d>
+      struct InitVariableMonomLocalFiniteElementMap<D,R,d,-1>
+      {
+        template<typename C>
+        static void init(C & c) {}
+      };
+    }
+
+	//! FiniteElementMap which provides MonomLocalFiniteElement instances, depending on the local polynomial degree
+    //! \ingroup FiniteElementMap
+	template<class M, class D, class R, int d, int maxP>
+	class VariableMonomLocalFiniteElementMap :
+      public Countable
+	{
+      typedef typename FixedOrderLocalBasisTraits<
+        typename MonomLocalFiniteElement<D,R,d,0>::Traits::LocalBasisType::Traits,0>::Traits T;
+      //! Type of finite element from local functions
+      typedef LocalFiniteElementVirtualInterface<T> FiniteElementType;
+    public:
+      typedef FiniteElementMapTraits<FiniteElementType> Traits;
+      
+      VariableMonomLocalFiniteElementMap (const M & m, unsigned int defaultP) :
+        mapper_(m), polOrder_(mapper_.size(), defaultP)
+      {
+        InitVariableMonomLocalFiniteElementMap<D,R,d,maxP>::init(finiteElements_);
+      }
+      
+	  //! \brief get local basis functions for entity
+	  template<class EntityType>
+	  const typename Traits::FiniteElementType& find (const EntityType& e) const
+	  {
+        unsigned int p = getOrder(e);
+        assert(p <= maxP);
+		return *(finiteElements_[p]);
+	  }
+
+	  template<class EntityType>
+	  void setOrder (const EntityType& e, unsigned int p)
+	  {
+        assert(p <= maxP);
+        unsigned int i = mapper_.map(e);
+        polOrder_[i] = p;
+	  }
+
+	  template<class EntityType>
+	  unsigned int getOrder (const EntityType& e) const
+	  {
+        unsigned int i = mapper_.map(e);
+        return polOrder_[i];
+	  }
+
+	private:
+      const M & mapper_;
+      std::vector<unsigned char> polOrder_;
+      Dune::array< Dune::shared_ptr<FiniteElementType>, maxP+1 > finiteElements_;
+    };
+
+    
+
+  }
+}
+
+#endif //DUNE_PDELAB_VARIABLEMONOMFEM_HH
