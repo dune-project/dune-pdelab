4c75c7e7fb195984149c75b8f62d7484e260565b
Jö Fahlke <joe@dune-project.org>
2010-12-05 23:24:46 +0000

    [TwoArgsMemberFunctionToGridFunctionAdaptor] A variation of the member-function to grid-function adaptor.
    This one works with member functions that return the value instead of
    assigning it to a reference parameter.
    
    Imported from SVN: r864 at trunk

--- dune/pdelab/function/memberadaptor.hh
+++ dune/pdelab/function/memberadaptor.hh
@@ -97,6 +97,94 @@ namespace Dune {
         <Member, Class, GV, RF, dimRange>(obj, member, gv);
     }
 
+    //! GridFunction implemented by a member function of some class
+    /**
+     * \tparam Member   Member function pointer type.
+     * \tparam Class    Type of the class containing the member.
+     * \tparam GV       The type of the GridView
+     * \tparam RF       The type of the range field
+     * \tparam dimRange The dimension of the Range.
+     *
+     * The member function must support the signature
+     * \code
+(obj.*member)(typename GV::template Codim<0>::Entity &e,
+              const FieldVector<typename GV::ctype, GV::dimension> &xl) const;
+     * \endcode
+     * The return value should be something assignable to \c
+     * FieldVector<RF,dimRange>.
+     */
+    template<class Member, class Class,
+             class GV, class RF, std::size_t dimR = 1>
+    class TwoArgsMemberFunctionToGridFunctionAdaptor :
+      public GridFunctionBase<
+        GridFunctionTraits< GV, RF, dimR, FieldVector<RF, dimR> >,
+        TwoArgsMemberFunctionToGridFunctionAdaptor<Member, Class, GV, RF, dimR>
+      >
+    {
+    public:
+      //! export traits class
+      typedef GridFunctionTraits< GV, RF, dimR, FieldVector<RF, dimR> > Traits;
+
+    private:
+      typedef GridFunctionBase<
+        Traits,
+        TwoArgsMemberFunctionToGridFunctionAdaptor<Member, Class, GV, RF, dimR>
+        > Base;
+
+      const Class &obj;
+      Member member;
+      const GV &gv;
+
+    public:
+      //! Construct an adaptor object
+      /**
+       * \param obj_    Class object to call the member function on.
+       * \param member_ Member function pointer to the member to use.
+       * \param gv_     The GridView to use.
+       *
+       * This class store the \c obj_ and \c gv_ references internally and
+       * becomes invalid when they become invalid.
+       */
+      TwoArgsMemberFunctionToGridFunctionAdaptor(const Class &obj_,
+                                                 Member member_,
+                                                 const GV& gv_) :
+        obj(obj_), member(member_), gv(gv_)
+      { }
+
+      void evaluate(const typename Traits::ElementType &e,
+                    const typename Traits::DomainType &x,
+                    typename Traits::RangeType &y) const
+      {
+        y = (obj.*member)(e, x);
+      }
+
+      //! get reference to the internal gridview.
+      const GV& getGridView() const { return gv; }
+    };
+
+    //! easy construction of a TwoArgsMemberFunctionToGridFunctionAdaptor
+    /**
+     * \relates TwoArgsMemberFunctionToGridFunctionAdaptor
+     *
+     * \code
+#include <dune/pdelab/function/memberadaptor.hh>
+     * \endcode
+     *
+     * The only required template parameters are \c RF and \c dimRange, all
+     * other template parameters can be deduced automatically.
+     */
+    template<class RF, std::size_t dimRange,
+             class GV, class Class, class Member>
+    TwoArgsMemberFunctionToGridFunctionAdaptor<Member, Class, GV, RF,
+                                               dimRange>
+    makeTwoArgsMemberFunctionToGridFunctionAdaptor(const Class &obj,
+                                                     Member member,
+                                                     const GV &gv)
+    {
+      return TwoArgsMemberFunctionToGridFunctionAdaptor
+        <Member, Class, GV, RF, dimRange>(obj, member, gv);
+    }
+
     //! BoundaryGridFunction implemented by a member function of some class
     /**
      * \tparam Member   Member function pointer type.
