c92ac9557f3bae9546f258b0dfa03b2433ce87d6
Steffen Müthing <muething@dune-project.org>
2011-01-24 10:51:33 +0000

    [TypeTree] Some more cleanup of nodetags
    Imported from SVN: r906 at branches/review_2011

--- dune/pdelab/common/nodetags.hh
+++ dune/pdelab/common/nodetags.hh
@@ -1,52 +1,50 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 
-#ifndef DUNE_PDELAB_NODETAGS_HH
-#define DUNE_PDELAB_NODETAGS_HH
+#ifndef DUNE_PDELAB_COMMON_TYPETREE_NODETAGS_HH
+#define DUNE_PDELAB_COMMON_TYPETREE_NODETAGS_HH
 
 namespace Dune {
   namespace PDELab {
+    namespace TypeTree {
 
-  namespace TypeTree { // for temporary coexistence with old TypeTree
+      /** \addtogroup TypeTree
+       *  \ingroup PDELab
+       *  \{
+       */
 
-    /** \addtogroup TypeTree
-     *  \ingroup PDELab
-     *  \{
-     */
+      //! Tag designating a leaf node.
+      struct LeafNodeTag {};
 
-    //! Tag designating a leaf node.
-    struct LeafNodeTag {};
+      //! Tag designating a power node.
+      struct PowerNodeTag {};
 
-    //! Tag designating a power node.
-    struct PowerNodeTag {};
+      //! Tag designating a composite node.
+      struct CompositeNodeTag {};
 
-    //! Tag designating a composite node.
-    struct CompositeNodeTag {};
+      //! Tag designating a composite node that is based on variadic templates.
+      struct VariadicCompositeNodeTag {};
 
-    //! Tag designating a composite node that is based on variadic templates.
-    struct VariadicCompositeNodeTag {};
+      //! Tag designating an empty child node in a non-variadic composite node.
+      struct EmptyNodeTag {};
 
 #ifndef DOXYGEN
 
-    //! Special tag used as start value in algorithms.
-    struct StartTag {};
+      //! Special tag used as start value in algorithms.
+      struct StartTag {};
 
-    //! Tag designating an empty child node in a non-variadic composite node.
-    struct EmptyNodeTag {};
-
-    //! This is a marker to fill empty spaces in composite nodes if support for variadic templates is missing.
-    struct EmptyNode
-    {
-      typedef EmptyNodeTag NodeTag;
-    };
+      //! This is a marker to fill empty spaces in composite nodes if support for variadic templates is missing.
+      struct EmptyNode
+      {
+        typedef EmptyNodeTag NodeTag;
+      };
 
 #endif // DOXYGEN
 
-    //! \} group TypeTree
+      //! \} group TypeTree
 
-  } // namespace TypeTree
-
+    } // namespace TypeTree
   } // namespace PDELab
 } //namespace Dune
 
-#endif DUNE_PDELAB_NODETAGS_HH
+#endif DUNE_PDELAB_COMMON_TYPETREE_NODETAGS_HH
