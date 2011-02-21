ca71312f4928d414c3cd868f5aa1df9d19af3f87
Steffen Müthing <muething@dune-project.org>
2011-02-21 23:10:59 +0000

    [Tests] Add unit test for interpolate() functionality
    Right now, this test does not yet check for the correctness
    of the result, still needs to be improved.
    
    Imported from SVN: r1071 at trunk

---
+++ dune/pdelab/test/testinterpolate.cc
@@ -0,0 +1,143 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <iostream>
+#include <dune/common/mpihelper.hh>
+#include <dune/common/exceptions.hh>
+#include <dune/common/fvector.hh>
+#include <dune/grid/yaspgrid.hh>
+#include "../finiteelementmap/p0fem.hh"
+#include "../finiteelementmap/q22dfem.hh"
+#include "../finiteelementmap/q1fem.hh"
+#include "../gridfunctionspace/gridfunctionspace.hh"
+#include "../backend/istlvectorbackend.hh"
+#include "../common/function.hh"
+#include "../gridfunctionspace/interpolate.hh"
+
+template<typename GV, std::size_t range_dim>
+struct interpolation_function
+  : public Dune::PDELab::AnalyticGridFunctionBase<
+  Dune::PDELab::AnalyticGridFunctionTraits<GV,double,range_dim>,
+  interpolation_function<GV,range_dim> >
+{
+
+public:
+  typedef Dune::PDELab::AnalyticGridFunctionTraits<GV,double,range_dim> Traits;
+  typedef Dune::PDELab::AnalyticGridFunctionBase<Traits,interpolation_function<GV,range_dim> > BaseT;
+
+  interpolation_function (const GV& gv) : BaseT(gv) {}
+
+  inline void evaluateGlobal (const typename Traits::DomainType& x,
+                              typename Traits::RangeType& y) const
+  {
+    y = x[0];
+  }
+};
+
+
+template<class GV>
+static void test_interpolate(const GV& gv)
+{
+  // instantiate finite element maps
+  Dune::GeometryType gt;
+  gt.makeCube(2);
+  typedef Dune::PDELab::P0LocalFiniteElementMap<double,double,GV::dimension> P0FEM;
+  P0FEM p0fem(gt);
+  typedef Dune::PDELab::Q1LocalFiniteElementMap<double,double,GV::dimension> Q12DFEM;
+  Q12DFEM q12dfem;
+  typedef Dune::PDELab::Q22DLocalFiniteElementMap<double,double> Q22DFEM;
+  Q22DFEM q22dfem;
+
+  // make a grid function space
+  typedef Dune::PDELab::GridFunctionSpace<GV,P0FEM> P0GFS;
+  P0GFS p0gfs(gv,p0fem);
+  typedef Dune::PDELab::GridFunctionSpace<GV,Q12DFEM> GFS1;
+  GFS1 gfs1(gv,q12dfem);
+  typedef Dune::PDELab::GridFunctionSpace<GV,Q22DFEM> GFS2;
+  GFS2 gfs2(gv,q22dfem);
+  typedef Dune::PDELab::GridFunctionSpace<GV,Q22DFEM,Dune::PDELab::NoConstraints,
+                                          Dune::PDELab::StdVectorBackend,
+                                          Dune::PDELab::GridFunctionRestrictedMapper> GFS3;
+  GFS3 gfs3(gv,q22dfem);
+
+  // test power
+  typedef Dune::PDELab::PowerGridFunctionSpace<GFS2,3> PGFS;
+  PGFS pgfs(gfs2,gfs2,gfs2);
+
+  // test composite
+  typedef Dune::PDELab::CompositeGridFunctionSpace<
+    Dune::PDELab::GridFunctionSpaceLexicographicMapper,
+    P0GFS,GFS1,GFS2,GFS3
+    > CGFS;
+
+  CGFS cgfs(p0gfs,gfs1,gfs2,gfs3);
+
+  // master space - contains power and composite twice, once for interpolation from
+  // scalar function, once for interpolation from vector function
+  typedef Dune::PDELab::CompositeGridFunctionSpace<
+    Dune::PDELab::GridFunctionSpaceLexicographicMapper,
+    GFS1,PGFS,CGFS,PGFS,CGFS
+    > GFS;
+
+  GFS gfs(gfs1,pgfs,cgfs,pgfs,cgfs);
+
+  // make coefficent Vector
+  typedef typename GFS::template VectorContainer<double>::Type V;
+  V x(gfs,0.0);
+
+  // build interpolation function tree
+  typedef interpolation_function<GV,1> scalar_interpolation_function;
+  typedef interpolation_function<GV,3> interpolation_function_3d;
+  typedef interpolation_function<GV,4> interpolation_function_4d;
+
+  scalar_interpolation_function sif(gv);
+  interpolation_function_3d if3d(gv);
+  interpolation_function_4d if4d(gv);
+
+  typedef Dune::PDELab::CompositeGridFunction<
+    scalar_interpolation_function,
+    scalar_interpolation_function,
+    scalar_interpolation_function,
+    interpolation_function_3d,
+    interpolation_function_4d
+    > F;
+
+  F f(sif,sif,sif,if3d,if4d);
+
+  // interpolate
+  Dune::PDELab::interpolate(f,gfs,x);
+
+  //Dune::VTKWriter<GV> vtkwriter(gv);
+  //Dune::PDELab::vtkwriter_tree_addvertexdata(vtkwriter,
+}
+
+int main(int argc, char** argv)
+{
+  try{
+    //Maybe initialize Mpi
+    Dune::MPIHelper::instance(argc, argv);
+
+    std::cout << "interpolation tests (2D)" << std::endl;
+    // need a grid in order to test grid functions
+    Dune::FieldVector<double,2> L(1.0);
+    Dune::FieldVector<int,2> N(1);
+    Dune::FieldVector<bool,2> B(false);
+    Dune::YaspGrid<2> grid(L,N,B,0);
+    grid.globalRefine(1);
+
+    test_interpolate(grid.leafView());
+
+	// test passed
+	return 0;
+
+  }
+  catch (Dune::Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+	return 1;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+	return 1;
+  }
+}
