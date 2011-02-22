ddfe1b38a9f1918cc002bd96feaca0a7b29a47d4
Steffen Müthing <muething@dune-project.org>
2011-02-22 08:40:44 +0000

    [TypeTree] Rename headerguard macro in treepath.hh to reflect new file location
    Imported from SVN: r1073 at trunk

--- dune/pdelab/common/typetree/treepath.hh
+++ dune/pdelab/common/typetree/treepath.hh
@@ -1,8 +1,8 @@
 // -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=8 sw=2 sts=2:
 
-#ifndef DUNE_PDELAB_COMMON_TREEPATH_HH
-#define DUNE_PDELAB_COMMON_TREEPATH_HH
+#ifndef DUNE_PDELAB_COMMON_TYPETREE_TREEPATH_HH
+#define DUNE_PDELAB_COMMON_TYPETREE_TREEPATH_HH
 
 #include <cstddef>
 
@@ -500,4 +500,4 @@ namespace Dune {
   } // namespace PDELab
 } //namespace Dune
 
-#endif // DUNE_PDELAB_COMMON_TREEPATH_HH
+#endif // DUNE_PDELAB_COMMON_TYPETREE_TREEPATH_HH
