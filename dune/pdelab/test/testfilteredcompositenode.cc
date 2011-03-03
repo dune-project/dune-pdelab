d45c52af8b1169cc33ab09c59c728933659f6bff
Steffen Müthing <muething@dune-project.org>
2011-03-03 13:50:41 +0000

    [Unit Tests] Improve testfilteredcompositenode
    - Move test to function to avoid code duplication
    - test for more complicated cases like filtering
      a filtered node
    
    Imported from SVN: r1240 at trunk

--- dune/pdelab/test/testfilteredcompositenode.cc
+++ dune/pdelab/test/testfilteredcompositenode.cc
@@ -53,12 +53,38 @@ public:
 };
 
 
+template<typename Node, typename Filter>
+void testFilteredCompositeNode(Node& node, Filter filter)
+{
+  typedef SimpleFilteredNode<Node,Filter> FN;
+  FN filteredNode(node);
+  Dune::PDELab::TypeTree::applyToTree(filteredNode,TreePrinter());
+  typedef Dune::PDELab::TypeTree::TreeInfo<FN> TI;
+  std::cout << "depth: " << TI::depth << std::endl
+            << "nodes: " << TI::nodeCount << std::endl
+            << "leafs: " << TI::leafCount << std::endl;
+
+  typedef Dune::PDELab::TypeTree::TransformTree<FN,TestTransformation> Transformation;
+
+  typedef typename Transformation::Type TFN;
+
+  TFN tfn = Transformation::transform(filteredNode);
+
+  typedef Dune::PDELab::TypeTree::TreeInfo<TFN> TTI;
+
+  dune_static_assert(TTI::depth == TI::depth, "error in transformation with filtered node");
+
+  dune_static_assert(TTI::nodeCount == TI::nodeCount, "error in transformation with filtered node");
+
+  dune_static_assert(TTI::leafCount == TI::leafCount, "error in transformation with filtered node");
+
+  Dune::PDELab::TypeTree::applyToTree(tfn,TreePrinter());
+}
+
+
 int main(int argc, char** argv)
 {
 
-  // basic tests
-
-  // leaf node
   SimpleLeaf sl1;
 
   typedef SimplePower<SimpleLeaf,3> SP1;
@@ -69,8 +95,6 @@ int main(int argc, char** argv)
 
   SimpleLeaf sl2;
   SP1 sp1_2(sl2,false);
-
-  Dune::PDELab::TypeTree::applyToTree(sp1_1,TreePrinter());
 
   typedef SimpleComposite<SimpleLeaf,SP1,SimpleLeaf> SC1;
   SC1 sc1_1(sl1,sp1_2,sl2);
@@ -88,66 +112,17 @@ int main(int argc, char** argv)
   typedef SimpleVariadicComposite<SimpleLeaf,SVC1,SimpleLeaf,SP1,SC1> SVC2;
   SVC2 svc2_1(sl1,svc1_2,sl2,sp1_3,sc1_1);
 
-  typedef Dune::PDELab::TypeTree::TreeInfo<SVC2> TI;
+  testFilteredCompositeNode(svc2_1,LeafFilter());
 
-  typedef SimpleFilteredNode<SVC2,LeafFilter> FCN1;
-  typedef Dune::PDELab::TypeTree::TreeInfo<FCN1> FCN1_TI;
+  testFilteredCompositeNode(svc2_1,Dune::PDELab::TypeTree::IndexFilter<1,3,2>());
 
-  FCN1 fcn1_1(svc2_1);
+  typedef Dune::PDELab::TypeTree::IndexFilter<3,1,0,4,1,2,1,0,2,1> IndexFilter1;
+  testFilteredCompositeNode(svc2_1,IndexFilter1());
 
-  typedef SimpleFilteredNode<SVC2,Dune::PDELab::TypeTree::IndexFilter<1,2,3> > FCN2;
-  typedef Dune::PDELab::TypeTree::TreeInfo<FCN2> FCN2_TI;
+  typedef SimpleFilteredNode<SVC2,IndexFilter1> SFN;
+  SFN sfn(svc2_1);
 
-  FCN2 fcn2_1(svc2_1);
-
-  typedef SimpleFilteredNode<SVC2,Dune::PDELab::TypeTree::IndexFilter<3,1,0,4,1,2,1,0,2,1> > FCN3;
-  typedef Dune::PDELab::TypeTree::TreeInfo<FCN3> FCN3_TI;
-
-  FCN3 fcn3_1(svc2_1);
-
-
-  // test TreeInfo
-  //dune_static_assert(TI::depth == 4 && TI::nodeCount == 14 && TI::leafCount == 10,
-  //                   "TreeInfo yields wrong information");
-
-  Dune::PDELab::TypeTree::applyToTree(svc2_1,TreePrinter());
-  std::cout << "depth: " << TI::depth << std::endl
-            << "nodes: " << TI::nodeCount << std::endl
-            << "leafs: " << TI::leafCount << std::endl;
-
-  std::cout << std::endl << "leaf filter:" << std::endl;
-  Dune::PDELab::TypeTree::applyToTree(fcn1_1,TreePrinter());
-  std::cout << "depth: " << FCN1_TI::depth << std::endl
-            << "nodes: " << FCN1_TI::nodeCount << std::endl
-            << "leafs: " << FCN1_TI::leafCount << std::endl;
-
-  std::cout << std::endl << "index filter (1,2,3):" << std::endl;
-  Dune::PDELab::TypeTree::applyToTree(fcn2_1,TreePrinter());
-  std::cout << "depth: " << FCN2_TI::depth << std::endl
-            << "nodes: " << FCN2_TI::nodeCount << std::endl
-            << "leafs: " << FCN2_TI::leafCount << std::endl;
-
-  std::cout << std::endl << "index filter (3,1,0,4):" << std::endl;
-  Dune::PDELab::TypeTree::applyToTree(fcn3_1,TreePrinter());
-  std::cout << "depth: " << FCN3_TI::depth << std::endl
-            << "nodes: " << FCN3_TI::nodeCount << std::endl
-            << "leafs: " << FCN3_TI::leafCount << std::endl;
-
-  typedef Dune::PDELab::TypeTree::TransformTree<FCN3,TestTransformation> Transformation;
-
-  typedef typename Transformation::Type TFCN3;
-
-  TFCN3 tfcn3_1 = Transformation::transform(fcn3_1);
-
-  typedef Dune::PDELab::TypeTree::TreeInfo<TFCN3> TFCN3_TI;
-
-  dune_static_assert(TFCN3_TI::depth == FCN3_TI::depth, "error in transformation with filtered node");
-
-  dune_static_assert(TFCN3_TI::nodeCount == FCN3_TI::nodeCount, "error in transformation with filtered node");
-
-  dune_static_assert(TFCN3_TI::leafCount == FCN3_TI::leafCount, "error in transformation with filtered node");
-
-  Dune::PDELab::TypeTree::applyToTree(tfcn3_1,TreePrinter());
+  testFilteredCompositeNode(sfn,IndexFilter1());
 
   return 0;
 }
