7519d1f90e0e3021f2328b9f477d5439b64601ac
Steffen Müthing <muething@dune-project.org>
2011-03-07 14:06:39 +0000

    [Local Containers] Add weighted accumulation views to LocalVector and LocalMatrix
    It is now possible to obtain views for LocalVector
    and LocalMatrix that only allow accumulation and
    take into account a weight during the process.
    
    The views can be obtained by calling
    weightedAccumulationView() and mainly have the following
    interface:
    
    accumulate() - adds value to entry, taking weight into account
    weight() - get weight
    rawAccumulate() - adds value to entry unmodified
    
    The last two methods together can be used to move
    the weight multiplication out of the loop over the
    degrees of freedom.
    
    For backwards compatibility, it is also still possible
    to access the containers using operator[] or operator()
    as before, but the views will return proxies that
    only support operator+=() and operator-=() which
    take the weight into account.
    
    Imported from SVN: r1305 at trunk

--- dune/pdelab/gridfunctionspace/localvector.hh
+++ dune/pdelab/gridfunctionspace/localvector.hh
@@ -4,6 +4,7 @@
 #define DUNE_PDELAB_LOCALVECTOR_HH
 
 #include <vector>
+#include <dune/common/deprecated.hh>
 
 #include "localindex.hh"
 
@@ -14,29 +15,166 @@
 
 namespace Dune {
   namespace PDELab {
-    
+
+#ifndef DOXYGEN
+
+    //! A proxy class to make the new weighted residual and jacobian containers backwards compatible.
+    /**
+     * This proxy for a single entry of a weighted local vector / local matrix only supports
+     * the two operations operator+=() and operator-=(). In both cases, the argument will
+     * be multiplied by the weight associated with the corresponding weighted container.
+     */
+    template<typename T, typename W>
+    class WeightedContainerEntryProxy
+    {
+
+    public:
+
+      template<typename U>
+      void operator+=(const U& r)
+      {
+        _value += _weight * r;
+      }
+
+      template<typename U>
+      void operator-=(const U& r)
+      {
+        _value -= _weight * r;
+      }
+
+      WeightedContainerEntryProxy(T& value, W weight)
+        : _value(value)
+        , _weight(weight)
+      {}
+
+    private:
+
+      void operator=(const WeightedContainerEntryProxy&);
+
+      T& _value;
+      const W _weight;
+    };
+
+
+#endif // DOXYGEN
+
     /**
        \addtogroup PDELAB_StrictTrialAndTest Strict Trial and Test space handling
        \ingroup GridFunctionSpace
        \{
     */
 
+    //! An accumulate-only view on a local vector that automatically takes into account an accumulation weight.
+    template<typename C>
+    class WeightedVectorAccumulationView
+    {
+    public:
+
+      //! The type of the underlying container.
+      typedef C Container;
+
+      //! The value type of the entries.
+      typedef typename Container::value_type value_type;
+
+      //! The type of the weight applied when accumulating contributions.
+      typedef typename Container::weight_type weight_type;
+
+      //! A special wrapper type to enable backwards compatibility with current containers when directly accessing entries.
+      typedef WeightedContainerEntryProxy<value_type,weight_type> reference;
+
+      //! The size_type of the underlying container.
+      typedef typename Container::size_type size_type;
+
+      //! Returns a reference proxy to an entry of the underlying container.
+      /**
+       * \returns A proxy that wraps the entry in the underlying container and
+       *          only allows application of operator+=() and operator-=(). In
+       *          both cases, the argument will automatically be multiplied by
+       *          the weight associated with this view.
+       */
+      reference operator[] (size_type n) DUNE_DEPRECATED
+      {
+        _modified = true;
+        return reference(_container[n],_weight);
+      }
+
+      //! Returns the weight associated with this view.
+      /**
+       * \note This can be used together with rawAccumulate() to avoid applying the weight at
+       * each loop iteration.
+       */
+      weight_type weight()
+      {
+        _modified = true;
+        return _weight;
+      }
+
+      //! Applies the current weight to v and adds the result to the n-th entry of the container.
+      void accumulate(size_type n, value_type v)
+      {
+        _modified = true;
+        _container[n] += _weight * v;
+      }
+
+      //! Adds v to the n-th entry of the underlying container without applying the current weight.
+      /**
+       * \warning When using this method, you must take care of applying the weight yourself, otherwise
+       *          your program may exhibit strange behavior or even calculate wrong results!
+       */
+      void rawAccumulate(size_type n, value_type v)
+      {
+        _container[n] += v;
+      }
+
+      //! Constructor
+      WeightedVectorAccumulationView(C& container, weight_type weight)
+        : _container(container)
+        , _weight(weight)
+        , _modified(false)
+      {}
+
+      //! Returns the size of the underlying container.
+      size_type size() const
+      {
+        return _container.size();
+      }
+
+      //! Returns whether this view has been written to.
+      bool modified() const
+      {
+        return _modified;
+      }
+
+    private:
+      C& _container;
+      const weight_type _weight;
+      bool _modified;
+    };
+
     /**
        \brief a simple container to store local vector entry
 
        This makes it possible to use strict type checking, even for integers.
+
+       \tparam T   The type of values to store in the vector.
+       \tparam TAG Tag type for differentiating between trial and test space vectors.
+       \tparam W   The type of weight applied in a WeightedAccumulationView.
      */
-    template<typename T, typename TAG>
+    template<typename T, typename TAG, typename W = T>
     class LocalVector : public std::vector<T>
     {
     private:
-      typedef std::vector<T> BaseT; 
+      typedef std::vector<T> BaseT;
     public:
       typedef typename BaseT::value_type  value_type;
       typedef typename BaseT::size_type  v_size_type;
       typedef typename BaseT::reference    reference;
       typedef typename BaseT::const_reference const_reference;
       typedef LocalIndex<v_size_type, TAG> size_type;
+      typedef W weight_type;
+
+      //! An accumulate-only view of the vector that automatically takes into account an accumulation weight.
+      typedef WeightedVectorAccumulationView<LocalVector> WeightedAccumulationView;
 
       /**
 	 \{
@@ -55,21 +193,35 @@ namespace Dune {
       reference operator[] (size_type n) { return BaseT::operator[](n.i); }
       const_reference operator[] (size_type n) const { return BaseT::operator[](n.i); }
       /** \} */
+
+      //! Returns a weighted accumulate-only view of this vector with the given weight.
+      WeightedAccumulationView weightedAccumulationView(weight_type weight)
+      {
+        return WeightedAccumulationView(*this,weight);
+      }
+
     private:
     };
 
 #ifndef DOXYGEN
     // specialization for AnySpaceTag
-    template<typename T>
-    class LocalVector<T, AnySpaceTag> : public std::vector<T>
+    template<typename T, typename W>
+    class LocalVector<T, AnySpaceTag, W> : public std::vector<T>
     {
     private:
-      typedef std::vector<T> BaseT; 
+      typedef std::vector<T> BaseT;
     public:
       typedef typename BaseT::value_type  value_type;
       typedef typename BaseT::size_type    size_type;
       typedef typename BaseT::reference    reference;
       typedef typename BaseT::const_reference const_reference;
+      typedef W weight_type;
+      typedef WeightedVectorAccumulationView<LocalVector> WeightedAccumulationView;
+
+      WeightedAccumulationView weightedAccumulationView(weight_type weight)
+      {
+        return WeightedAccumulationView(*this,weight);
+      }
 
       /**
 	 \{
--- dune/pdelab/gridoperatorspace/localmatrix.hh
+++ dune/pdelab/gridoperatorspace/localmatrix.hh
@@ -4,18 +4,118 @@
 #ifndef DUNE_PDELAB_LOCALMATRIX_HH
 #define DUNE_PDELAB_LOCALMATRIX_HH
 
-#include<vector>
+#include <dune/pdelab/gridfunctionspace/localvector.hh>
 
 namespace Dune {
   namespace PDELab {
 
+    //! An accumulate-only view on a local matrix that automatically takes into account an accumulation weight.
+    template<typename C>
+    class WeightedMatrixAccumulationView
+    {
+
+    public:
+
+      //! The type of the underlying container.
+      typedef C Container;
+
+      //! The value type of the entries.
+      typedef typename C::value_type value_type;
+
+      //! The type of the weight applied when accumulating contributions.
+      typedef typename C::weight_type weight_type;
+
+      //! The size_type of the underlying container.
+      typedef typename C::size_type size_type;
+
+      //! A special wrapper type to enable backwards compatibility with current containers when directly accessing entries.
+      typedef WeightedContainerEntryProxy<value_type,weight_type> reference;
+
+      //! Returns the weight associated with this view.
+      /**
+       * \note This can be used together with rawAccumulate() to avoid applying the weight at
+       * each loop iteration.
+       */
+      weight_type weight()
+      {
+        _modified = true;
+        return _weight;
+      }
+
+      //! Applies the current weight to v and adds the result to the (i,j)-th entry of the container.
+      void accumulate(size_type i, size_type j, value_type v)
+      {
+        _modified = true;
+        _container(i,j) += _weight * v;
+      }
+
+      //! Adds v to the (i,j)-th entry of the underlying container without applying the current weight.
+      /**
+       * \warning When using this method, you must take care of applying the weight yourself, otherwise
+       *          your program may exhibit strange behavior or even calculate wrong results!
+       */
+      void rawAccumulate(size_type i, size_type j, value_type v)
+      {
+        _container(i,j) += v;
+      }
+
+      //! Returns a reference proxy to an entry of the underlying container.
+      /**
+       * \returns A proxy that wraps the entry in the underlying container and
+       *          only allows application of operator+=() and operator-=(). In
+       *          both cases, the argument will automatically be multiplied by
+       *          the weight associated with this view.
+       */
+      reference operator()(size_type i, size_type j)
+      {
+        _modified = true;
+        return reference(_container(i,j),_weight);
+      }
+
+      //! Returns the number of rows of the underlying container.
+      size_type nrows() const
+      {
+        return _container.nrows();
+      }
+
+      //! Returns the number of colums of the underlying container.
+      size_type ncols() const
+      {
+        return _container.ncols();
+      }
+
+      //! Returns whether this view has been written to.
+      bool modified() const
+      {
+        return false;
+      }
+
+      // Constructor
+      WeightedMatrixAccumulationView(Container& container, weight_type weight)
+        : _container(container)
+        , _weight(weight)
+        , _modified(false)
+      {}
+
+    private:
+      Container& _container;
+      const weight_type _weight;
+      bool _modified;
+    };
+
 	// a simple container that stores a dense matrix in a std::vector
-	template<typename T>
+	template<typename T, typename W = T>
 	class LocalMatrix
 	{
 	public:
+
       typedef T value_type;
+      typedef W weight_type;
       typedef int size_type;
+
+      //! An accumulate-only view of the matrix that automatically takes into account an accumulation weight.
+      typedef WeightedMatrixAccumulationView<LocalMatrix> WeightedAccumulationView;
+
 	  LocalMatrix () {}
 
 	  LocalMatrix (size_type r, size_type c)
@@ -29,14 +129,14 @@ namespace Dune {
 	  void resize (size_type r, size_type c)
 	  {
 		m.resize(r*c);
-		rows = r; 
+		rows = r;
 		cols = c;
 	  }
 
 	  void assign (size_type r, size_type c, const T& t)
 	  {
 		m.assign(r*c,t);
-		rows = r; 
+		rows = r;
 		cols = c;
 	  }
 
@@ -76,7 +176,13 @@ namespace Dune {
             y[i] += (*this)(i,j) * x[j];
         }
       }
-      
+
+      //! Returns a weighted accumulate-only view of this matrix with the given weight.
+      WeightedAccumulationView weightedAccumulationView(weight_type weight)
+      {
+        return WeightedAccumulationView(*this,weight);
+      }
+
 	private:
 	  std::vector<T> m;
 	  size_type rows, cols;
