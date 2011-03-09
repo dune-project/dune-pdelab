7d1ed38c202f929245307415b9699fa3d292ef17
Steffen Müthing <muething@dune-project.org>
2011-03-09 22:00:04 +0000

    [LocalVector] Convert LocalMatrix to new access interface with (LFS,index) pairs
    This patch adds the new element access using pairs
    of (LFS,index) and also cleans up the implementation
    
    - add new access methods to container and accumulate-only
      view
    - add methods to access the LocalMatrix from the
      view
    - add methods to access the bare storage container
      from both the view and the LocalMatrix
    - add method getEntry(i,j) for accessing the (i,j)-th
      entry of the matrix. Useful both for external functions
      doing things like printing the matrix and for
      centralizing the index transformation in a single
      place.
    - add assignment from field type to improve consistency
      between LocalVector and LocalMatrix
    - deprecate all old access methods
    - add new method usmv() with same semantics as the
      DenseVector implemention from dune-common
    
    Imported from SVN: r1342 at trunk

--- dune/pdelab/gridoperatorspace/localmatrix.hh
+++ dune/pdelab/gridoperatorspace/localmatrix.hh
@@ -24,6 +24,12 @@ namespace Dune {
       //! The type of the underlying container.
       typedef C Container;
 
+      //! The type of the storage container underlying the LocalVector.
+      /**
+       * \warning This is not a matrix-like container anymore, but a std::vector-like one!
+       */
+      typedef typename Container::BaseContainer BaseContainer;
+
       //! The value type of the entries.
       typedef typename C::value_type value_type;
 
@@ -57,11 +63,14 @@ namespace Dune {
         _weight = weight;
       }
 
-      //! Applies the current weight to v and adds the result to the (i,j)-th entry of the container.
-      void accumulate(size_type i, size_type j, value_type v)
+      //! Applies the current weight to v and adds the result to the matrix entry associated with the i-th entry of lfsv and the j-th entry of lfsu.
+      template<typename LFSU, typename LFSV>
+      void accumulate(const LFSV& lfsv, size_type i,
+                      const LFSU& lfsu, size_type j,
+                      value_type v)
       {
         _modified = true;
-        _container(i,j) += _weight * v;
+        _container(lfsv,i,lfsu,j) += _weight * v;
       }
 
       //! Adds v to the (i,j)-th entry of the underlying container without applying the current weight.
@@ -69,9 +78,12 @@ namespace Dune {
        * \warning When using this method, you must take care of applying the weight yourself, otherwise
        *          your program may exhibit strange behavior or even calculate wrong results!
        */
-      void rawAccumulate(size_type i, size_type j, value_type v)
+      template<typename LFSU, typename LFSV>
+      void rawAccumulate(const LFSV& lfsv, size_type i,
+                         const LFSU& lfsu, size_type j,
+                         value_type v)
       {
-        _container(i,j) += v;
+        _container(lfsv,i,lfsu,j) += v;
       }
 
       //! Returns a reference proxy to an entry of the underlying container.
@@ -120,7 +132,39 @@ namespace Dune {
         _modified = false;
       }
 
-      // Constructor
+      //! Returns the container (of type LocalMatrix) that this view is based on.
+      Container& container()
+      {
+        _modified = true;
+        return _container;
+      }
+
+      //! Returns the container (of type LocalMatrix) that this view is based on (const version).
+      const Container& container() const
+      {
+        return _container;
+      }
+
+      //! Returns the storage container of the underlying LocalMatrix.
+      /**
+       * \warning This is not a matrix-like container anymore, but a std::vector-like one!
+       */
+      BaseContainer& base()
+      {
+        _modified = true;
+        return _container.base();
+      }
+
+      //! Returns the storage container of the underlying LocalVector (const version).
+      /**
+       * \warning This is not a matrix-like container anymore, but a std::vector-like one!
+       */
+      const BaseContainer& base() const
+      {
+        return _container.base();
+      }
+
+      //! Constructor
       WeightedMatrixAccumulationView(Container& container, weight_type weight)
         : _container(container)
         , _weight(weight)
@@ -133,77 +177,165 @@ namespace Dune {
       bool _modified;
     };
 
-	// a simple container that stores a dense matrix in a std::vector
-	template<typename T, typename W = T>
+
+    //! A dense matrix for storing data associated with the degrees of freedom of a pair of LocalFunctionSpaces.
+    /**
+     * This container represents a dense matrix based on a std::vector-like storage container and supports accessing
+     * its entries indexed by pairs of (LocalFunctionSpace,DOF of LocalFunctionSpace). If the LocalFunctionSpaces
+     * contain tags indicating whether they are trial or test spaces, the access methods will also assert that the
+     * first space is a test space and the second space is a trial space.
+     *
+     * \tparam T            The type of values to store in the matrix.
+     * \tparam W            The type of weight applied in a WeightedAccumulationView.
+     */	template<typename T, typename W = T>
 	class LocalMatrix
 	{
 	public:
 
-      typedef T value_type;
+      //! The type of the underlying storage container.
+      /**
+       * \warning This is not a matrix-like container anymore, but a std::vector-like one!
+       */
+      typedef std::vector<T> BaseContainer;
+
+      //! The value type of this container.
+      typedef typename BaseContainer::value_type  value_type;
+
+      //! The size type of this container.
+      typedef typename BaseContainer::size_type    size_type;
+
+      //! The reference type of this container.
+      typedef typename BaseContainer::reference    reference;
+
+      //! The const reference type of this container.
+      typedef typename BaseContainer::const_reference const_reference;
+
+      //! The weight type of this container.
+      /**
+       * A value of this type will be used to assign a weight to contributions in
+       * a WeightedAccumulationView.
+       */
       typedef W weight_type;
-      typedef int size_type;
 
-      //! An accumulate-only view of the matrix that automatically takes into account an accumulation weight.
-      typedef WeightedMatrixAccumulationView<LocalMatrix> WeightedAccumulationView;
+      //! An accumulate-only view of this container that automatically applies a weight to all contributions.
+      typedef WeightedVectorAccumulationView<LocalMatrix> WeightedAccumulationView;
 
+      //! Default constructor
 	  LocalMatrix () {}
 
+      //! Construct a LocalMatrix with r rows and c columns.
 	  LocalMatrix (size_type r, size_type c)
-		: m(r*c), rows(r), cols(c)
+		: _container(r*c)
+        , _rows(r)
+        , _cols(c)
 	  {}
 
+      //! Construct a LocalMatrix with r rows and c columns and initialize its entries with t.
 	  LocalMatrix (size_type r, size_type c, const T& t)
-		: m(r*c,t), rows(r), cols(c)
+		: _container(r*c,t)
+        , _rows(r)
+        , _cols(c)
 	  {}
 
+      //! Resize the matrix.
 	  void resize (size_type r, size_type c)
 	  {
-		m.resize(r*c);
-		rows = r;
-		cols = c;
+		_container.resize(r*c);
+		_rows = r;
+		_cols = c;
 	  }
 
-	  void assign (size_type r, size_type c, const T& t)
-	  {
-		m.assign(r*c,t);
-		rows = r;
-		cols = c;
-	  }
-
-	  const T& operator() (size_type i, size_type j) const
-	  {
-		return m[j*rows+i];
-	  }
-
-	  T& operator() (size_type i, size_type j)
-	  {
-		return m[j*rows+i];
-	  }
-
-      LocalMatrix& operator *= (const T& x)
+      //! Assign t to all entries of the matrix.
+      LocalMatrix& operator=(const T& t)
       {
-        for (size_t i=0; i<m.size(); ++i) m[i] *= x;
+        std::fill(_container.begin(),_container.end(),t);
         return *this;
       }
 
-	  size_type nrows () const
+      //! Resize the matrix and assign t to all entries.
+	  void assign (size_type r, size_type c, const T& t)
 	  {
-		return rows;
+		_container.assign(r*c,t);
+		_rows = r;
+		_cols = c;
 	  }
 
+      //! Direct access to the (i,j)-th entry.
+	  T& operator() (size_type i, size_type j) DUNE_DEPRECATED
+	  {
+		return getEntry(i,j);
+	  }
+
+      //! Direct access to the (i,j)-th entry (const version).
+	  const T& operator() (size_type i, size_type j) const DUNE_DEPRECATED
+	  {
+		return getEntry(i,j);
+	  }
+
+      //! Access the value associated with the i-th DOF of lfsv and the j-th DOF of lfsu.
+      /**
+       * \param lfsv The LocalFunctionSpace whose DOF will determine the row index of the entry.
+       * \param i    The index of the DOF of lfsv that will determine the row index of the entry.
+       * \param lfsu The LocalFunctionSpace whose DOF will determine the column index of the entry.
+       * \param j    The index of the DOF of lfsu that will determine the column index of the entry.
+       */
+      template<typename LFSU, typename LFSV>
+	  T& operator() (const LFSV& lfsv, size_type i, const LFSU& lfsu, size_type j)
+	  {
+		return getEntry(lfsv.localIndex(i),lfsu.localIndex(j));
+	  }
+
+      //! Access the value associated with the i-th DOF of lfsv and the j-th DOF of lfsu (const version).
+      /**
+       * \param lfsv The LocalFunctionSpace whose DOF will determine the row index of the entry.
+       * \param i    The index of the DOF of lfsv that will determine the row index of the entry.
+       * \param lfsu The LocalFunctionSpace whose DOF will determine the column index of the entry.
+       * \param j    The index of the DOF of lfsu that will determine the column index of the entry.
+       */
+      template<typename LFSU, typename LFSV>
+	  const T& operator() (const LFSV& lfsv, size_type i, const LFSU& lfsu, size_type j) const
+	  {
+		return getEntry(lfsv.localIndex(i),lfsu.localIndex(j));
+	  }
+
+      //! Multiplies all entries of the matrix with x.
+      LocalMatrix& operator *= (const T& x)
+      {
+        std::transform(_container.begin(),_container.end(),_container.begin(),std::bind1st(std::multiplies<T>(),x));
+        return *this;
+      }
+
+      //! Returns the number of rows.
+	  size_type nrows () const
+	  {
+		return _rows;
+	  }
+
+      //! Returns the number of columns.
 	  size_type ncols () const
 	  {
-		return cols;
+		return _cols;
 	  }
 
       //! y = A x
       template<class X, class R>
       void umv (const X& x, R& y) const
       {
-        for (size_type i=0; i<rows; ++i)
+        for (size_type i=0; i<_rows; ++i)
         {
-          for (size_type j=0; j<cols; j++)
-            y[i] += (*this)(i,j) * x[j];
+          for (size_type j=0; j<_cols; j++)
+            accessBaseContainer(y)[i] += getEntry(i,j) * accessBaseContainer(x)[j];
+        }
+      }
+
+      //! y = alpha A x
+      template<class X, class R>
+      void usmv (const value_type& alpha, const X& x, R& y) const
+      {
+        for (size_type i=0; i<_rows; ++i)
+        {
+          for (size_type j=0; j<_cols; j++)
+            accessBaseContainer(y)[i] += alpha * getEntry(i,j) * accessBaseContainer(x)[j];
         }
       }
 
@@ -217,14 +349,20 @@ namespace Dune {
         return 1.0;
       }
 
-      void accumulate(size_type i, size_type j, value_type v) DUNE_DEPRECATED
+      template<typename LFSU, typename LFSV>
+      void DUNE_DEPRECATED accumulate(const LFSV& lfsv, size_type i,
+                                      const LFSU& lfsu, size_type j,
+                                      value_type v)
       {
-        this->operator()(i,j) += v;
+        (*this)(lfsv,i,lfsu,j) += v;
       }
 
-      void rawAccumulate(size_type i, size_type j, value_type v) DUNE_DEPRECATED
+      template<typename LFSU, typename LFSV>
+      void DUNE_DEPRECATED rawAccumulate(const LFSV& lfsv, size_type i,
+                                         const LFSU& lfsu, size_type j,
+                                         value_type v)
       {
-        this->operator()(i,j) += v;
+        (*this)(lfsv,i,lfsu,j) += v;
       }
 
 
@@ -234,18 +372,65 @@ namespace Dune {
         return WeightedAccumulationView(*this,weight);
       }
 
+      //! Returns the underlying storage container.
+      /**
+       * \warning This is not a matrix-like container anymore, but a std::vector-like one!
+       */
+      BaseContainer& base()
+      {
+        return _container;
+      }
+
+      //! Returns the underlying storage container (const version).
+      /**
+       * \warning This is not a matrix-like container anymore, but a std::vector-like one!
+       */
+      const BaseContainer& base() const
+      {
+        return _container;
+      }
+
+      //! Direct (unmapped) access to the (i,j)-th entry of the matrix.
+      /**
+       * \note This method is just a convenience method for helper function doing things
+       *       like printing the matrix, as the BaseContainer is a linear std::vector-like
+       *       object and does not know about the shape of the matrix anymore.
+       *
+       * \warning This method should normally not be called by normal users. Only use it if you
+       *          really know what you are doing!
+       */
+      value_type& getEntry(size_type i, size_type j)
+      {
+        return _container[j*_rows + i];
+      }
+
+      //! Direct (unmapped) access to the (i,j)-th entry of the matrix (const version).
+      /**
+       * \note This method is just a convenience method for helper function doing things
+       *       like printing the matrix, as the BaseContainer is a linear std::vector-like
+       *       object and does not know about the shape of the matrix anymore.
+       *
+       * \warning This method should normally not be called by normal users. Only use it if you
+       *          really know what you are doing!
+       */
+      const value_type& getEntry(size_type i, size_type j) const
+      {
+        return _container[j*_rows + i];
+      }
+
 	private:
-	  std::vector<T> m;
-	  size_type rows, cols;
+
+	  std::vector<T> _container;
+	  size_type _rows, _cols;
 	};
 
-    template<class Stream, class T>
-    Stream &operator<<(Stream &stream, const LocalMatrix<T> &m) {
+    template<class Stream, class T, class W>
+    Stream &operator<<(Stream &stream, const LocalMatrix<T,W> &m) {
       for(int r = 0; r < m.nrows(); ++r) {
         if(m.ncols() >= 1)
-          stream << m(r, 0);
+          stream << m.getEntry(r, 0);
         for(int c = 1; c < m.ncols(); ++c)
-          stream << "\t" << m(r, c);
+          stream << "\t" << m.getEntry(r, c);
         stream << "\n";
       }
       return stream;
