47d4942f3a759f7138d0bbeea21c8bd60104dbe1
Jö Fahlke <joe@dune-project.org>
2011-03-10 16:29:08 +0000

    [GridFunctionProbe] Do the hierarchic search on the Interior_Partition instead of All_Partition.
    This avoids an "internal error" in the hierarchic search with ghosts (who may
    not have all children).
    
    MERGED from branches/2.1snapshot@1366.
    
    
    Imported from SVN: r1368 at trunk

--- dune/pdelab/common/functionutilities.hh
+++ dune/pdelab/common/functionutilities.hh
@@ -118,7 +118,7 @@ gf.getGridView().comm().sum(sum);
           e.reset(new EPtr
                   (HierarchicSearch<typename GV::Grid, typename GV::IndexSet>
                    (gfp->getGridView().grid(), gfp->getGridView().indexSet()).
-                   findEntity(xg)));
+                   template findEntity<Interior_Partition>(xg)));
           // make sure only interior entities are accepted
           if((*e)->partitionType() == InteriorEntity)
             evalRank = myRank;
