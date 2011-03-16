c6af40bb5662dc72c9a29e45bc5ce20061c04a64
Steffen Müthing <muething@dune-project.org>
2011-03-16 13:18:49 +0000

    [LocalAssemblerEngineBase] Port to new onBind() / onUnbind() method signatures
    Imported from SVN: r1409 at trunk

--- dune/pdelab/gridoperator/common/localassemblerenginebase.hh
+++ dune/pdelab/gridoperator/common/localassemblerenginebase.hh
@@ -101,64 +101,142 @@ namespace Dune {
         //! @name Callbacks for LocalFunctionSpace binding and unbinding events
         //! @{
 
-        template<typename EG, typename LFSU, typename LFSV>
-        void onBindLFSUV(const EG& eg, const LFSU& lfsu, const LFSV& lfsv)
+        template<typename EG,
+                 typename LFSU, typename LFSV>
+        void onBindLFSUV(const EG& eg,
+                         const LFSU& lfsu, const LFSV& lfsv)
         {
         }
 
-        template<typename EG, typename LFSV>
-        void onBindLFSV(const EG& eg, const LFSV& lfsv)
+        template<typename EG,
+                 typename LFSV>
+        void onBindLFSV(const EG& eg,
+                        const LFSV& lfsv)
         {
         }
 
-        template<typename EG, typename LFSU, typename LFSV>
-        void onUnbindLFSUV(const EG& eg, const LFSU& lfsu, const LFSV& lfsv)
+        template<typename EG,
+                 typename LFSU, typename LFSV>
+        void onUnbindLFSUV(const EG& eg,
+                           const LFSU& lfsu, const LFSV& lfsv)
         {
         }
 
-        template<typename EG, typename LFSV_S>
-        void onUnbindLFSV(const EG& eg, const LFSV_S& lfsv_s)
-        {
-        }
-
-        template<typename IG, typename LFSU_N, typename LFSV_N>
-        void onBindLFSUVOutside(const IG& ig, const LFSU_N& lfsu_n, const LFSV_N& lfsv_n)
-        {
-        }
-
-        template<typename IG, typename LFSV_N>
-        void onBindLFSVOutside(const IG& ig, const LFSV_N& lfsv_n)
-        {
-        }
-
-        template<typename IG, typename LFSU_N, typename LFSV_N>
-        void onUnbindLFSUVOutside(const IG& ig, const LFSU_N& lfsu_n, const LFSV_N& lfsv_n)
-        {
-        }
-
-        template<typename IG, typename LFSV_N>
-        void onUnbindLFSVOutside(const IG& ig, const LFSV_N& lfsv_n)
+        template<typename EG,
+                 typename LFSV_S>
+        void onUnbindLFSV(const EG& eg,
+                          const LFSV_S& lfsv_s)
         {
         }
 
 
-        template<typename IG, typename LFSU_C, typename LFSV_C>
-        void onBindLFSUVCoupling(const IG& ig, const LFSU_C& lfsu_c, const LFSV_C& lfsv_c)
+        template<typename IG,
+                 typename LFSU, typename LFSV>
+        void onBindLFSUVInside(const IG& ig,
+                               const LFSU& lfsu, const LFSV& lfsv)
         {
         }
 
-        template<typename IG, typename LFSV_C>
-        void onBindLFSVCoupling(const IG& ig, const LFSV_C& lfsv_c)
+        template<typename IG,
+                 typename LFSV>
+        void onBindLFSVInside(const IG& ig,
+                              const LFSV& lfsv)
         {
         }
 
-        template<typename IG, typename LFSU_C, typename LFSV_C>
-        void onUnbindLFSUVCoupling(const IG& ig, const LFSU_C& lfsu_c, const LFSV_C& lfsv_c)
+        template<typename IG,
+                 typename LFSU, typename LFSV>
+        void onUnbindLFSUVInside(const IG& ig,
+                                 const LFSU& lfsu, const LFSV& lfsv)
         {
         }
 
-        template<typename IG, typename LFSV_C>
-        void onUnbindLFSVCoupling(const IG& ig, const LFSV_C& lfsv_c)
+        template<typename IG,
+                 typename LFSV_S>
+        void onUnbindLFSVInside(const IG& ig,
+                                const LFSV_S& lfsv_s)
+        {
+        }
+
+
+        template<typename IG,
+                 typename LFSU_S, typename LFSV_S,
+                 typename LFSU_N, typename LFSV_N>
+        void onBindLFSUVOutside(const IG& ig,
+                                const LFSU_S& lfsu_s, const LFSV_S& lfsv_s,
+                                const LFSU_N& lfsu_n, const LFSV_N& lfsv_n)
+        {
+        }
+
+        template<typename IG,
+                 typename LFSV_S,
+                 typename LFSV_N>
+        void onBindLFSVOutside(const IG& ig,
+                               const LFSV_S& lfsv_s,
+                               const LFSV_N& lfsv_n)
+        {
+        }
+
+        template<typename IG,
+                 typename LFSU_S, typename LFSV_S,
+                 typename LFSU_N, typename LFSV_N>
+        void onUnbindLFSUVOutside(const IG& ig,
+                                  const LFSU_S& lfsu_s, const LFSV_S& lfsv_s,
+                                  const LFSU_N& lfsu_n, const LFSV_N& lfsv_n)
+        {
+        }
+
+        template<typename IG,
+                 typename LFSV_S,
+                 typename LFSV_N>
+        void onUnbindLFSVOutside(const IG& ig,
+                                 const LFSV_S& lfsv_s,
+                                 const LFSV_N& lfsv_n)
+        {
+        }
+
+
+        template<typename IG,
+                 typename LFSU_S, typename LFSV_S,
+                 typename LFSU_N, typename LFSV_N,
+                 typename LFSU_C, typename LFSV_C>
+        void onBindLFSUVCoupling(const IG& ig,
+                                 const LFSU_S& lfsu_s, const LFSV_S& lfsv_s,
+                                 const LFSU_N& lfsu_n, const LFSV_N& lfsv_n,
+                                 const LFSU_C& lfsu_c, const LFSV_C& lfsv_c)
+        {
+        }
+
+        template<typename IG,
+                 typename LFSV_S,
+                 typename LFSV_N,
+                 typename LFSV_C>
+        void onBindLFSVCoupling(const IG& ig,
+                                const LFSV_S& lfsv_s,
+                                const LFSV_N& lfsv_n,
+                                const LFSV_C& lfsv_c)
+        {
+        }
+
+        template<typename IG,
+                 typename LFSU_S, typename LFSV_S,
+                 typename LFSU_N, typename LFSV_N,
+                 typename LFSU_C, typename LFSV_C>
+        void onUnbindLFSUVCoupling(const IG& ig,
+                                   const LFSU_S& lfsu_s, const LFSV_S& lfsv_s,
+                                   const LFSU_N& lfsu_n, const LFSV_N& lfsv_n,
+                                   const LFSU_C& lfsu_c, const LFSV_C& lfsv_c)
+        {
+        }
+
+        template<typename IG,
+                 typename LFSV_S,
+                 typename LFSV_N,
+                 typename LFSV_C>
+        void onUnbindLFSVCoupling(const IG& ig,
+                                  const LFSV_S& lfsv_s,
+                                  const LFSV_N& lfsv_n,
+                                  const LFSV_C& lfsv_c)
         {
         }
 
