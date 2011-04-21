bd6a427a4a9fca0476689259677e619aeeb95c24
Felix Heimann <felix@dune-project.org>
2011-04-21 13:42:49 +0000

    Remove old grid operator space
    Imported from SVN: r1446 at trunk

--- dune/pdelab/gridoperatorspace/gridoperatorspace.hh
+++
@@ -1,5 +0,0 @@
-#ifdef USE_OLD_GRIDOPERATORSPACE
-#include <dune/pdelab/gridoperatorspace/old_gridoperatorspace.hh>
-#else
-#include <dune/pdelab/gridoperatorspace/new_gridoperatorspace.hh>
-#endif
--- dune/pdelab/gridoperatorspace/instationarygridoperatorspace.hh
+++
@@ -1,5 +0,0 @@
-#ifdef USE_OLD_GRIDOPERATORSPACE
-#include <dune/pdelab/gridoperatorspace/old_instationarygridoperatorspace.hh>
-#else
-#include <dune/pdelab/gridoperatorspace/new_instationarygridoperatorspace.hh>
-#endif
