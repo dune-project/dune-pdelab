e0dfbd1084fce031a9ac453589b2ee9d3f9f9904
Jö Fahlke <joe@dune-project.org>
2011-08-07 00:22:00 +0000

    [vtkexport] Untabify.
    Hopefully I'll be able to commit my later changes now...
    
    Imported from SVN: r1578 at trunk

--- dune/pdelab/common/vtkexport.hh
+++ dune/pdelab/common/vtkexport.hh
@@ -1,5 +1,5 @@
-// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// vi: set et ts=4 sw=2 sts=2:
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
 
 #ifndef DUNE_PDELAB_VTKEXPORT_HH
 #define DUNE_PDELAB_VTKEXPORT_HH
@@ -13,16 +13,16 @@ namespace Dune {
   namespace PDELab {
 
     //! wrap a GridFunction so it can be used with the VTKWriter from dune-grid.
-	template<typename T> // T is a grid function
-	class VTKGridFunctionAdapter
-	  : public Dune::VTKFunction<typename T::Traits::GridViewType::Grid>
+    template<typename T> // T is a grid function
+    class VTKGridFunctionAdapter
+      : public Dune::VTKFunction<typename T::Traits::GridViewType::Grid>
     {
-	  typedef typename T::Traits::GridViewType::Grid::ctype DF;
-	  enum {n=T::Traits::GridViewType::dimension};
-	  typedef typename T::Traits::GridViewType::Grid::template Codim<0>::Entity Entity;
+      typedef typename T::Traits::GridViewType::Grid::ctype DF;
+      enum {n=T::Traits::GridViewType::dimension};
+      typedef typename T::Traits::GridViewType::Grid::template Codim<0>::Entity Entity;
 
-	public:
-	  VTKGridFunctionAdapter (const T& t_, std::string s_)
+    public:
+      VTKGridFunctionAdapter (const T& t_, std::string s_)
         : t(t_), s(s_), remap(T::Traits::dimRange)
       {
         for(std::size_t c = 0; c < T::Traits::dimRange; ++c)
@@ -44,34 +44,34 @@ namespace Dune {
       VTKGridFunctionAdapter (const T& t_, std::string s_,
                               const std::vector<std::size_t> &remap_)
         : t(t_), s(s_), remap(remap_)
-	  {}
+      {}
 
-	  virtual int ncomps () const
-	  {
+      virtual int ncomps () const
+      {
         return remap.size();;
-	  }
+      }
 
-	  virtual double evaluate (int comp, const Entity& e, const Dune::FieldVector<DF,n>& xi) const
-	  {
-		typename T::Traits::DomainType x;
-		typename T::Traits::RangeType y;
+      virtual double evaluate (int comp, const Entity& e, const Dune::FieldVector<DF,n>& xi) const
+      {
+        typename T::Traits::DomainType x;
+        typename T::Traits::RangeType y;
 
-		for (int i=0; i<n; i++) 
-		  x[i] = xi[i];
-		t.evaluate(e,x,y);
+        for (int i=0; i<n; i++)
+          x[i] = xi[i];
+        t.evaluate(e,x,y);
         return y[remap[comp]];
-	  }
-	  
-	  virtual std::string name () const
-	  {
-		return s;
-	  }
+      }
 
-	private:
-	  const T& t;
-	  std::string s;
+      virtual std::string name () const
+      {
+        return s;
+      }
+
+    private:
+      const T& t;
+      std::string s;
       std::vector<std::size_t> remap;
-	};
+    };
 
     /** vizualize order of a hp-FiniteElementMap so it can be used with the VTKWriter from dune-grid.
         @tparam GV GridView to vizualize on
@@ -79,35 +79,35 @@ namespace Dune {
      */
     template<typename G, typename FEM>
     class VTKFiniteElementMapAdapter
-	  : public Dune::VTKFunction<G>
+      : public Dune::VTKFunction<G>
     {
-	  typedef typename G::ctype DF;
-	  enum {n=G::dimension};
-	  typedef typename G::template Codim<0>::Entity Entity;
+      typedef typename G::ctype DF;
+      enum {n=G::dimension};
+      typedef typename G::template Codim<0>::Entity Entity;
 
-	public:
+    public:
       VTKFiniteElementMapAdapter (const FEM& fem_, std::string s_)
-		: fem(fem_), s(s_)
-	  {}
+        : fem(fem_), s(s_)
+      {}
 
-	  virtual int ncomps () const
-	  {
-		return 1;
-	  }
+      virtual int ncomps () const
+      {
+        return 1;
+      }
 
-	  virtual double evaluate (int comp, const Entity& e, const Dune::FieldVector<DF,n>& xi) const
-	  {
+      virtual double evaluate (int comp, const Entity& e, const Dune::FieldVector<DF,n>& xi) const
+      {
         return fem.getOrder(e);
-	  }
-	  
-	  virtual std::string name () const
-	  {
-		return s;
-	  }
+      }
 
-	private:
-	  const FEM& fem;
-	  std::string s;
+      virtual std::string name () const
+      {
+        return s;
+      }
+
+    private:
+      const FEM& fem;
+      std::string s;
     };
 
   }
