9e45549e9b5ca7506cc825fd3683f28bacdd3348
Adrian Ngo <ngo@dune-project.org>
2011-08-09 17:37:21 +0000

    Add hanging nodes constraints handling for 2D Cubes with Q1 2D and 3D Simplices with P1
    Imported from SVN: r1594 at trunk

--- dune/pdelab/finiteelementmap/hangingnodeconstraints.hh
+++ dune/pdelab/finiteelementmap/hangingnodeconstraints.hh
@@ -55,7 +55,14 @@ namespace Dune {
           const LFS & lfs = e_has_hangingnodes ? lfs_e : lfs_f;
           const LocalCoefficientType & localCoefficients =
             lfs.finiteElement().localCoefficients();
-          assert(localCoefficients.size()==8);
+
+          if(dimension == 3){
+            assert(localCoefficients.size()==8);
+          }
+          else{
+            assert(localCoefficients.size()==4);
+          }
+
           const int faceindex = e_has_hangingnodes ? ig.indexInInside() : ig.indexInOutside();
           const GRE & refelement = e_has_hangingnodes ? refelement_e : refelement_f;
           const FlagVector & nodeState = e_has_hangingnodes ? nodeState_e : nodeState_f;
@@ -84,6 +91,7 @@ namespace Dune {
             typename T::RowType contribution;
 
             if(dimension == 3){
+
               assert(nodeState.size() == 8);
               
               const SizeType i = 4*j;
@@ -119,16 +127,172 @@ namespace Dune {
                 // Write into local constraints container
               }
 
-            } // dimension == 3
-            else if(dimension == 2){
+            } else if(dimension == 2){
               
+              assert(nodeState.size() == 4);
+
+
+              // Only hanging nodes have contribution to other nodes
+              if(nodeState[m[j]].isHanging()){
+
+                const SizeType node_coeff_index = mapEntityCoeff[m[j]];
+                
+                const SizeType n_j = 1-j;
+
+                assert( !nodeState[m[n_j]].isHanging() );
+
+                // If both neighbors are hanging nodes, then this node
+                // is diagonal to the target of the contribution
+
+                contribution[mapEntityCoeff[ m[n_j] ]] = 0.5;
+                trafo[node_coeff_index] = contribution;
+                // Write into local constraints container
+              }
+
+            } // end if(dimension==3)
+
+          } // j
+
+        } // end of static void assembleConstraints
+
+      }; // end of class CubeGridQ1Assembler
+
+
+      class SimplexGridP1Assembler
+      {
+      public:
+        template<typename I, 
+                 typename LFS, 
+                 typename T, 
+                 typename FlagVector>
+        static void assembleConstraints( const FlagVector & nodeState_e, 
+                                         const FlagVector & nodeState_f,
+                                         const bool & e_has_hangingnodes, 
+                                         const bool & f_has_hangingnodes,
+                                         const LFS & lfs_e, const LFS & lfs_f,
+                                         T& trafo_e, T& trafo_f,
+                                         const IntersectionGeometry<I>& ig) 
+        {
+          typedef IntersectionGeometry<I> Intersection;
+          typedef typename Intersection::EntityPointer CellEntityPointer;
+          typedef typename Intersection::Geometry FaceGeometry;
+          typedef typename FaceGeometry::ctype DT;
+          typedef typename LFS::Traits::FiniteElementType FiniteElementType;
+          typedef typename FiniteElementType::Traits::LocalCoefficientsType LocalCoefficientType;
+          typedef typename FiniteElementType::Traits::LocalBasisType::Traits::DomainFieldType DFT;
+          typedef typename FiniteElementType::Traits::LocalBasisType::Traits::RangeFieldType RFT;
+          typedef typename LFS::Traits::SizeType SizeType;
+
+          const CellEntityPointer e = ig.inside();
+          const CellEntityPointer f = ! ig.boundary() ? ig.outside() : ig.inside();
+
+          const std::size_t dimension = Intersection::dimension;
+
+          typedef Dune::GenericReferenceElement<DT,dimension> GRE;
+          const GRE& refelement_e = Dune::GenericReferenceElements<DT,dimension>::general(e->type());
+          const GRE& refelement_f = Dune::GenericReferenceElements<DT,dimension>::general(f->type());
+
+          // If both entities have hangingnodes, then the face is
+          // conforming and no constraints have to be applied.
+          if(e_has_hangingnodes && f_has_hangingnodes)
+            return;
+
+          // Choose local function space etc for element with hanging nodes
+          const LFS & lfs = e_has_hangingnodes ? lfs_e : lfs_f;
+          const LocalCoefficientType & localCoefficients =
+            lfs.finiteElement().localCoefficients();
+
+          if(dimension == 3){
+            assert(localCoefficients.size()==4);
+          }
+          else{
+            assert(localCoefficients.size()==3);
+          }
+
+          const int faceindex = e_has_hangingnodes ? ig.indexInInside() : ig.indexInOutside();
+          const GRE & refelement = e_has_hangingnodes ? refelement_e : refelement_f;
+          const FlagVector & nodeState = e_has_hangingnodes ? nodeState_e : nodeState_f;
+          T & trafo = e_has_hangingnodes ? trafo_e : trafo_f;
+
+          // A map mapping the local entity index to the local coefficient index
+          std::vector<SizeType> mapEntityCoeff(localCoefficients.size());
+          for (SizeType i=0; i<localCoefficients.size(); i++){
+            mapEntityCoeff[localCoefficients.localKey(i).subEntity()] = localCoefficients.localKey(i).index() + i;
+            if( localCoefficients.localKey(i).codim() != dimension){
+              DUNE_THROW(Dune::InvalidStateException,
+                         "Local coefficients are expected to be bound to vertex entities");
             }
+          }//i
 
-          }//j          
+          // A map mapping the local indices from the face to local
+          // indices of the cell
+          std::vector<int> m(refelement.size(faceindex,1,dimension));
+          for (int j=0; j<refelement.size(faceindex,1,dimension); j++)
+            m[j] = refelement.subEntity(faceindex,1,j,dimension);
+            
+          // Find the corresponding entity in the reference element
+          for (int j=0; j<refelement.size(faceindex,1,dimension); j++){
 
-        }// assembleConstraints
-      };
-    };
+            // The contribution factors of the base function bound to entity j
+            typename T::RowType contribution;
+
+            if(dimension == 3){
+
+              assert(nodeState.size() == 4);
+              // Only hanging nodes have contribution to other nodes
+              if(nodeState[m[j]].isHanging()){
+                const SizeType node_coeff_index = mapEntityCoeff[m[j]];
+
+                for( int k=1; k<=2; ++k ){
+                  
+                  const SizeType n_j = (j+k)%3;
+                 
+                  if( !nodeState[m[n_j]].isHanging() ){
+                
+                    // If both neighbors are hanging nodes, then this node
+                    // is diagonal to the target of the contribution
+                    contribution[mapEntityCoeff[ m[n_j] ]] = 0.5;
+                    trafo[node_coeff_index] = contribution;
+                    // Write into local constraints container
+                    
+                  }
+
+                }
+
+
+
+              }
+
+            } else if(dimension == 2){
+
+              assert(nodeState.size() == 3);
+              // Only hanging nodes have contribution to other nodes
+              if(nodeState[m[j]].isHanging()){
+                const SizeType node_coeff_index = mapEntityCoeff[m[j]];
+                const SizeType n_j = 1-j;
+                assert( !nodeState[m[n_j]].isHanging() );
+                // If both neighbors are hanging nodes, then this node
+                // is diagonal to the target of the contribution
+                contribution[mapEntityCoeff[ m[n_j] ]] = 0.5;
+                trafo[node_coeff_index] = contribution;
+                // Write into local constraints container
+              }
+
+
+            } // end if(dimension==3)
+
+          } // j
+
+        } // end of static void assembleConstraints
+
+      }; // end of class SimplexGridP1Assembler
+
+    }; // end of class HangingNodesConstraintsAssemblers
+
+
+
+
+
 
     //! Constraints construction
     // works in any dimension and on all element types
@@ -145,7 +309,9 @@ namespace Dune {
       enum { doVolume = false };
       enum { dimension = Grid::dimension };
 
-      HangingNodesDirichletConstraints(Grid & grid, bool adaptToIsolatedHangingNotes, const BoundaryFunction & _boundaryFunction)
+      HangingNodesDirichletConstraints( Grid & grid, 
+                                        bool adaptToIsolatedHangingNotes, 
+                                        const BoundaryFunction & _boundaryFunction )
         : manager(grid, _boundaryFunction)
       {
         if(adaptToIsolatedHangingNotes)
@@ -285,7 +451,7 @@ namespace Dune {
                               ig);
       } // skeleton
 
-    };
+    }; // end of class HangingNodesDirichletConstraints
 
 
   }
