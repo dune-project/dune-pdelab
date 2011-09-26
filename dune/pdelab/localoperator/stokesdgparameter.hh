2dc5892a34f5a25bfa62caef5898b23f58ecf2d0
Christian Engwer <christi@dune-project.org>
2011-09-26 18:23:19 +0000

    [stokes] introduce common boundary-condition-types for all stokes problems
    Imported from SVN: r1645 at trunk

--- dune/pdelab/localoperator/stokesdgparameter.hh
+++ dune/pdelab/localoperator/stokesdgparameter.hh
@@ -1,35 +1,10 @@
 #ifndef DUNE_PDELAB_LOCALOPERATOR_STOKESDGPARAMETER_HH
 #define DUNE_PDELAB_LOCALOPERATOR_STOKESDGPARAMETER_HH
 
+#include "stokesparameter.hh"
+
 namespace Dune {
     namespace PDELab {
-
-        /**
-           These are the boundary condition types as to be returned by
-           the employed boundary type function. 
-
-           Possible types:
-
-           <ul>
-
-           <li>\a DoNothing : Do not evaluate boundary integrals.
-
-           <li>\a VelocityDirichlet : Dirichlet conditions for velocity.
-
-           <li>\a PressureDirichlet : Natural Neumann conditions for the
-           impulse flux. These are equivalent to a fixed pressure
-           condition \b if \f$ \forall i : n \cdot \nabla v_i = 0 \f$.
-
-           </ul>
-         */
-        struct StokesBoundaryCondition {
-            enum Type {
-                DoNothing = 0,
-                VelocityDirichlet = 1,
-                PressureDirichlet = 2,
-                SlipVelocity = 3
-            };
-        };
 
         /** 
             \brief This is the default implementation for the interior
