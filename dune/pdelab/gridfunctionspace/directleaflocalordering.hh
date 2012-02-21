79fa2b7319b2d0a1651893860f0ccdc90b1588e8
Steffen Müthing <muething@dune-project.org>
2012-02-21 21:11:32 +0000

    [Ordering] Add new class DirectLeafLocalOrdering
    DirectLeafLocalOrdering is a special version of a
    local ordering designed for the case where the
    GridViewOrdering is attached to a leaf GridFunctionSpace.
    
    In order to avoid efficiency problems for this common
    scenario, both the GridViewOrdering and the associated
    LocalOrdering differ from the default implementation
    to reduce their memory footprint.
    
    Imported from SVN: r1748 at branches/new_dof_handling

---
+++ dune/pdelab/gridfunctionspace/directleaflocalordering.hh
@@ -0,0 +1,79 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
+
+#ifndef DUNE_PDELAB_GRIDFUNCTIONSPACE_DIRECTLEAFLOCALORDERING_HH
+#define DUNE_PDELAB_GRIDFUNCTIONSPACE_DIRECTLEAFLOCALORDERING_HH
+
+#include <dune/pdelab/gridfunctionspace/orderingutility.hh>
+
+#include <vector>
+
+namespace Dune {
+  namespace PDELab {
+
+    template<typename GFS, typename DI, typename CI>
+    class DirectLeafLocalOrdering
+    {
+
+    public:
+
+      typedef OrderingTraits<DI,CI> Traits;
+
+      void map_local_index(const typename Traits::SizeType geometry_type_index,
+                           const typename Traits::SizeType entity_index,
+                           typename Traits::TreeIndexView mi,
+                           typename Traits::ContainerIndex& ci) const
+      {
+        DUNE_THROW(NotImplemented,"not implemented");
+      }
+
+      typename Traits::SizeType size(const typename Traits::SizeType geometry_type_index, const typename Traits::SizeType entity_index) const
+      {
+        typedef typename Traits::SizeType size_type;
+        if (_fixed_size)
+          return _gt_dof_offsets[geometry_type_index];
+        else if (_gt_used[geometry_type_index])
+          {
+            const size_type index = _gt_entity_offsets[geometry_type_index] + entity_index;
+            return _entity_dof_offsets[index+1] - _entity_dof_offsets[index];
+          }
+        else
+          return 0;
+      }
+
+      typename Traits::SizeType size(const typename Traits::SizeType geometry_type_index, const typename Traits::SizeType entity_index, const typename Traits::SizeType child_index) const
+      {
+        DUNE_THROW(NotImplemented,"not implemented");
+      }
+
+      typename Traits::SizeType offset(const typename Traits::SizeType geometry_type_index, const typename Traits::SizeType entity_index, const typename Traits::SizeType child_index) const
+      {
+        assert(child_index == 0);
+        return 0;
+      }
+
+      DirectLeafLocalOrdering(GFS& gfs)
+        : _gfs(gfs)
+        , _container_blocked(false)
+      {}
+
+    protected:
+
+      const GFS& _gfs;
+      bool _fixed_size;
+      const bool _container_blocked;
+
+      std::vector<bool> _codim_used;
+      std::vector<bool> _gt_used;
+
+      std::vector<typename Traits::SizeType> _gt_entity_offsets;
+      std::vector<typename Traits::SizeType> _gt_dof_offsets;
+      std::vector<typename Traits::SizeType> _entity_dof_offsets;
+
+    };
+
+
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_GRIDFUNCTIONSPACE_DIRECTLEAFLOCALORDERING_HH
