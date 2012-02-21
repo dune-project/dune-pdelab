33596f3d8e6d86cea56f685f244fe9e8ebe06909
Steffen Müthing <muething@dune-project.org>
2012-02-21 21:10:52 +0000

    [Ordering] New header orderingutility.hh with some helper classes
    Imported from SVN: r1730 at branches/new_dof_handling

---
+++ dune/pdelab/gridfunctionspace/orderingutility.hh
@@ -0,0 +1,55 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
+
+#ifndef DUNE_PDELAB_GRIDFUNCTIONSPACE_ORDERINGUTILITY_HH
+#define DUNE_PDELAB_GRIDFUNCTIONSPACE_ORDERINGUTILITY_HH
+
+namespace Dune {
+  namespace PDELab {
+
+    template<typename MI, typename CI>
+    struct OrderingTraits
+    {
+      typedef MI MultiIndex;
+      typedef CI ContainerIndex;
+
+      typedef typename MI::Traits::SizeType SizeType;
+    };
+
+    template<typename MI, typename CI>
+    class VirtualOrderingBase
+    {
+    public:
+
+      typedef OrderingTraits<MI,CI> Traits;
+
+      virtual void map_index(const typename Traits::MultiIndex& mi, typename Traits::ContainerIndex& ci) const = 0;
+    };
+
+
+    template<typename child_type>
+    struct extract_child_bases
+      : public TypeTree::DirectChildrenVisitor
+      , public TypeTree::DynamicTraversal
+    {
+
+      template<typename Node, typename Child, typename TreePath, typename ChildIndex>
+      void afterChild(const Node& node, Child& child, TreePath tp, ChildIndex child_index);
+      {
+        _children[child_index] = &child;
+      }
+
+      extract_child_bases(std::vector<child_type*>& children)
+        : _children(children)
+      {}
+
+    private:
+      std::vector<child_type*> _children;
+
+    };
+
+   //! \} group GridFunctionSpace
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_GRIDFUNCTIONSPACE_ORDERINGUTILITY_HH
