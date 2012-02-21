ce018d30a6ae3f2ca3e47bdc5968e8601961cec8
Steffen Müthing <muething@dune-project.org>
2012-02-21 21:12:38 +0000

    [Ordering] Make entity blocked ordering work with CompositeGridFunctionSpace
    Imported from SVN: r1779 at branches/new_dof_handling

--- dune/pdelab/gridfunctionspace/entityblockedlocalordering.hh
+++ dune/pdelab/gridfunctionspace/entityblockedlocalordering.hh
@@ -200,6 +200,187 @@ namespace Dune {
 
 
 
+
+
+
+
+
+
+
+    //! Interface for merging index spaces
+    template<DUNE_TYPETREE_COMPOSITENODE_TEMPLATE_CHILDREN>
+    class CompositeEntityBlockedLocalOrdering
+      : public DUNE_TYPETREE_COMPOSITENODE_BASETYPE
+      , public LocalOrderingBase<typename DUNE_TYPETREE_COMPOSITENODE_FIRST_CHILD::Traits::GridView,
+                                 typename DUNE_TYPETREE_COMPOSITENODE_FIRST_CHILD::Traits::DOFIndex,
+                                 typename DUNE_TYPETREE_COMPOSITENODE_FIRST_CHILD::Traits::ContainerIndex>
+    {
+
+      typedef DUNE_TYPETREE_COMPOSITENODE_BASETYPE Node;
+      typedef LocalOrderingBase<typename DUNE_TYPETREE_COMPOSITENODE_FIRST_CHILD::Traits::GridView,
+                                typename DUNE_TYPETREE_COMPOSITENODE_FIRST_CHILD::Traits::DOFIndex,
+                                typename DUNE_TYPETREE_COMPOSITENODE_FIRST_CHILD::Traits::ContainerIndex> Base;
+
+    public:
+
+      typedef typename Base::Traits Traits;
+
+      CompositeEntityBlockedLocalOrdering(bool container_blocked, DUNE_TYPETREE_COMPOSITENODE_STORAGE_CONSTRUCTOR_SIGNATURE)
+        : Node(DUNE_TYPETREE_COMPOSITENODE_CHILDVARIABLES)
+        , Base(*this,container_blocked)
+      {}
+
+      const typename Traits::GridView& gridView() const
+      {
+        return this->template child<0>().gridView();
+      }
+
+      //! update internal data structures
+      /**
+       * This method must be called after initialization and every time the
+       * structure of the dof-vector of one of gfs's children changes.  All
+       * the children must have been set up properly before the call to
+       * update().
+       */
+      void update() {
+        /*
+        Dune::dinfo << asImp().name() << ":" << std::endl;
+
+        if(!NonLeafOrderingBase<SizeType, Imp>::fixedSize())
+          DUNE_THROW(InvalidStateException, className<Imp>() << " works "
+                     "only with children that have a uniform size for all "
+                     "entities of a given geometry type/all intersections");
+
+        asImp().sizeCheck();
+
+        printInfo(dinfo);
+        */
+      }
+
+#if 0
+      //! \brief offset of the block of dofs attached to a given entity (of
+      //!        arbitrary codimension)
+      /**
+       * \note This method should be available for all types of entities
+       *       required by the grid.  It is mostly required for
+       *       communcation, so if it is known that this method is not
+       *       actually called for a given entity type the implementation
+       *       may throw NotImplemented.
+       * \note If the grid does not support a given entity type, it may
+       *       still be possible to get this information using
+       *       entityOffset(const Element &e, std::size_t codim, std::size_t
+       *       subentity).
+       *
+       * \throw NotImplemented        If this EntityType is not supported by
+       *                              the ordering.
+       * \throw InvalidStateException If blocked()==false.
+       */
+      template<class Entity>
+      SizeType entityOffset(const Entity &e) const {
+        return asImp().subMap(0,
+                              asImp().template child<0>().entityOffset(e));
+      }
+      //! \brief offset of the blocks of dofs attached to a given subentity
+      //!        of an element
+      /**
+       * This method determines the starting offset of the block of dofs
+       * attached to a subentity of the given codim 0 entity.  If the grid
+       * (and the ordering) directly support entities of the given
+       * codimension, this is equivalent to calling
+       * entityOffset(*e.subEntity<codim>(subentity)).
+       */
+      template<class Element>
+      SizeType entityOffset(const Element &e, std::size_t codim,
+                            std::size_t subentity) const {
+        return asImp().subMap
+          (0, asImp().template child<0>().entityOffset(e, codim, subentity));
+      }
+      //! offset of the block of dofs attached to a given intersection
+      template<class Intersection>
+      SizeType intersectionOffset(const Intersection &i) const {
+        return asImp().subMap
+          (0, asImp().template child<0>().intersectionOffset(i));
+      }
+
+#endif // 0
+
+    };
+
+    template<typename GFS, typename Transformation, typename OrderingTag>
+    struct composite_gfs_to_local_ordering_descriptor;
+
+
+    template<typename GFS, typename Transformation>
+    struct composite_gfs_to_local_ordering_descriptor<GFS,Transformation,EntityBlockedOrderingTag>
+    {
+
+      static const bool recursive = true;
+
+      template<typename... TC>
+      struct result
+      {
+        typedef CompositeEntityBlockedLocalOrdering<TC...> type;
+        typedef shared_ptr<type> storage_type;
+      };
+
+      template<typename... TC>
+      static typename result<TC...>::type transform(const GFS& gfs, const Transformation& t, shared_ptr<TC>... children)
+      {
+        return typename result<TC...>::type(false,children...);
+      }
+
+      template<typename... TC>
+      static typename result<TC...>::storage_type transform_storage(shared_ptr<const GFS> gfs, const Transformation& t, shared_ptr<TC>... children)
+      {
+        return make_shared<typename result<TC...>::type>(false,children...);
+      }
+
+    };
+
+    template<typename GFS, typename Params>
+    composite_gfs_to_local_ordering_descriptor<
+      GFS,
+      gfs_to_local_ordering<Params>,
+      typename GFS::OrderingTag
+      >
+    lookupNodeTransformation(GFS* gfs, gfs_to_local_ordering<Params>* t, CompositeGridFunctionSpaceTag tag);
+
+
+    template<typename GFS, typename Transformation>
+    struct composite_gfs_to_ordering_descriptor<GFS,Transformation,EntityBlockedOrderingTag>
+    {
+
+      static const bool recursive = false;
+
+      typedef TypeTree::TransformTree<GFS,gfs_to_local_ordering<Transformation> > LocalOrderingTransformation;
+      typedef typename LocalOrderingTransformation::Type LocalOrdering;
+
+      typedef GridViewOrdering<LocalOrdering> transformed_type;
+
+      typedef shared_ptr<transformed_type> transformed_storage_type;
+
+      static transformed_type transform(const GFS& gfs, const Transformation& t)
+      {
+        return transformed_type(make_tuple(make_shared<LocalOrdering>(LocalOrderingTransformation::transform(gfs,gfs_to_local_ordering<Transformation>()))),false);
+      }
+
+      static transformed_storage_type transform_storage(shared_ptr<const GFS> gfs, const Transformation& t)
+      {
+        return make_shared<transformed_type>(make_tuple(make_shared<LocalOrdering>(LocalOrderingTransformation::transform_storage(gfs,gfs_to_local_ordering<Transformation>()))),false);
+      }
+
+    };
+
+    template<typename GFS, typename Params>
+    composite_gfs_to_ordering_descriptor<
+      GFS,
+      gfs_to_ordering<Params>,
+      typename GFS::OrderingTag
+      >
+    lookupNodeTransformation(GFS* gfs, gfs_to_ordering<Params>* t, CompositeGridFunctionSpaceTag tag);
+
+
+
 #if 0
     //! Interface for merging index spaces
     template<class SizeType, class Tag, class Child, std::size_t k>
