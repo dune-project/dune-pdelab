2ddfc4a0292b0d756c4af99f7a201b62435c9ec1
Rebecca Neumann <rebecca@dune-project.org>
2012-04-25 12:31:04 +0000

    make path a constructor argument, add optional offset for numbering (e.g. for checkpointing), disable output
    Imported from SVN: r1994 at trunk

--- dune/pdelab/instationary/pvdwriter.hh
+++ dune/pdelab/instationary/pvdwriter.hh
@@ -22,16 +22,18 @@ namespace Dune
         std::string path;
         std::vector<double> timesteps;
         Dune::VTK::OutputType outputtype;
+        unsigned int offset;
 
     public:
 
         PVDWriter(const GridView & gv_, std::string basename_,
                   Dune::VTK::DataMode datamode_ = Dune::VTKOptions::conforming,
-                  Dune::VTK::OutputType outputtype_ = Dune::VTK::appendedraw) :
+                  Dune::VTK::OutputType outputtype_ = Dune::VTK::appendedraw,
+                  std::string path_="vtk", unsigned int offset_=0) :
             VTK(gv_,datamode_), gv(gv_),
             basename(basename_), fn(basename_),
-            path("vtk"),
-            outputtype(outputtype_) {}
+            path(path_), outputtype(outputtype_),
+            offset(offset_){}
 
         void write(double time)
         {
@@ -44,13 +46,13 @@ namespace Dune
             /* write pvd file */
             std::string pvdname = basename + ".pvd";
             std::ofstream pvd(pvdname.c_str());
-            std::cout << "WRITE PVD FILE " << pvdname << std::endl;
+            //std::cout << "WRITE PVD FILE " << pvdname << std::endl;
             assert(pvd.is_open());
             pvd << std::fixed;
             pvd << "<?xml version=\"1.0\"?>\n"
                 << "<VTKFile type=\"Collection\" version=\"0.1\">\n"
                 << "<Collection>\n";
-            PDELab::FilenameHelper fnloop(basename);
+            PDELab::FilenameHelper fnloop(basename,offset);
             for (unsigned int i=0; i<timesteps.size(); i++)
                 {
                     std::string fname = getParallelHeaderName(fnloop.getName(), path, gv.comm().size());
