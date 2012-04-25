d6f1062e6c30a0590af20043c72cb09dce698d50
Rebecca Neumann <rebecca@dune-project.org>
2012-04-25 12:15:31 +0000

    add emacs/vi indentation rules, indent accordingly, whitespace cleanup
    Imported from SVN: r1993 at trunk

--- dune/pdelab/instationary/pvdwriter.hh
+++ dune/pdelab/instationary/pvdwriter.hh
@@ -1,3 +1,5 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=8 sw=2 sts=2:
 #ifndef DUNE_VTK_PVDWRITER_HH
 #define DUNE_VTK_PVDWRITER_HH
 
@@ -20,17 +22,17 @@ namespace Dune
         std::string path;
         std::vector<double> timesteps;
         Dune::VTK::OutputType outputtype;
-        
+
     public:
 
         PVDWriter(const GridView & gv_, std::string basename_,
-            Dune::VTK::DataMode datamode_ = Dune::VTKOptions::conforming,
-            Dune::VTK::OutputType outputtype_ = Dune::VTK::appendedraw) :
+                  Dune::VTK::DataMode datamode_ = Dune::VTKOptions::conforming,
+                  Dune::VTK::OutputType outputtype_ = Dune::VTK::appendedraw) :
             VTK(gv_,datamode_), gv(gv_),
             basename(basename_), fn(basename_),
             path("vtk"),
             outputtype(outputtype_) {}
-        
+
         void write(double time)
         {
             /* remember current time step */
@@ -50,12 +52,12 @@ namespace Dune
                 << "<Collection>\n";
             PDELab::FilenameHelper fnloop(basename);
             for (unsigned int i=0; i<timesteps.size(); i++)
-            {
-                std::string fname = getParallelHeaderName(fnloop.getName(), path, gv.comm().size());
-                pvd << "  <DataSet timestep=\"" << timesteps[i]
-                    << "\" file=\"" << fname << "\"/>\n";
-                fnloop.increment();
-            }
+                {
+                    std::string fname = getParallelHeaderName(fnloop.getName(), path, gv.comm().size());
+                    pvd << "  <DataSet timestep=\"" << timesteps[i]
+                        << "\" file=\"" << fname << "\"/>\n";
+                    fnloop.increment();
+                }
             pvd << "</Collection>\n"
                 << "</VTKFile>\n";
             pvd.close();
@@ -64,7 +66,7 @@ namespace Dune
             fn.increment();
         }
     };
-    
+
 } // end namespace Dune
 
 #endif // DUNE_VTK_PVDWRITER_HH
