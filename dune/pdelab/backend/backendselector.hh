af00f2c40d432e06baf1ac5e278ce6ecc7494bc2
Steffen Müthing <muething@dune-project.org>
2012-05-04 07:48:25 +0000

    [backendvectorselector.hh] Clean up whitespace
    Imported from SVN: r2006 at branches/new_dof_handling

--- dune/pdelab/backend/backendselector.hh
+++ dune/pdelab/backend/backendselector.hh
@@ -2,7 +2,7 @@
 #define DUNE_PDELAB_BACKENDSELECTOR_HH
 namespace Dune {
   namespace PDELab {
-    
+
     template<typename Backend, typename GridFunctionSpace, typename FieldType>
     struct BackendVectorSelectorHelper
     {};
@@ -14,7 +14,7 @@ namespace Dune {
       typedef typename BackendVectorSelectorHelper<Backend, GridFunctionSpace, FieldType>::Type Type;
     };
 
-    
+
   }
 }
 
