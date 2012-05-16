261c903dca1ef7be3526e6caa525614eb8b7effc
Steffen Müthing <muething@dune-project.org>
2012-05-16 09:48:09 +0000

    [ConstraintsParameters] Improve constraints parameters implementation
    Currently, the constraints parameters we ship with PDELab are very bare-bones.
    This patch improves the current situtation in a number of ways:
    
    - Add some documentation.
    - Provide alternative implementations that will always return false for the predicates
      and clearly state that the default implementations will always return true.
    - Add adapter classes that deduce Neumann from Dirichlet conditions and vice versa
      for the common standard case where all boundaries are either Neumann or Dirichlet.
    
    Imported from SVN: r2041 at branches/new_dof_handling

--- dune/pdelab/constraints/constraintsparameters.hh
+++ dune/pdelab/constraints/constraintsparameters.hh
@@ -14,26 +14,243 @@ namespace Dune {
     //! \ingroup PDELab
     //! \{
 
-    //! Interface for the constraints parameters describing dirichlet constraints
+    /**
+     * Interface for the constraints parameters describing Dirichlet constraints.
+     *
+     * \note This class not only describes the required interface of the parameter class,
+     *       but can also be used as a convenient standard implementation that will add
+     *       Dirichlet constraints to all locations it is queried about.
+     */
     struct DirichletConstraintsParameters :
       public TypeTree::LeafNode
     {
+
+      /**
+       * Indicates whether the given position should be Dirichlet-constrained.
+       *
+       * \param intersection The grid intersection containing the queried location.
+       * \param coord        The position of the queried location in local coordinates
+       *                     of the intersection.
+       *
+       * \returns            true iff the given location should have a Dirichlet constraint.
+       */
       template<typename I>
       bool isDirichlet(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
       {
         return true;
       }
+
+      /**
+       * Sets the current time (only required for instationary problems).
+       *
+       * \note This method only needs to be implement for instationary problems.
+       */
+      template<typename T>
+      void setTime(const T& time)
+      {}
+
     };
 
-    //! Interface for the constraints parameters describing flux constraints, e.g. needed for RT0
+    /**
+     * Dirichlet constraints parameters convenience implementation that does not apply
+     * Dirichlet constraints anywhere.
+     *
+     * \sa DirichletConstraintsParameters
+     */
+    struct NoDirichletConstraintsParameters :
+      public TypeTree::LeafNode
+    {
+
+      /**
+       * Predicate implementation that will always return false.
+       */
+      template<typename I>
+      bool isDirichlet(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
+      {
+        return false;
+      }
+
+      /**
+       * Sets the current time (only required for instationary problems).
+       *
+       * \note This method only needs to be implement for instationary problems.
+       */
+      template<typename T>
+      void setTime(const T& time)
+      {}
+
+    };
+
+
+    /**
+     * Interface for the constraints parameters describing flux (Neumann) constraints.
+     *
+     * \note This class not only describes the required interface of the parameter class,
+     *       but can also be used as a convenient standard implementation that will add
+     *       Neumann constraints to all locations it is queried about.
+     */
     struct FluxConstraintsParameters :
       public TypeTree::LeafNode
     {
+
+      /**
+       * Indicates whether the given position should be Neumann-constrained.
+       *
+       * \param intersection The grid intersection containing the queried location.
+       * \param coord        The position of the queried location in local coordinates
+       *                     of the intersection.
+       *
+       * \returns            true iff the given location should have a Neumann constraint.
+       */
       template<typename I>
       bool isNeumann(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
       {
         return true;
       }
+
+      /**
+       * Sets the current time (only required for instationary problems).
+       *
+       * \note This method only needs to be implement for instationary problems.
+       */
+      template<typename T>
+      void setTime(const T& time)
+      {}
+
+    };
+
+    /**
+     * Flux (Neumann) constraints parameters convenience implementation that does not apply
+     * Neumann constraints anywhere.
+     *
+     * \sa FluxConstraintsParameters
+     */
+    struct NoFluxConstraintsParameters :
+      public TypeTree::LeafNode
+    {
+
+      /**
+       * Predicate implementation that will always return false.
+       */
+      template<typename I>
+      bool isNeumann(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
+      {
+        return false;
+      }
+
+      /**
+       * Sets the current time (only required for instationary problems).
+       *
+       * \note This method only needs to be implement for instationary problems.
+       */
+      template<typename T>
+      void setTime(const T& time)
+      {}
+
+    };
+
+    /**
+     * Adapter class that deduces Neumann constraints from a model of DirichletConstraintsParameters
+     * by applying the standard logic that every location on the boundary is either Neumann or
+     * Dirichlet constrained.
+     *
+     * \warning This adapter will only work for the standard case of \f$\partial \Omega = \Gamma_D \cup \Gamma_N$\f.
+     *          Do not try to use it for more advanced problems such as flow with flux, outflow and Dirichlet constraints!
+     *
+     * \tparam DirichletConstraintsParameters The type of the underlying Dirichlet constraints parameters implementation.
+     */
+    template<typename DirichletConstraintsParameters>
+    struct FluxFromDirichletConstraintsAdapter :
+      public TypeTree::LeafNode
+    {
+
+      /**
+       * Forwards to the underlying DirichletConstraintsParameters implementation.
+       */
+      template<typename I>
+      bool isDirichlet(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
+      {
+        return _dirichletConstraintsParameters.isDirichlet(intersection,coord);
+      }
+
+      /**
+       * Returns the opposite of isDirichlet().
+       */
+      template<typename I>
+      bool isNeumann(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
+      {
+        return !_dirichletConstraintsParameters.isDirichlet(intersection,coord);
+      }
+
+      /**
+       * Forwards the new time to the underlying DirichletConstraintsParameters object.
+       */
+      template<typename T>
+      void setTime(const T& time)
+      {
+        _dirichletConstraintsParameters.setTime(time);
+      }
+
+      FluxFromDirichletConstraintsAdapter(DirichletConstraintsParameters& dirichletConstraintsParameters)
+        : _dirichletConstraintsParameters(dirichletConstraintsParameters)
+      {}
+
+    private:
+
+      DirichletConstraintsParameters& _dirichletConstraintsParameters;
+
+    };
+
+    /**
+     * Adapter class that deduces Dirichlet constraints from a model of FluxConstraintsParameters
+     * by applying the standard logic that every location on the boundary is either Neumann or
+     * Dirichlet constrained.
+     *
+     * \warning This adapter will only work for the standard case of \f$\partial \Omega = \Gamma_D \cup \Gamma_N$\f.
+     *          Do not try to use it for more advanced problems such as flow with flux, outflow and Dirichlet constraints!
+     *
+     * \tparam FluxConstraintsParameters The type of the underlying Neumann constraints parameters implementation.
+     */
+    template<typename FluxConstraintsParameters>
+    struct DirichletFromFluxConstraintsAdapter :
+      public TypeTree::LeafNode
+    {
+
+      /**
+       * Returns the opposite of isNeumann().
+       */
+      template<typename I>
+      bool isDirichlet(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
+      {
+        return !_fluxConstraintsParameters.isNeumann(intersection,coord);
+      }
+
+      /**
+       * Forwards to the underlying FluxConstraintsParameters implementation
+       */
+      template<typename I>
+      bool isNeumann(const I & intersection, const FieldVector<typename I::ctype, I::dimension-1> & coord) const
+      {
+        return _fluxConstraintsParameters.isNeumann(intersection,coord);
+      }
+
+      /**
+       * Forwards the new time to the underlying FluxConstraintsParameters object.
+       */
+      template<typename T>
+      void setTime(const T& time)
+      {
+        _fluxConstraintsParameters.setTime(time);
+      }
+
+      DirichletFromFluxConstraintsAdapter(FluxConstraintsParameters& fluxConstraintsParameters)
+        : _fluxConstraintsParameters(fluxConstraintsParameters)
+      {}
+
+    private:
+
+      FluxConstraintsParameters& _fluxConstraintsParameters;
+
     };
 
     //! \}
