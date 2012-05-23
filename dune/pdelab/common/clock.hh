4c161ef3d36395ffd41c0d1060cbaf84350a0dba
Jö Fahlke <joe@dune-project.org>
2012-05-23 11:51:42 +0000

    [clock] Functions to get wall time and process time and a class to store times with nanosecond resolution, as provided by modern systems.
    Note: nanosecond resolution doesn't imply nanosecond accuracy...
    
    Imported from SVN: r2050 at trunk

---
+++ dune/pdelab/common/clock.hh
@@ -0,0 +1,72 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
+
+#ifndef DUNE_PDELAB_COMMON_CLOCK_HH
+#define DUNE_PDELAB_COMMON_CLOCK_HH
+
+#include <ostream>
+
+#include <sys/types.h>
+
+#include <dune/common/exceptions.hh>
+
+namespace Dune {
+  namespace PDELab {
+
+    //! struct to store temporal values
+    struct TimeSpec {
+      //! seconds part
+      time_t tv_sec;
+      //! nanoseconds part
+      /**
+       * \note 0 <= tv_nsec < 1e9 should always hold.
+       */
+      long tv_nsec;
+
+      inline TimeSpec &operator+=(const TimeSpec &o) {
+        tv_sec += o.tv_sec;
+        tv_nsec += o.tv_nsec;
+        if(tv_nsec >= 1000000000L) {
+          ++tv_sec;
+          tv_nsec -= 1000000000L;
+        }
+        return *this;
+      }
+      inline TimeSpec operator+(const TimeSpec &o) const {
+        TimeSpec tmp(*this);
+        tmp += o;
+        return tmp;
+      }
+      inline TimeSpec &operator-=(const TimeSpec &o) {
+        tv_sec -= o.tv_sec;
+        tv_nsec -= o.tv_nsec;
+        if(tv_nsec < 0L) {
+          --tv_sec;
+          tv_nsec += 1000000000L;
+        }
+        return *this;
+      }
+      inline TimeSpec operator-(const TimeSpec &o) const {
+        TimeSpec tmp(*this);
+        tmp -= o;
+        return tmp;
+      }
+
+    };
+
+    //! insert a timespec into an output stream
+    std::ostream &operator<<(std::ostream &s, const TimeSpec &t);
+
+    //! exception thrown by clock functions
+    struct ClockError : Exception {};
+
+    //! get the wall time in seconds since the epoch
+    TimeSpec getWallTime();
+    //! get the cpu time in seconds used by the current process
+    TimeSpec getProcessTime();
+
+  } // namespace PDELab
+} //namespace Dune
+
+#endif // DUNE_PDELAB_COMMON_CLOCK_HH
+
--- m4/dune-pdelab.m4
+++ m4/dune-pdelab.m4
@@ -11,5 +11,7 @@ AC_DEFUN([DUNE_PDELAB_CHECKS],
 # Additional checks needed to find the module
 AC_DEFUN([DUNE_PDELAB_CHECK_MODULE],[
   AC_MSG_NOTICE([Searching for dune-pdelab...])
-  DUNE_CHECK_MODULES([dune-pdelab], [pdelab/common/function.hh])
+  DUNE_CHECK_MODULES([dune-pdelab], [pdelab/common/clock.hh],[dnl
+    return Dune::PDELab::getWallTime().tv_sec;
+  ])
 ])
