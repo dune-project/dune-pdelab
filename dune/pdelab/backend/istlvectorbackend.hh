b7ab0d43db125a5c18ab94fb6f1f9049ba23ff5a
Steffen Müthing <muething@dune-project.org>
2012-06-13 12:46:46 +0000

    [ISTLVectorBackend] Rewrite the ISTL vector type deduction TMP
    The TMP that deduces the ISTL vector structure from the GFS tree is
    not really working for complex scenarios with static blocking.
    
    This patch is a complete rewrite of the existing implementation and
    implements the following behavior:
    
    - Backends for leaf spaces *can* specify a static block size as second
      template parameter, even if there should be no blocking at the leaf
      level (first parameter is no_blocking). Right now, this is the only way
      to tell the algorithm about the desired block size of the leaf spaces.
      In a later iteration, it should be possibly to extract this information
      directly from the FiniteElementMaps (with a suitably extended interface).
    
    - static_blocking may appear exactly once on each path from a leaf space
      to the root (this is due to restrictions in ISTL). If the static_blocking appears
      at a non-leaf level, the algorithm will automatically figure out the correct
      static block size from the information in the leafs.
    
    - In all other cases, the algorithm makes sure that all children of a node
      expect an identical blocking structure (again, ISTL restrictions).
    
    - It is possible to specify dynamic_blocking multiple times on a path from
      a leaf to the root, but only after an (optional) static_blocking.
    
    Imported from SVN: r2094 at branches/new_dof_handling

--- dune/pdelab/backend/istlvectorbackend.hh
+++ dune/pdelab/backend/istlvectorbackend.hh
@@ -34,8 +34,6 @@ namespace Dune {
       typedef istl_vector_backend_tag tag;
 
       dune_static_assert((block_size_ > 0),"block size for FieldVector has to be positive");
-      dune_static_assert((blocking == ISTLParameters::static_blocking || block_size_ == 1),
-                         "Explicitly specified block size only supported for static blocking");
 
       typedef std::size_t size_type;
 
@@ -53,112 +51,6 @@ namespace Dune {
       {
         return Traits::blocked;
       }
-    };
-
-    template<typename E,typename Node, typename Tag>
-    struct extract_istl_vector_helper
-    {
-      // export node type, as the actual calculation takes place in the reduction functor
-      typedef Node type;
-    };
-
-    struct LeafGridFunctionSpaceTag;
-
-    template<typename E, typename Node>
-    struct extract_istl_vector_helper<E,Node,LeafGridFunctionSpaceTag>
-    {
-      typedef BlockVector<FieldVector<E,Node::Traits::Backend::blockSize> > type;
-    };
-
-    template<typename E>
-    struct extract_istl_vector
-    {
-
-      template<typename Node, typename TreePath>
-      struct doVisit
-      {
-        static const bool value = true;
-      };
-
-      template<typename Node, typename TreePath>
-      struct visit
-      {
-        typedef typename extract_istl_vector_helper<E,Node,typename Node::ImplementationTag>::type type;
-      };
-
-    };
-
-    struct combine_istl_vector_siblings
-    {
-
-      template<typename V1, typename V2>
-      struct reduce
-      {
-        dune_static_assert((is_same<V1,V2>::value) || // identical child types
-                           (is_same<V1,void>::value), // special case to catch start type
-                           "This backend only supports sub-vectors of identical type.");
-
-        typedef V2 type;
-      };
-
-    };
-
-    template<typename ChildContainer, typename ParentNode, typename ParentBackendTag, ISTLParameters::Blocking blocking>
-    struct istl_vector_parent_helper
-    {
-
-      dune_static_assert((Dune::AlwaysFalse<ChildContainer>::value),
-                         "unsupported combination of child container and parent backend.");
-
-    };
-
-
-    template<typename ChildContainer, typename ParentNode>
-    struct istl_vector_parent_helper<ChildContainer,ParentNode,istl_vector_backend_tag,ISTLParameters::dynamic_blocking>
-    {
-
-      typedef BlockVector<ChildContainer> type;
-
-    };
-
-    template<typename ChildContainer, typename ParentNode>
-    struct istl_vector_parent_helper<ChildContainer,ParentNode,istl_vector_backend_tag,ISTLParameters::no_blocking>
-    {
-
-      typedef ChildContainer type;
-
-    };
-
-
-  template<typename ChildContainer, typename ParentNode>
-  struct istl_vector_parent_helper<ChildContainer,ParentNode,istl_vector_backend_tag,ISTLParameters::static_blocking>
-    {
-
-      dune_static_assert(ChildContainer::block_type::dimension == 1,"currently only nesting for scalar child blocks is supported");
-      dune_static_assert(ParentNode::Traits::Backend::blockSize == ParentNode::CHILDREN,"parent block size must match number of children");
-
-      typedef BlockVector<FieldVector<typename ChildContainer::field_type,ParentNode::CHILDREN> > type;
-
-    };
-
-
-
-    struct combine_istl_vector_parent
-    {
-
-      template<typename ChildContainer, typename Node>
-      struct reduce
-      {
-
-        typedef typename istl_vector_parent_helper<
-          ChildContainer,
-          Node,
-          typename Node::Traits::Backend::tag,
-          Node::Traits::Backend::Traits::block_type
-          >::type type;
-
-      };
-
     };
 
 
@@ -748,19 +640,285 @@ namespace Dune {
     };
 
 
+    namespace istl {
+
+      // TMPs for deducing ISTL block structure from GFS backends
+
+      // tag dispatch switch on GFS tag for per-node functor - general version
+      template<typename E,typename Node, typename Tag>
+      struct vector_descriptor_helper
+      {
+        // export backend type, as the actual TMP is in the parent reduction functor
+        typedef typename Node::Traits::Backend type;
+      };
+
+      // descriptor for backends of leaf spaces collecting various information about
+      // possible blocking structures
+      template<typename E, typename Backend>
+      struct leaf_vector_descriptor
+      {
+
+        dune_static_assert(Backend::Traits::block_type != ISTLParameters::dynamic_blocking,
+                           "Dynamcially blocked leaf spaces are not supported by this backend.");
+
+        // flag for sibling reduction - always true in the leaf case
+        static const bool support_no_blocking = true;
+
+        // flag indicating whether the associated vector type supports cascading
+        // the static blocking further up the tree (i.e. create larger static blocks
+        // at the parent node level. Due to ISTL limitations, this only works once in
+        // the hierarchy, so we only support cascading if we don't already do static
+        // blocking at the current level.
+        static const bool support_cascaded_blocking =
+          Backend::Traits::block_type == ISTLParameters::no_blocking; // FIXME
+
+        // The static block size of the associated vector
+        static const std::size_t block_size =
+          Backend::Traits::block_type == ISTLParameters::static_blocking
+          ? Backend::Traits::block_size
+          : 1;
+
+        // The cumulative block size is used by the algorithm to calculate total block
+        // size over several children for cascaded blocking. Right now, this will always be set to
+        // the block size passed in by the user, but it might also be possible to provide this
+        // information in the FiniteElementMap and provide automatic blocking detection.
+        static const std::size_t cumulative_block_size = Backend::Traits::block_size;
+
+        // The element type for the vector.
+        typedef E element_type;
+
+        // The ISTL vector type associated with the current subtree.
+        typedef BlockVector<FieldVector<E,block_size> > vector_type;
+
+      };
+
+      // Tag dispatch for leaf spaces - extract leaf descriptor.
+      template<typename E, typename Node>
+      struct vector_descriptor_helper<E,Node,LeafGridFunctionSpaceTag>
+      {
+        typedef leaf_vector_descriptor<E,typename Node::Traits::Backend> type;
+      };
+
+      // the actual functor
+      template<typename E>
+      struct extract_vector_descriptor
+      {
+
+        template<typename Node, typename TreePath>
+        struct doVisit
+        {
+          // visit all nodes
+          static const bool value = true;
+        };
+
+        template<typename Node, typename TreePath>
+        struct visit
+        {
+          // forward to actual implementation via tag dispatch
+          typedef typename vector_descriptor_helper<E,Node,typename Node::ImplementationTag>::type type;
+        };
+
+      };
+
+      // Descriptor for combining sibling nodes in the tree
+      template<typename Sibling, typename Child>
+      struct cascading_vector_descriptor
+      {
+
+        // We only support cascaded blocking if all children support it
+        static const bool support_cascaded_blocking =
+          Sibling::support_cascaded_blocking &&
+          Child::support_cascaded_blocking;
+
+        // ISTL requires a single, globally constant blocking structure
+        // for its containers, so we make sure the siblings don't disagree
+        // on it.
+        static const bool support_no_blocking =
+          (Sibling::support_no_blocking &&
+          is_same<
+            typename Sibling::vector_type,
+            typename Child::vector_type
+           >::value);
+
+        // block size
+        static const std::size_t block_size =
+          support_no_blocking ? Sibling::block_size : 1;
+
+        // The element type for the vector.
+        typedef typename Sibling::element_type element_type;
+
+        // Accumulate total block size of all siblings
+        static const std::size_t cumulative_block_size =
+          Sibling::cumulative_block_size + Child::cumulative_block_size;
+
+        // The ISTL vector type associated with the current subtree.
+        typedef BlockVector<FieldVector<element_type,block_size> > vector_type;
+
+      };
+
+
+      // Switch that turns off standard reduction for the first child of a node.
+      // Default case: do the standard reduction.
+      template<typename D1, typename D2>
+      struct initial_reduction_switch
+      {
+        typedef cascading_vector_descriptor<D1,D2> type;
+      };
+
+      // specialization for first child
+      template<typename D2>
+      struct initial_reduction_switch<void,D2>
+      {
+        typedef D2 type;
+      };
+
+      // sibling reduction functor
+      struct combine_vector_descriptor_siblings
+      {
+
+        template<typename D1, typename D2>
+        struct reduce
+          : public initial_reduction_switch<D1,D2>
+        {};
+
+      };
+
+      // Data part of child -> parent reduction descriptor
+      template<typename Child, typename Backend>
+      struct parent_child_vector_descriptor_data
+      {
+
+        // If all our have a common blocking structure, we can just
+        // concatenate them without doing any blocking
+        static const bool support_no_blocking =
+          Child::support_no_blocking;
+
+        // We support cascaded blocking if neither we nor any of our
+        // children are blocked yet.
+        static const bool support_cascaded_blocking =
+          Child::support_cascaded_blocking &&
+          Backend::Traits::block_type == ISTLParameters::no_blocking;
+
+        // Throw an assertion if the user requests static blocking at this level,
+        // but we cannot support it.
+        dune_static_assert((Backend::Traits::block_type != ISTLParameters::static_blocking) ||
+                           Child::support_cascaded_blocking,
+                           "invalid blocking structure.");
+
+        // If we block statically, we create bigger blocks, otherwise the
+        // block size doesn't change.
+        static const std::size_t block_size =
+          Backend::Traits::block_type == ISTLParameters::static_blocking
+          ? Child::cumulative_block_size
+          : Child::block_size;
+
+        // Just forward this...
+        static const std::size_t cumulative_block_size =
+          Child::cumulative_block_size;
+
+        // The element type for the vector.
+        typedef typename Child::element_type element_type;
+
+        // The ISTL vector type associated with our subtrees.
+        typedef typename Child::vector_type child_vector_type;
+
+      };
+
+      // dispatch switch on blocking type - prototype
+      template<typename Data, ISTLParameters::Blocking>
+      struct parent_child_vector_descriptor;
+
+      // dispatch switch on blocking type - no blocking case
+      template<typename Data>
+      struct parent_child_vector_descriptor<
+        Data,
+        ISTLParameters::no_blocking
+        >
+        : public Data
+      {
+        dune_static_assert(Data::support_no_blocking,
+                           "Cannot combine incompatible child block structures without static blocking. "
+                           "Did you want to apply static blocking at this level?");
+
+        // Just forward the child vector type
+        typedef typename Data::child_vector_type vector_type;
+      };
+
+      // dispatch switch on blocking type - dynamic blocking case
+      template<typename Data>
+      struct parent_child_vector_descriptor<
+        Data,
+        ISTLParameters::dynamic_blocking
+        >
+        : public Data
+      {
+        dune_static_assert(Data::support_no_blocking,
+                           "Incompatible child block structures detected, cannot perform dynamic blocking. "
+                           "Did you want to apply static blocking at this level?");
+
+        // Wrap the child vector type in another BlockVector
+        typedef BlockVector<typename Data::child_vector_type> vector_type;
+      };
+
+      // dispatch switch on blocking type - static blocking case
+      template<typename Data>
+      struct parent_child_vector_descriptor<
+        Data,
+        ISTLParameters::static_blocking
+        >
+        : public Data
+      {
+        // build new block vector with large field block size
+        typedef BlockVector<
+          FieldVector<
+            typename Data::element_type,
+            Data::block_size
+            >
+          > vector_type;
+      };
+
+      // Child - parent reduction functor
+      struct combine_vector_descriptor_parent
+      {
+
+        template<typename Child, typename Backend>
+        struct reduce
+        {
+
+          struct type
+            : public parent_child_vector_descriptor<parent_child_vector_descriptor_data<
+                                                      Child,
+                                                      Backend>,
+                                                    Backend::Traits::block_type
+                                                    >
+          {};
+        };
+
+      };
+
+      // policy describing the GFS tree -> ISTL vector reduction
+      template<typename E>
+      struct vector_creation_policy
+        : public TypeTree::TypeAccumulationPolicy<extract_vector_descriptor<E>,
+                                                  combine_vector_descriptor_siblings,
+                                                  void,
+                                                  combine_vector_descriptor_parent,
+                                                  TypeTree::bottom_up_reduction>
+      {};
+
+    } // namespace istl
+
+    // helper struct invoking the GFS tree -> ISTL vector reduction
     template<typename GFS, typename E>
     struct ISTLVectorSelectorHelper
     {
 
       typedef typename TypeTree::AccumulateType<
         GFS,
-        extract_istl_vector<E>,
-        combine_istl_vector_siblings,
-        void,
-        combine_istl_vector_parent
-        >::type istl_container_type;
+        istl::vector_creation_policy<E>
+        >::type vector_descriptor;
 
-      typedef ISTLBlockVectorContainer<GFS,istl_container_type> Type;
+      typedef ISTLBlockVectorContainer<GFS,typename vector_descriptor::vector_type> Type;
 
     };
 
