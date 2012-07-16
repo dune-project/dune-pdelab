1f0d8d680175fb2842f4ee49893519002e5da758
Steffen Müthing <muething@dune-project.org>
2012-07-16 14:00:28 +0200

    [DataHandle] Port data handles to new API and clean up implementation
    This patch ports the data handle support to the new index + backend APIs. I've
    also seized the opportunity and done a major cleanup of this code to reduce code
    duplication and get rid of unintuitive naming conventions like GenericDataHandle[23]? ,
    which do not convey at all what they do.
    
    In particular, this patch does the following:
    
    - Replace the diverse GenericDataHandle classes with a single GFSDataHandle that should
      be used for all communication involving a GridFunctionSpace. This class takes care of
      index lookup and data loading, something we don't want to repeat in several classes, as
      it has become more involved than it used to be (setup index cache, bind local vector view,
      commit data...). This data handle is templated on a GatherScatter functor that is passed the
      buffer, the entity and the ready-to-use local vector view (and the transmission size in case
      of scatter() ). It is the responsibility of that functor to actually iterate over the data
      for the entity.
    
    - In order to free the user from having to do the iteration, there are three new classes
      DataGatherScatter, DataEntityGatherScatter and DataContainerIndexGatherScatter, which are templated
      on yet another functor and which iterate over the data bound to the entity and call the inner functor
      for each entry. They differ in what parameters they pass on to the inner container (their names should
      be a good clue ;-) ) and when used together with the new GFSDataHandle, largely correspond to the
      old GenericDataHandle family of classes.
    
    - The old GenericDataHandle classes are gone without replacement. There names were just too unintuitive
      for my taste.
    
    - All the readymade handles for adding, copying, ... data are still there and work the way they used to,
      but are now implemented in terms of GFSDataHandle + one of the three generic GatherScatter functors.

--- dune/pdelab/gridfunctionspace/genericdatahandle.hh
+++ dune/pdelab/gridfunctionspace/genericdatahandle.hh
@@ -9,232 +9,195 @@
 #include <dune/grid/common/datahandleif.hh>
 #include <dune/grid/common/gridenums.hh>
 
+#include <dune/pdelab/gridfunctionspace/entitycontainerindexcache.hh>
+
 namespace Dune {
   namespace PDELab {
 
-	/// \brief implement a data handle with a grid function space
-	/// \tparam GFS a grid function space
-	/// \tparam V   a vector container associated with the GFS
-	/// \tparam T   gather/scatter methods with argumemts buffer, and data
-	template<class GFS, class V, class T, class E=typename V::ElementType>
-	class GenericDataHandle
-	  : public Dune::CommDataHandleIF<GenericDataHandle<GFS,V,T>,E>
-	{
-	  typedef typename GFS::Traits::BackendType B;
 
-	public:
-	  GenericDataHandle (const GFS& gfs_, V& v_, T t_) 
-		: gfs(gfs_), v(v_), t(t_), global(gfs.maxLocalSize())
-	  {}
+    /// \brief implement a data handle with a grid function space
+    /// \tparam GFS a grid function space
+    /// \tparam V   a vector container associated with the GFS
+    /// \tparam T   gather/scatter methods with argumemts buffer, and data
+    template<typename GFS, typename V, typename GatherScatter, typename E = typename V::ElementType>
+    class GFSDataHandle
+      : public Dune::CommDataHandleIF<GFSDataHandle<GFS,V,GatherScatter,E>,E>
+    {
 
-	  //!  \brief export type of data for message buffer
-	  typedef typename V::ElementType DataType;
+    public:
 
-	  //! returns true if data for this codim should be communicated
-	  bool contains (int dim, int codim) const
-	  {
-		return gfs.dataHandleContains(dim,codim);
-	  }
+      typedef E DataType;
+      typedef typename GFS::Traits::SizeType size_type;
 
-	  //!  \brief returns true if size per entity of given dim and codim is a constant
-	  bool fixedsize (int dim, int codim) const
-	  {
-		return gfs.dataHandleFixedSize(dim,codim);
-	  }
+      GFSDataHandle(const GFS& gfs, V& v, GatherScatter gather_scatter)
+        : _gfs(gfs)
+        , _index_cache(gfs)
+        , _local_view(v)
+        , _gather_scatter(gather_scatter)
+      {}
 
-	  /*!  \brief how many objects of type DataType have to be sent for a given entity
+      //! returns true if data for this codim should be communicated
+      bool contains(int dim, int codim) const
+      {
+        return _gfs.dataHandleContains(dim,codim);
+      }
 
-		Note: Only the sender side needs to know this size. 
-	  */
-	  template<class EntityType>
-	  size_t size (EntityType& e) const
-	  {
-		return gfs.dataHandleSize(e);
-	  }
+      //!  \brief returns true if size per entity of given dim and codim is a constant
+      bool fixedsize(int dim, int codim) const
+      {
+        return _gfs.dataHandleFixedSize(dim,codim);
+      }
 
-	  //! \brief pack data from user to message buffer
-	  template<class MessageBuffer, class EntityType>
-	  void gather (MessageBuffer& buff, const EntityType& e) const
-	  {
-		gfs.dataHandleGlobalIndices(e,global);
-		for (size_t i=0; i<global.size(); ++i)
-		  t.gather(buff,B::access(v,global[i]));
-	  }
+      /*!  \brief how many objects of type DataType have to be sent for a given entity
 
-	  /*! \brief unpack data from message buffer to user
+        Note: Only the sender side needs to know this size.
+      */
+      template<typename Entity>
+      size_type size(Entity& e) const
+      {
+        return _gfs.dataHandleSize(e);
+      }
 
-		n is the number of objects sent by the sender
-	  */
-	  template<class MessageBuffer, class EntityType>
-	  void scatter (MessageBuffer& buff, const EntityType& e, size_t n)
-	  {
-		gfs.dataHandleGlobalIndices(e,global);
-		if (global.size()!=n)
+      //! \brief pack data from user to message buffer
+      template<typename MessageBuffer, typename Entity>
+      void gather(MessageBuffer& buff, const Entity& e) const
+      {
+        _index_cache.update(e);
+        _local_view.bind(_index_cache);
+        _gather_scatter.gather(buff,e,_local_view);
+        _local_view.unbind();
+      }
+
+      /*! \brief unpack data from message buffer to user
+
+        n is the number of objects sent by the sender
+      */
+      template<typename MessageBuffer, typename Entity>
+      void scatter(MessageBuffer& buff, const Entity& e, size_type n)
+      {
+        _index_cache.update(e);
+        _local_view.bind(_index_cache);
+        _gather_scatter.scatter(buff,n,e,_local_view);
+        _local_view.commit();
+        _local_view.unbind();
+      }
+
+    private:
+
+      typedef EntityContainerIndexCache<GFS> IndexCache;
+      typedef typename V::template LocalView<IndexCache> LocalView;
+
+      const GFS& _gfs;
+      mutable IndexCache _index_cache;
+      mutable LocalView _local_view;
+      mutable GatherScatter _gather_scatter;
+
+    };
+
+
+    template<typename GatherScatter>
+    class DataGatherScatter
+    {
+
+    public:
+
+      typedef std::size_t size_type;
+
+      template<typename MessageBuffer, typename Entity, typename LocalView>
+      void gather(MessageBuffer& buff, const Entity& e, const LocalView& local_view) const
+      {
+        for (std::size_t i = 0; i < local_view.size(); ++i)
+          _gather_scatter.gather(buff,local_view[i]);
+      }
+
+      template<typename MessageBuffer, typename Entity, typename LocalView>
+      void scatter(MessageBuffer& buff, size_type n, const Entity& e, LocalView& local_view) const
+      {
+        if (local_view.size() != n)
           DUNE_THROW(Exception,"size mismatch in generic data handle");
-		for (size_t i=0; i<global.size(); ++i)
-		  t.scatter(buff,B::access(v,global[i]));
-	  }
+        for (std::size_t i = 0; i < local_view.size(); ++i)
+          _gather_scatter.scatter(buff,local_view[i]);
+      }
 
-	private:
-	  const GFS& gfs;
-	  V& v;
-	  mutable T t;
-	  mutable std::vector<typename GFS::Traits::SizeType> global;
-	};
+      DataGatherScatter(GatherScatter gather_scatter)
+        : _gather_scatter(gather_scatter)
+      {}
+
+    private:
+
+      GatherScatter _gather_scatter;
+
+    };
 
 
-	/// \brief implements a data handle with a grid function space
-    ///
-    /// The difference to GenericDataHandle is that
-    /// the signature of the gather and scatter methods called on 
-    /// on the underlying GatherScatter is different. Namely, the entity is
-    /// an additional argument in the function call
-    ///
-	/// \tparam GFS a grid function space
-	/// \tparam  V a vector container associated with the GFS
-	/// \tparam T type of the functor with  gather/scatter methods  with argumemts buffer, entity, and data.
-	template<class GFS, class V, class T>
-	class GenericDataHandle2
-	  : public Dune::CommDataHandleIF<GenericDataHandle2<GFS,V,T>,typename V::ElementType>
-	{
-	  typedef typename GFS::Traits::BackendType B;
+    template<typename GatherScatter>
+    class DataEntityGatherScatter
+    {
 
-	public:
-	  GenericDataHandle2 (const GFS& gfs_, V& v_, T t_) 
-		: gfs(gfs_), v(v_), t(t_), global(gfs.maxLocalSize())
-	  {}
+    public:
 
-	  //! \brief export type of data for message buffer
-	  typedef typename V::ElementType DataType;
+      typedef std::size_t size_type;
 
-	  //! \brief returns true if data for this codim should be communicated
-	  bool contains (int dim, int codim) const
-	  {
-		return gfs.dataHandleContains(dim,codim);
-	  }
+      template<typename MessageBuffer, typename Entity, typename LocalView>
+      void gather(MessageBuffer& buff, const Entity& e, const LocalView& local_view) const
+      {
+        for (std::size_t i = 0; i < local_view.size(); ++i)
+          _gather_scatter.gather(buff,e,local_view[i]);
+      }
 
-	  //! \brief returns true if size per entity of given dim and codim is a constant
-	  bool fixedsize (int dim, int codim) const
-	  {
-		return gfs.dataHandleFixedSize(dim,codim);
-	  }
-
-	  /*! \brief how many objects of type DataType have to be sent for a given entity
-
-		Note: Only the sender side needs to know this size. 
-	  */
-	  template<class EntityType>
-	  size_t size (EntityType& e) const
-	  {
-		return gfs.dataHandleSize(e);
-	  }
-
-	  //! \brief pack data from user to message buffer
-	  template<class MessageBuffer, class EntityType>
-	  void gather (MessageBuffer& buff, const EntityType& e) const
-	  {
-		gfs.dataHandleGlobalIndices(e,global);
-		for (size_t i=0; i<global.size(); ++i)
-		  t.gather(buff,e,B::access(v,global[i]));
-	  }
-
-	  /*! \brief unpack data from message buffer to user
-
-		n is the number of objects sent by the sender
-	  */
-	  template<class MessageBuffer, class EntityType>
-	  void scatter (MessageBuffer& buff, const EntityType& e, size_t n)
-	  {
-		gfs.dataHandleGlobalIndices(e,global);
-		if (global.size()!=n)
+      template<typename MessageBuffer, typename Entity, typename LocalView>
+      void scatter(MessageBuffer& buff, size_type n, const Entity& e, LocalView& local_view) const
+      {
+        if (local_view.size() != n)
           DUNE_THROW(Exception,"size mismatch in generic data handle");
-		for (size_t i=0; i<global.size(); ++i)
-		  t.scatter(buff,e,B::access(v,global[i]));
-	  }
+        for (std::size_t i = 0; i < local_view.size(); ++i)
+          _gather_scatter.scatter(buff,e,local_view[i]);
+      }
 
-	private:
-	  const GFS& gfs;
-	  V& v;
-	  mutable T t;
-	  mutable std::vector<typename GFS::Traits::SizeType> global;
-	};
+      DataEntityGatherScatter(GatherScatter gather_scatter)
+        : _gather_scatter(gather_scatter)
+      {}
 
-	/// \brief implements a data handle with a grid function space
-    ///
-    /// The difference to GenericDataHandle is that
-    /// the signature of the gather and scatter methods called on 
-    /// on the underlying GatherScatter is different. Namely, the entity is
-    /// an additional argument in the function call
-    ///
-	/// \tparam GFS a grid function space
-	/// \tparam  V a vector container associated with the GFS
-	/// \tparam T type of the functor with  gather/scatter methods  with argumemts buffer, entity, and data.
-	template<class GFS, class V, class T>
-	class GenericDataHandle3
-	  : public Dune::CommDataHandleIF<GenericDataHandle3<GFS,V,T>,typename V::ElementType>
-	{
-	  typedef typename GFS::Traits::BackendType B;
+    private:
 
-	public:
-	  GenericDataHandle3 (const GFS& gfs_, V& v_, T t_) 
-		: gfs(gfs_), v(v_), t(t_), global(gfs.maxLocalSize())
-	  {}
+      GatherScatter _gather_scatter;
 
-	  //! \brief export type of data for message buffer
-	  typedef typename V::ElementType DataType;
+    };
 
-	  //! \brief returns true if data for this codim should be communicated
-	  bool contains (int dim, int codim) const
-	  {
-		return gfs.dataHandleContains(dim,codim);
-	  }
 
-	  //! \brief returns true if size per entity of given dim and codim is a constant
-	  bool fixedsize (int dim, int codim) const
-	  {
-		return gfs.dataHandleFixedSize(dim,codim);
-	  }
+    template<typename GatherScatter>
+    class DataContainerIndexGatherScatter
+    {
 
-	  /*! \brief how many objects of type DataType have to be sent for a given entity
+    public:
 
-		Note: Only the sender side needs to know this size. 
-	  */
-	  template<class EntityType>
-	  size_t size (EntityType& e) const
-	  {
-		return gfs.dataHandleSize(e);
-	  }
+      typedef std::size_t size_type;
 
-	  //! \brief pack data from user to message buffer
-	  template<class MessageBuffer, class EntityType>
-	  void gather (MessageBuffer& buff, const EntityType& e) const
-	  {
-		gfs.dataHandleGlobalIndices(e,global);
-		for (size_t i=0; i<global.size(); ++i)
-		  t.gather(buff,global[i],B::access(v,global[i]));
-	  }
+      template<typename MessageBuffer, typename Entity, typename LocalView>
+      void gather(MessageBuffer& buff, const Entity& e, const LocalView& local_view) const
+      {
+        for (std::size_t i = 0; i < local_view.size(); ++i)
+          _gather_scatter.gather(buff,local_view.cache().container_index(i),local_view[i]);
+      }
 
-	  /*! \brief unpack data from message buffer to user
-
-		n is the number of objects sent by the sender
-	  */
-	  template<class MessageBuffer, class EntityType>
-	  void scatter (MessageBuffer& buff, const EntityType& e, size_t n)
-	  {
-		gfs.dataHandleGlobalIndices(e,global);
-		if (global.size()!=n)
+      template<typename MessageBuffer, typename Entity, typename LocalView>
+      void scatter(MessageBuffer& buff, size_type n, const Entity& e, LocalView& local_view) const
+      {
+        if (local_view.size() != n)
           DUNE_THROW(Exception,"size mismatch in generic data handle");
-		for (size_t i=0; i<global.size(); ++i)
-		  t.scatter(buff,global[i],B::access(v,global[i]));
-	  }
+        for (std::size_t i = 0; i < local_view.size(); ++i)
+          _gather_scatter.scatter(buff,local_view.cache().container_index(i),local_view[i]);
+      }
 
-	private:
-	  const GFS& gfs;
-	  V& v;
-	  mutable T t;
-	  mutable std::vector<typename GFS::Traits::SizeType> global;
-	};
+      DataContainerIndexGatherScatter(GatherScatter gather_scatter)
+        : _gather_scatter(gather_scatter)
+      {}
 
+    private:
+
+      GatherScatter _gather_scatter;
+
+    };
 
 
 	class AddGatherScatter
@@ -254,17 +217,17 @@ namespace Dune {
 		data += x;
 	  }
 	};
-	
-	template<class GFS, class V, class E=typename V::ElementType>
+
+    template<class GFS, class V>
 	class AddDataHandle
-	  : public GenericDataHandle<GFS,V,AddGatherScatter,E>
+      : public GFSDataHandle<GFS,V,DataGatherScatter<AddGatherScatter> >
 	{
-	  typedef GenericDataHandle<GFS,V,AddGatherScatter,E> BaseT;
+      typedef GFSDataHandle<GFS,V,DataGatherScatter<AddGatherScatter> > BaseT;
 
 	public:
 
-	  AddDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,AddGatherScatter())
+      AddDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataGatherScatter<AddGatherScatter>(AddGatherScatter()))
 	  {}
 	};
 
@@ -289,14 +252,14 @@ namespace Dune {
 	
 	template<class GFS, class V>
 	class AddClearDataHandle
-	  : public GenericDataHandle<GFS,V,AddClearGatherScatter>
+      : public GFSDataHandle<GFS,V,DataGatherScatter<AddClearGatherScatter> >
 	{
-	  typedef GenericDataHandle<GFS,V,AddClearGatherScatter> BaseT;
+      typedef GFSDataHandle<GFS,V,DataGatherScatter<AddClearGatherScatter> > BaseT;
 
 	public:
 
-	  AddClearDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,AddClearGatherScatter())
+      AddClearDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataGatherScatter<AddClearGatherScatter>(AddClearGatherScatter()))
 	  {}
 	};
 
@@ -320,14 +283,14 @@ namespace Dune {
 	
 	template<class GFS, class V>
 	class CopyDataHandle
-	  : public GenericDataHandle<GFS,V,CopyGatherScatter>
+      : public GFSDataHandle<GFS,V,DataGatherScatter<CopyGatherScatter> >
 	{
-	  typedef GenericDataHandle<GFS,V,CopyGatherScatter> BaseT;
+      typedef GFSDataHandle<GFS,V,DataGatherScatter<CopyGatherScatter >> BaseT;
 
 	public:
 
-	  CopyDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,CopyGatherScatter())
+      CopyDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataGatherScatter<CopyGatherScatter>(CopyGatherScatter()))
 	  {}
 	};
 
@@ -351,14 +314,14 @@ namespace Dune {
 	
 	template<class GFS, class V>
 	class MinDataHandle
-	  : public GenericDataHandle<GFS,V,MinGatherScatter>
+      : public GFSDataHandle<GFS,V,DataGatherScatter<MinGatherScatter> >
 	{
-	  typedef GenericDataHandle<GFS,V,MinGatherScatter> BaseT;
+      typedef GFSDataHandle<GFS,V,DataGatherScatter<MinGatherScatter> > BaseT;
 
 	public:
 
-	  MinDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,MinGatherScatter())
+      MinDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataGatherScatter<MinGatherScatter>(MinGatherScatter()))
 	  {}
 	};
 
@@ -382,14 +345,14 @@ namespace Dune {
 	
 	template<class GFS, class V>
 	class MaxDataHandle
-	  : public GenericDataHandle<GFS,V,MaxGatherScatter>
+      : public GFSDataHandle<GFS,V,DataGatherScatter<MaxGatherScatter> >
 	{
-	  typedef GenericDataHandle<GFS,V,MaxGatherScatter> BaseT;
+      typedef GFSDataHandle<GFS,V,DataGatherScatter<MaxGatherScatter> > BaseT;
 
 	public:
 
-	  MaxDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,MaxGatherScatter())
+      MaxDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataGatherScatter<MaxGatherScatter>(MaxGatherScatter()))
 	  {}
 	};
 
@@ -420,14 +383,14 @@ namespace Dune {
 
 	template<class GFS, class V>
 	class PartitionDataHandle
-	  : public GenericDataHandle2<GFS,V,PartitionGatherScatter>
+      : public GFSDataHandle<GFS,V,DataEntityGatherScatter<PartitionGatherScatter> >
 	{
-	  typedef GenericDataHandle2<GFS,V,PartitionGatherScatter> BaseT;
+      typedef GFSDataHandle<GFS,V,DataEntityGatherScatter<PartitionGatherScatter> > BaseT;
 
 	public:
 
-	  PartitionDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,PartitionGatherScatter())
+      PartitionDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataEntityGatherScatter<PartitionGatherScatter>(PartitionGatherScatter()))
 	  {
         v_ = gfs_.gridView().comm().rank();
       }
@@ -457,14 +420,14 @@ namespace Dune {
 	
 	template<class GFS, class V>
 	class GhostDataHandle
-	  : public Dune::PDELab::GenericDataHandle2<GFS,V,GhostGatherScatter>
+      : public Dune::PDELab::GFSDataHandle<GFS,V,DataEntityGatherScatter<GhostGatherScatter> >
 	{
-	  typedef Dune::PDELab::GenericDataHandle2<GFS,V,GhostGatherScatter> BaseT;
+      typedef Dune::PDELab::GFSDataHandle<GFS,V,DataEntityGatherScatter<GhostGatherScatter> > BaseT;
 
 	public:
 
-	  GhostDataHandle (const GFS& gfs_, V& v_) 
-		: BaseT(gfs_,v_,GhostGatherScatter())
+      GhostDataHandle (const GFS& gfs_, V& v_)
+        : BaseT(gfs_,v_,DataEntityGatherScatter<GhostGatherScatter>(GhostGatherScatter()))
 	  {
         v_ = static_cast<typename V::ElementType>(0);
       }
