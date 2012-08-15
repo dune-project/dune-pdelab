6a6b78c5485f2a53a6bfa47c92d92b198f8e91ce
Peter Bastian <peter@dune-project.org>
2012-08-15 12:58:22 +0000

    typo
    Imported from SVN: r2195 at trunk

--- dune/pdelab/boilerplate/pdelab.hh
+++ dune/pdelab/boilerplate/pdelab.hh
@@ -9,7 +9,7 @@
     These classes are experimental !
 
     To see an example how they might simplify your life see 
-    dune-pdelab-howto/examples/convection-diffusion/tutorial.cc
+    dune-pdelab-howto/src/convection-diffusion/tutorial.cc
 */
 
 // first of all we include a lot of dune grids and pdelab files
