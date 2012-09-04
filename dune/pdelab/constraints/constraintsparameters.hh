21d3e35cdb366ad6b15494fd33ce967591c25a77
Markus Blatt <mblatt@dune-project.org>
2012-09-04 13:32:31 +0000

    Correctly ended formula to doxygen happy and latex run through.
    Imported from SVN: r2217 at trunk

--- dune/pdelab/constraints/constraintsparameters.hh
+++ dune/pdelab/constraints/constraintsparameters.hh
@@ -154,7 +154,7 @@ namespace Dune {
      * by applying the standard logic that every location on the boundary is either Neumann or
      * Dirichlet constrained.
      *
-     * \warning This adapter will only work for the standard case of \f$\partial \Omega = \Gamma_D \cup \Gamma_N$\f.
+     * \warning This adapter will only work for the standard case of \f$\partial \Omega = \Gamma_D \cup \Gamma_N\f$.
      *          Do not try to use it for more advanced problems such as flow with flux, outflow and Dirichlet constraints!
      *
      * \tparam DirichletConstraintsParameters The type of the underlying Dirichlet constraints parameters implementation.
