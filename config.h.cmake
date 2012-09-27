268765d953b2d2a2f6bd2b33f5e9ab5fd61ede45
Markus Blatt <mblatt@dune-project.org>
2012-09-27 16:41:35 +0000

    Added missing config.h.cmake
    Imported from SVN: r2255 at branches/cmake

---
+++ config.h.cmake
@@ -0,0 +1,34 @@
+/* begin dune-pdelab
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten 
+*/
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* This is only true if PETSc was found by configure _and_ if the application
+   uses the UG_CPPFLAGS */
+#cmakedefine HAVE_PETSC ENABLE_PETSC
+
+/* end dune-pdelab */
