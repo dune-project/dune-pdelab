be19462e59452799ea023d084d04461c50526213
Steffen Müthing <muething@dune-project.org>
2012-10-26 11:56:18 +0200

    [ParallelISTLHelper] Port ParallelISTLHelper to new infrastructure
    This patch converts the ParallelISTLHelper to use the new DOF infrastructure and the
    DataHandles from genericdatahandle.hh. As those changes touch pretty much every line
    of the implementation, you could also call this a rewrite, but most of the algorithms
    haven't really changed.
    
    So here goes:
    
    * As this is pretty much an ISTL-only file, move the header to backend/istl/parallelhelper.hh.
    
    * Likewise, rename Dune::PDELab::ParallelISTLHelper -> Dune::PDELab::istl::ParallelHelper
    
    * Remove the DataHandle implementations from ParallelISTLHelper and use the implementations
      from genericdatahandle.hh. Those data handles are not tied in any way to the ISTL backend
      and will probably be useful when writing a parallel PETSc backend as well.
    
    * Kill the BlockProcessor. That thing was unbelievably fragile and tended to break when looked
      at funnily; apart from that, I have never really understood its purpose, especially after
      sifting through the code in ParallelISTLHelper and noticing that that code tends to promptly
      undo anything the BlockProcessor has just done. As it turns out, it can be removed without
      causing any problems, so good riddance!
    
    * Improve the masking operation restricting vectors to the disjoint partitioning of the grid
      to work with arbitrarily nested vectors. The method was also renamed to maskForeignDOFs().
    
    * Provide a method for calculating the local dot product on the disjoint partition to work
      with arbitrarily nested vectors. This feature was missing, causing the code to be repeated
      in several places. Also, properly support complex-valued vectors by delegating to Dune::dot().
      The new method is called disjointDot().
    
    * Improve AMG setup code by really exploiting the fact that the code only works for entity-blocked
      orderings anyway because the AMG code considers the blocked FieldMatrices as single entities.
      This allows us to remove a lot of inner loops over FieldVector block entries and really makes
      the code a lot more readable. It also helped getting rid of the BlockProcessor, so wins all
      around.
    
    * Inspect the newly-exported parallel layout information of the GridFunctionSpace and reduce the
      size of the communication interfaces for nonoverlapping spaces.
    
    * Better documentation!
    
    Move parallelistlhelper

---
+++ dune/pdelab/backend/istl/parallelhelper.hh
@@ -0,0 +1,436 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=8 sw=2 sts=2:
+#ifndef DUNE_PDELAB_BACKEND_ISTL_PARALLELHELPER_HH
+#define DUNE_PDELAB_BACKEND_ISTL_PARALLELHELPER_HH
+
+#include <limits>
+
+#include <dune/common/deprecated.hh>
+#include <dune/common/mpihelper.hh>
+#include <dune/common/static_assert.hh>
+
+#include <dune/istl/owneroverlapcopy.hh>
+#include <dune/istl/solvercategory.hh>
+#include <dune/istl/operators.hh>
+#include <dune/istl/solvers.hh>
+#include <dune/istl/preconditioners.hh>
+#include <dune/istl/scalarproducts.hh>
+#include <dune/istl/paamg/amg.hh>
+#include <dune/istl/paamg/pinfo.hh>
+#include <dune/istl/io.hh>
+#include <dune/istl/superlu.hh>
+
+#include <dune/pdelab/constraints/constraints.hh>
+#include <dune/pdelab/gridfunctionspace/genericdatahandle.hh>
+#include <dune/pdelab/backend/istlvectorbackend.hh>
+#include <dune/pdelab/gridfunctionspace/tags.hh>
+
+namespace Dune {
+  namespace PDELab {
+    namespace istl {
+
+      //! \addtogroup Backend
+      //! \ingroup PDELab
+      //! \{
+
+
+      //========================================================
+      // A parallel helper class providing a nonoverlapping
+      // decomposition of all degrees of freedom
+      //========================================================
+
+      template<typename GFS>
+      class ParallelHelper
+      {
+
+        //! Type for storing rank values.
+        typedef int RankIndex;
+
+        //! Type used to store owner rank values of all DOFs.
+        typedef typename Dune::PDELab::BackendVectorSelector<GFS,RankIndex>::Type RankVector;
+        //! Type used to store ghost flag of all DOFs.
+        typedef typename Dune::PDELab::BackendVectorSelector<GFS,bool>::Type GhostVector;
+
+        //! ContainerIndex of the underlying GridFunctionSpace.
+        typedef typename GFS::Ordering::Traits::ContainerIndex ContainerIndex;
+
+      public:
+
+        ParallelHelper (const GFS& gfs, int verbose = 1)
+          : _gfs(gfs)
+          , _rank(gfs.gridView().comm().rank())
+          , _ranks(gfs,_rank)
+          , _ghosts(gfs,false)
+          , _verbose(verbose)
+        {
+
+          // Let's try to be clever and reduce the communication overhead by picking the smallest
+          // possible communication interface depending on the overlap structure of the GFS.
+          if (gfs.ordering().containedPartitions() == NonOverlappingPartitionSelector::partition_mask)
+            {
+              // The GFS only spans the interior and border partitions, so we can skip sending or
+              // receiving anything else.
+              _interiorBorder_all_interface = InteriorBorder_InteriorBorder_Interface;
+              _all_all_interface = InteriorBorder_InteriorBorder_Interface;
+            }
+          else
+            {
+              // In general, we have to transmit more.
+              _interiorBorder_all_interface = InteriorBorder_All_Interface;
+              _all_all_interface = All_All_Interface;
+            }
+
+          if (_gfs.gridView().comm().size()>1)
+            {
+
+              // find out about ghosts
+              //GFSDataHandle<GFS,GhostVector,GhostGatherScatter>
+              GhostDataHandle<GFS,GhostVector>
+                gdh(_gfs,_ghosts,false);
+              _gfs.gridView().communicate(gdh,_interiorBorder_all_interface,Dune::ForwardCommunication);
+
+              // create disjoint DOF partitioning
+              //            GFSDataHandle<GFS,RankVector,DisjointPartitioningGatherScatter<RankIndex> >
+              //  ibdh(_gfs,_ranks,DisjointPartitioningGatherScatter<RankIndex>(_rank));
+              DisjointPartitioningDataHandle<GFS,RankVector> pdh(_gfs,_ranks);
+              _gfs.gridView().communicate(pdh,_interiorBorder_all_interface,Dune::ForwardCommunication);
+
+            }
+
+        }
+
+        //! Mask out all DOFs not owned by the current process with 0.
+        template<typename X>
+        void maskForeignDOFs(X& x) const
+        {
+          // dispatch to implementation.
+          maskForeignDOFS(istl::container_tag(x),x,_ranks);
+        }
+
+      private:
+
+        // Implementation for block vector; recursively masks blocks.
+        template<typename X, typename Mask>
+        void maskForeignDOFS(istl::tags::block_vector, X& x, const Mask& mask) const
+        {
+          typename Mask::const_iterator mask_it = mask.begin();
+          for (typename X::iterator it = x.begin(),
+                 end_it = x.end();
+               it != end_it;
+               ++it, ++mask_it)
+            maskForeignDOFS(istl::container_tag(*it),*it,*mask_it);
+        }
+
+        // Implementation for field vector, iterates over entries and masks them individually.
+        template<typename X, typename Mask>
+        void maskForeignDOFS(istl::tags::field_vector, X& x, const Mask& mask) const
+        {
+          typename Mask::const_iterator mask_it = mask.begin();
+          for (typename X::iterator it = x.begin(),
+                 end_it = x.end();
+               it != end_it;
+               ++it, ++mask_it)
+            *it = (*mask_it == _rank ? *it : typename X::field_type(0));
+        }
+
+      public:
+
+        //! Tests whether the given index is owned by this process.
+        bool owned(const ContainerIndex& i) const
+        {
+          return _ranks[i] == _rank;
+        }
+
+        //! Tests whether the given index belongs to a ghost DOF.
+        bool isGhost(const ContainerIndex& i) const
+        {
+          return _ghosts[i];
+        }
+
+        //! Calculates the (rank-local) dot product of x and y on the disjoint partition defined by the helper.
+        template<typename X, typename Y>
+        typename PromotionTraits<
+          typename X::field_type,
+          typename Y::field_type
+          >::PromotedType
+        disjointDot(const X& x, const Y& y) const
+        {
+          return disjointDot(istl::container_tag(istl::raw(x)),
+                             istl::raw(x),
+                             istl::raw(y),
+                             istl::raw(_ranks)
+                             );
+        }
+
+      private:
+
+        // Implementation for BlockVector, collects the result of recursively
+        // invoking the algorithm on the vector blocks.
+        template<typename X, typename Y, typename Mask>
+        typename PromotionTraits<
+        typename X::field_type,
+        typename Y::field_type
+        >::PromotedType
+        disjointDot(istl::tags::block_vector, const X& x, const Y& y, const Mask& mask) const
+        {
+          typedef typename PromotionTraits<
+            typename X::field_type,
+            typename Y::field_type
+            >::PromotedType result_type;
+
+          result_type r(0);
+
+          typename Y::const_iterator y_it = y.begin();
+          typename Mask::const_iterator mask_it = mask.begin();
+          for (typename X::const_iterator x_it = x.begin(),
+                 end_it = x.end();
+               x_it != end_it;
+               ++x_it, ++y_it,  ++mask_it)
+            r += disjointDot(istl::container_tag(*x_it),*x_it,*y_it,*mask_it);
+
+          return r;
+        }
+
+        // Implementation for FieldVector, iterates over the entries and calls Dune::dot() for DOFs
+        // associated with the current rank.
+        template<typename X, typename Y, typename Mask>
+        typename PromotionTraits<
+          typename X::field_type,
+          typename Y::field_type
+          >::PromotedType
+        disjointDot(istl::tags::field_vector, const X& x, const Y& y, const Mask& mask) const
+        {
+          typedef typename PromotionTraits<
+            typename X::field_type,
+            typename Y::field_type
+            >::PromotedType result_type;
+
+          result_type r(0);
+
+          typename Y::const_iterator y_it = y.begin();
+          typename Mask::const_iterator mask_it = mask.begin();
+          for (typename X::const_iterator x_it = x.begin(),
+                 end_it = x.end();
+               x_it != end_it;
+               ++x_it, ++y_it, ++mask_it)
+            r += (*mask_it == _rank ? Dune::dot(*x_it,*y_it) : result_type(0));
+
+          return r;
+        }
+
+      public:
+
+        //! Returns the MPI rank of this process.
+        RankIndex rank() const
+        {
+          return _rank;
+        }
+
+#if HAVE_MPI
+
+        //! Makes the matrix consistent and creates the parallel information for AMG.
+        /**
+         * This function accomplishes two things:
+         *
+         * 1. Makes the matrix consistent w.r.t. to the disjoint partitioning of the DOF space,
+         *    i.e. aggregates matrix entries for border entries from neighboring ranks.
+         *
+         * 2. Sets up the parallel communication information for AMG.
+         *
+         * \warning  This function silenty assumes that the matrix only has a single level
+         *           of blocking and will not work correctly otherwise. Also note that AMG
+         *           will only work correctly for P1 discretisations.
+         *
+         * \param m  The PDELab matrix container.
+         * \param c  The parallel information object providing index set, interfaces and
+         *           communicators.
+         */
+        template<typename MatrixType, typename Comm>
+        void createIndexSetAndProjectForAMG(MatrixType& m, Comm& c);
+
+      private:
+
+        // Checks whether a matrix block is owned by the current process. Used for the AMG
+        // construction and thus assumes a single level of blocking and blocks with ownership
+        // restricted to a single DOF.
+        bool owned_for_amg(std::size_t i) const
+        {
+          return _ranks.base()[i][0] == _rank;
+        }
+
+        // Checks whether a matrix block is associated with a ghost entity. Used for the AMG
+        // construction and thus assumes a single level of blocking and blocks with ownership
+        // restricted to a single DOF.
+        bool is_ghost_for_amg(std::size_t i) const
+        {
+          return _ghosts.base()[i][0];
+        }
+
+#endif // HAVE_MPI
+
+      private:
+
+        const GFS& _gfs;
+        const RankIndex _rank;
+        RankVector _ranks; // vector to identify unique decomposition
+        GhostVector _ghosts; //vector to identify ghost dofs
+        int _verbose; //verbosity
+
+        //! The actual communication interface used when algorithm requires InteriorBorder_All_Interface.
+        InterfaceType _interiorBorder_all_interface;
+
+        //! The actual communication interface used when algorithm requires All_All_Interface.
+        InterfaceType _all_all_interface;
+      };
+
+#if HAVE_MPI
+
+      template<typename GFS>
+      template<typename M, typename C>
+      void ParallelHelper<GFS>::createIndexSetAndProjectForAMG(M& m, C& c)
+      {
+
+        const bool is_bcrs_matrix =
+          is_same<
+            typename istl::tags::container<
+              typename M::BaseT
+              >::type::base_tag,
+          istl::tags::bcrs_matrix
+          >::value;
+
+        const bool block_type_is_field_matrix =
+          is_same<
+            typename istl::tags::container<
+              typename M::BaseT::block_type
+              >::type::base_tag,
+          istl::tags::field_matrix
+          >::value;
+
+        // We assume M to be a BCRSMatrix in the following, so better check for that
+        dune_static_assert(is_bcrs_matrix && block_type_is_field_matrix, "matrix structure not compatible with AMG");
+
+        // ********************************************************************************
+        // In the following, the code will always assume that all DOFs stored in a single
+        // block of the BCRSMatrix are attached to the same entity and can be handled
+        // identically. For that reason, the code often restricts itself to inspecting the
+        // first entry of the blocks in the diverse BlockVectors.
+        // ********************************************************************************
+
+        typedef typename GFS::Traits::GridViewType GV;
+        typedef typename RankVector::size_type size_type;
+        const GV& gv = _gfs.gridView();
+
+        // Do we need to communicate at all?
+        const bool need_communication = _gfs.gridView().comm().size() > 1;
+
+        // First find out which dofs we share with other processors
+        typedef typename BackendVectorSelector<GFS,bool>::Type BoolVector;
+        BoolVector sharedDOF(_gfs, false);
+
+        if (need_communication)
+          {
+            SharedDOFDataHandle<GFS,BoolVector> data_handle(_gfs,sharedDOF,false);
+            _gfs.gridView().communicate(data_handle,_all_all_interface,Dune::ForwardCommunication);
+          }
+
+        // Count shared dofs that we own
+        typedef typename C::ParallelIndexSet::GlobalIndex GlobalIndex;
+        GlobalIndex count = 0;
+
+        for (size_type i = 0; i < sharedDOF.N(); ++i)
+          if (owned_for_amg(i) && sharedDOF.base()[i][0])
+            ++count;
+
+        dverb << gv.comm().rank() << ": shared block count is " << count.touint() << std::endl;
+
+        // Communicate per-rank count of owned and shared DOFs to all processes.
+        std::vector<GlobalIndex> counts(_gfs.gridView().comm().size());
+        _gfs.gridView().comm().allgather(&count, 1, &(counts[0]));
+
+        // Compute start index start_p = \sum_{i=0}^{i<p} counts_i
+        GlobalIndex start = std::accumulate(counts.begin(),counts.begin() + _rank,GlobalIndex(0));
+
+        typedef typename Dune::PDELab::BackendVectorSelector<GFS,GlobalIndex>::Type GIVector;
+        GIVector scalarIndices(_gfs, std::numeric_limits<GlobalIndex>::max());
+
+        for (size_type i = 0; i < sharedDOF.N(); ++i)
+          if (owned_for_amg(i) && sharedDOF.base()[i][0])
+            {
+              scalarIndices.base()[i][0] = start;
+              ++start;
+            }
+
+        // Publish global indices for the shared DOFS to other processors.
+        if (need_communication)
+          {
+            MinDataHandle<GFS,GIVector> data_handle(_gfs,scalarIndices);
+            _gfs.gridView().communicate(data_handle,_interiorBorder_all_interface,Dune::ForwardCommunication);
+          }
+
+        // Setup the index set
+        c.indexSet().beginResize();
+        for (size_type i=0; i<scalarIndices.N(); ++i)
+          {
+            Dune::OwnerOverlapCopyAttributeSet::AttributeSet attr;
+            if(scalarIndices.base()[i][0] != std::numeric_limits<GlobalIndex>::max())
+              {
+                // global index exist in index set
+                if (owned_for_amg(i))
+                  {
+                    // This dof is managed by us.
+                    attr = Dune::OwnerOverlapCopyAttributeSet::owner;
+                  }
+                else if (is_ghost_for_amg(i) && c.getSolverCategory() == static_cast<int>(SolverCategory::nonoverlapping))
+                  {
+                    //use attribute overlap for ghosts in novlp grids
+                    attr = Dune::OwnerOverlapCopyAttributeSet::overlap;
+                  }
+                else
+                  {
+                    attr = Dune::OwnerOverlapCopyAttributeSet::copy;
+                  }
+                c.indexSet().add(scalarIndices.base()[i][0], typename C::ParallelIndexSet::LocalIndex(i,attr));
+              }
+          }
+        c.indexSet().endResize();
+
+        // Compute neighbors using communication
+        std::set<int> neighbors;
+
+        if (need_communication)
+          {
+            GFSNeighborDataHandle<GFS,int> data_handle(_gfs,_rank,neighbors);
+            _gfs.gridView().communicate(data_handle,_all_all_interface,Dune::ForwardCommunication);
+          }
+
+        c.remoteIndices().setNeighbours(neighbors);
+        c.remoteIndices().template rebuild<false>();
+      }
+
+#endif // HAVE_MPI
+
+      template<int s, bool isFakeMPIHelper>
+      struct CommSelector
+      {
+        typedef Dune::Amg::SequentialInformation type;
+      };
+
+
+#if HAVE_MPI
+
+      // Need MPI for OwnerOverlapCopyCommunication
+      template<int s>
+      struct CommSelector<s,false>
+      {
+        typedef OwnerOverlapCopyCommunication<bigunsignedint<s>,int> type;
+      };
+
+#endif // HAVE_MPI
+
+      //! \} group Backend
+
+    } // namespace istl
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_BACKEND_ISTL_PARALLELHELPER_HH
