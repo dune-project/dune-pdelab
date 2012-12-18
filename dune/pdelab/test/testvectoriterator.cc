f7122d05c8ceda0ac69f159f73ac784c5032b979
Steffen Müthing <muething@dune-project.org>
2012-12-18 21:01:31 +0100

    [Tests] Test vector_iterator on empty sub-vectors in testvectoriterator

--- dune/pdelab/test/testvectoriterator.cc
+++ dune/pdelab/test/testvectoriterator.cc
@@ -158,5 +158,38 @@ int main(int argc, char** argv)
     std::cout << v.one_norm() << std::endl;
   }
 
+  {
+    typedef Dune::BlockVector<Dune::BlockVector<Dune::DynamicVector<int> > > V;
+    typedef Dune::PDELab::istl::vector_iterator<V> Iterator;
+
+    V v(20);
+    int i = 19;
+    for (V::iterator it = v.begin(),
+           end = v.end();
+         it != end;
+         ++it, --i)
+      {
+        it->resize(i+1);
+        int j = i;
+        for (V::block_type::iterator it2 = it->begin(),
+           end2 = it->end();
+         it2 != end2;
+         ++it2, --j)
+          it2->resize(j);
+      }
+
+    v = 1;
+
+    std::cout << v.one_norm() << std::endl;
+
+    for (Iterator it = Iterator(v,false),
+           end = Iterator(v,true);
+         it != end;
+         ++it)
+      (*it) *= 3;
+
+    std::cout << v.one_norm() << std::endl;
+  }
+
   return 0;
 }
