67007c1620886f896669aa68c31253d0c47def83
Steffen Müthing <muething@dune-project.org>
2012-12-18 17:10:46 +0100

    [ISTL] Add nesting_depth TMP to determine how many nested levels of containers an ISTL object contains

--- dune/pdelab/backend/istl/tags.hh
+++ dune/pdelab/backend/istl/tags.hh
@@ -225,6 +225,47 @@ namespace Dune {
         return typename tags::container<T>::type();
       }
 
+#ifndef DOXYGEN
+
+      namespace impl {
+
+        template<typename T, std::size_t depth, typename Tag>
+        struct nesting_depth;
+
+        template<typename T, std::size_t depth>
+        struct nesting_depth<T,depth,tags::block_vector>
+          : public nesting_depth<typename T::block_type,depth+1,typename tags::container<typename T::block_type>::type::base_tag>
+        {};
+
+        template<typename T, std::size_t depth>
+        struct nesting_depth<T,depth,tags::field_vector>
+          : public integral_constant<std::size_t,depth+1>
+        {};
+
+        template<typename T, std::size_t depth>
+        struct nesting_depth<T,depth,tags::bcrs_matrix>
+          : public nesting_depth<typename T::block_type,depth+1,typename tags::container<typename T::block_type>::type::base_tag>
+        {};
+
+        template<typename T, std::size_t depth>
+        struct nesting_depth<T,depth,tags::field_matrix>
+          : public integral_constant<std::size_t,depth+1>
+        {};
+
+      }
+
+#endif // DOXYGEN
+
+      //! TMP for figuring out the depth up to which ISTL containers are nested.
+      /**
+       * This TMP calculates the nesting depth of ISTL containers. A FieldVector or
+       * FieldMatrix has a depth of 1.
+       */
+      template<typename T>
+      struct nesting_depth
+        : public impl::nesting_depth<T,0,typename tags::container<T>::type::base_tag>
+      {};
+
     } // namespace istl
 
 
