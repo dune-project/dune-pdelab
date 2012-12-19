068b89d4a9b9cd5b8f630235f6e4306a45c3683f
Steffen Müthing <muething@dune-project.org>
2012-12-19 16:37:26 +0100

    [ISTL] Adapt maskForeignDOFs() to new standard iterator type of backend vectors
    The standard iterator exported by the vector backend is no longer compatible with the
    block iterators of the ISTL vectors, so passing the _ranks vector into the maskForeignDOFs()
    machinery without unwrapping it doesn't work anymore.
    
    Unwrap it.

--- dune/pdelab/backend/istl/parallelhelper.hh
+++ dune/pdelab/backend/istl/parallelhelper.hh
@@ -105,7 +105,7 @@ namespace Dune {
         void maskForeignDOFs(X& x) const
         {
           // dispatch to implementation.
-          maskForeignDOFS(istl::container_tag(istl::raw(x)),istl::raw(x),_ranks);
+          maskForeignDOFs(istl::container_tag(istl::raw(x)),istl::raw(x),istl:raw(_ranks));
         }
 
       private:
