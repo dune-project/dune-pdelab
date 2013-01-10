6ac9daef59013cf89d4cfb31c8c27cf3ef916b74
Steffen Müthing <muething@dune-project.org>
2013-01-10 09:50:35 +0100

    [Cleanup] Fix warning about unmatched parentheses in gridoperatorspaceutilities.hh

--- dune/pdelab/gridoperatorspace/gridoperatorspaceutilities.hh
+++ dune/pdelab/gridoperatorspace/gridoperatorspaceutilities.hh
@@ -8,8 +8,8 @@
 
 #warning "dune/pdelab/gridoperatorspace/gridoperatorspaceutilities.hh" is deprecated
 #warning Use one or more of the following headers instead:
-#warning "dune/pdelab/gridoperator/common/assemblerutilities.hh>
-#warning "dune/pdelab/localoperator/callswitch.hh>
+#warning "dune/pdelab/gridoperator/common/assemblerutilities.hh>"
+#warning "dune/pdelab/localoperator/callswitch.hh>"
 #warning Note: This file is usually NOT required for local operator implementations anymore!
 
 #endif
