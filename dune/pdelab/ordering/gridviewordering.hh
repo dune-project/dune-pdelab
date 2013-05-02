252cf784a0155fbbdf9356c713638f7d89428ce0
Steffen Müthing <muething@dune-project.org>
2013-05-02 22:49:23 +0200

    [GridViewOrdering] Fix propagation of grid partition information for blocked GridViewOrdering
    The GridViewOrdering implementations need to manually copy the partition information
    from the local ordering, as the default partition set merging code in the OrderingBase
    constructor doesn't run for GridViewOrderings (their children are LocalOrderings, not
    Orderings).
    
    This copy step was done correctly for the more common case of the LeafGridViewOrdering,
    but forgotten in the GridViewOrdering for blocked spaces.

--- dune/pdelab/ordering/gridviewordering.hh
+++ dune/pdelab/ordering/gridviewordering.hh
@@ -459,6 +459,9 @@ namespace Dune {
         // make sure to switch off container blocking handling in the local ordering,
         // we already handle it in the GridViewOrdering
         localOrdering().disable_container_blocking();
+        // manually copy grid partition information from the local ordering, as this isn't handled
+        // automatically by LocalOrdering in this case
+        this->setPartitionSet(localOrdering());
       }
 
       LocalOrdering& localOrdering()
