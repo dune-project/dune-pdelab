9a18d11f7213c9aec83832d1c4b0cc59915449fb
Steffen Müthing <muething@dune-project.org>
2013-05-22 23:13:15 +0200

    [DenseBackend] Switch MatrixContainer to common UncachedMatrixViews

--- dune/pdelab/backend/dense/matrix.hh
+++ dune/pdelab/backend/dense/matrix.hh
@@ -7,11 +7,11 @@
 #include <functional>
 #include <numeric>
 
-#include <dune/common/static_assert.hh>
 #include <dune/common/typetraits.hh>
 #include <dune/common/shared_ptr.hh>
 #include <dune/pdelab/backend/tags.hh>
 #include <dune/pdelab/backend/backendselector.hh>
+#include <dune/pdelab/backend/common/uncachedmatrixview.hh>
 #include <dune/pdelab/backend/dense/descriptors.hh>
 
 namespace Dune {
@@ -31,6 +31,9 @@ namespace Dune {
         typedef ElementType field_type;
         typedef typename Container::size_type size_type;
 
+        typedef GFSU TrialGridFunctionSpace;
+        typedef GFSV TestGridFunctionSpace;
+
         typedef typename GFSV::Ordering::Traits::ContainerIndex RowIndex;
         typedef typename GFSU::Ordering::Traits::ContainerIndex ColIndex;
 
@@ -38,208 +41,11 @@ namespace Dune {
         typedef int Pattern;
 
         template<typename RowCache, typename ColCache>
-        class LocalView
-        {
+        using LocalView = UncachedMatrixView<MatrixContainer,RowCache,ColCache>;
 
-          dune_static_assert((is_same<typename RowCache::LocalFunctionSpace::Traits::GridFunctionSpace,GFSV>::value),
-                             "The RowCache passed to LocalView must belong to the underlying GFSV");
+        template<typename RowCache, typename ColCache>
+        using ConstLocalView = ConstUncachedMatrixView<const MatrixContainer,RowCache,ColCache>;
 
-          dune_static_assert((is_same<typename ColCache::LocalFunctionSpace::Traits::GridFunctionSpace,GFSU>::value),
-                             "The ColCache passed to LocalView must belong to the underlying GFSU");
-
-        public:
-
-          typedef typename Container::value_type E;
-          typedef E ElementType;
-
-          typedef RowCache RowIndexCache;
-          typedef ColCache ColIndexCache;
-
-          typedef typename RowCache::LocalFunctionSpace LFSV;
-          typedef typename ColCache::LocalFunctionSpace LFSU;
-
-          typedef typename LFSV::Traits::DOFIndex RowDOFIndex;
-          typedef typename LFSV::Traits::GridFunctionSpace::Ordering::Traits::ContainerIndex RowContainerIndex;
-
-          typedef typename LFSU::Traits::DOFIndex ColDOFIndex;
-          typedef typename LFSU::Traits::GridFunctionSpace::Ordering::Traits::ContainerIndex ColContainerIndex;
-
-          LocalView()
-            : _container(nullptr)
-            , _row_cache(nullptr)
-            , _col_cache(nullptr)
-          {}
-
-          LocalView(MatrixContainer& container)
-            : _container(&container)
-            , _row_cache(nullptr)
-            , _col_cache(nullptr)
-          {}
-
-          const RowIndexCache& rowIndexCache() const
-          {
-            assert(_row_cache);
-            return *_row_cache;
-          }
-
-          const ColIndexCache& colIndexCache() const
-          {
-            assert(_col_cache);
-            return *_col_cache;
-          }
-
-          void attach(MatrixContainer& container)
-          {
-            _container = &container;
-          }
-
-          void detach()
-          {
-            _container = nullptr;
-          }
-
-          void bind(const RowCache& row_cache, const ColCache& col_cache)
-          {
-            _row_cache = &row_cache;
-            _col_cache = &col_cache;
-          }
-
-          void unbind()
-          {
-          }
-
-          size_type N() const
-          {
-            return _row_cache->size();
-          }
-
-          size_type M() const
-          {
-            return _col_cache->size();
-          }
-
-          template<typename LC>
-          void read(LC& local_container) const
-          {
-            for (size_type i = 0; i < N(); ++i)
-              for (size_type j = 0; j < M(); ++j)
-                local_container.getEntry(i,j) = (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j));
-          }
-
-          template<typename LC>
-          void write(const LC& local_container) const
-          {
-            for (size_type i = 0; i < N(); ++i)
-              for (size_type j = 0; j < M(); ++j)
-                (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j)) = local_container.getEntry(i,j);
-          }
-
-          template<typename LC>
-          void add(const LC& local_container) const
-          {
-            for (size_type i = 0; i < N(); ++i)
-              for (size_type j = 0; j < M(); ++j)
-                (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j)) += local_container.getEntry(i,j);
-          }
-
-          void commit()
-          {
-          }
-
-
-          ElementType& operator()(size_type i, size_type j)
-          {
-            return (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j));
-          }
-
-          const ElementType& operator()(size_type i, size_type j) const
-          {
-            return (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j));
-          }
-
-          ElementType& operator()(const RowDOFIndex& i, const ColDOFIndex& j)
-          {
-            return (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j));
-          }
-
-          const ElementType& operator()(const RowDOFIndex& i, const ColDOFIndex& j) const
-          {
-            return (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j));
-          }
-
-          ElementType& operator()(const RowContainerIndex& i, const ColContainerIndex& j)
-          {
-            return (*_container)(i,j);
-          }
-
-          const ElementType& operator()(const RowContainerIndex& i, const ColContainerIndex& j) const
-          {
-            return (*_container)(i,j);
-          }
-
-          ElementType& operator()(const RowContainerIndex& i, size_type j)
-          {
-            return (*_container)(i,_col_cache->containerIndex(j));
-          }
-
-          const ElementType& operator()(const RowContainerIndex& i, size_type j) const
-          {
-            return (*_container)(i,_col_cache->containerIndex(j));
-          }
-
-          ElementType& operator()(size_type i, const ColContainerIndex& j)
-          {
-            return (*_container)(_row_cache->containerIndex(i),j);
-          }
-
-          const ElementType& operator()(size_type i, const ColContainerIndex& j) const
-          {
-            return (*_container)(_row_cache->containerIndex(i),j);
-          }
-
-
-          void add(size_type i, size_type j, const ElementType& v)
-          {
-            (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j)) += v;
-          }
-
-          void add(const RowDOFIndex& i, const ColDOFIndex& j, const ElementType& v)
-          {
-            (*_container)(_row_cache->containerIndex(i),_col_cache->containerIndex(j)) += v;
-          }
-
-          void add(const RowContainerIndex& i, const ColContainerIndex& j, const ElementType& v)
-          {
-            (*_container)(i,j) += v;
-          }
-
-          void add(const RowContainerIndex& i, size_type j, const ElementType& v)
-          {
-            (*_container)(i,_col_cache->containerIndex(j)) += v;
-          }
-
-          void add(size_type i, const ColContainerIndex& j, const ElementType& v)
-          {
-            (*_container)(_row_cache->containerIndex(i),j) += v;
-          }
-
-          MatrixContainer& global_container()
-          {
-            return *_container;
-          }
-
-          const MatrixContainer& global_container() const
-          {
-            return *_container;
-          }
-
-        private:
-
-          MatrixContainer* _container;
-          const RowCache* _row_cache;
-          const ColCache* _col_cache;
-
-        };
 
         template<typename GO>
         MatrixContainer(const GO& go)
