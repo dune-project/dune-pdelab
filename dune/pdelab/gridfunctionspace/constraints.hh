3d31045b186f56412159d8ad8e14383133b4b0dd
Christoph Grüninger <gruenich@dune-project.org>
2013-06-29 15:25:09 +0200

    Remove old (pre-PDELab 2.0) transition header gridfunctionspace/constraints.hh

--- dune/pdelab/gridfunctionspace/constraints.hh
+++
@@ -1,3 +0,0 @@
-#include <dune/pdelab/constraints/common/constraints.hh> // backward compatibility
-#warning Header "dune/pdelab/gridfunctionspace/constraints.hh" is deprecated
-#warning Please use "dune/pdelab/constraints/common/constraints.hh" instead
