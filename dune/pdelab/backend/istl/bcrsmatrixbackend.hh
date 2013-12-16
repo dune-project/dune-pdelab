4e217c41ca2c044bee8c230cdaeec4ec8f140570
Steffen Müthing <muething@dune-project.org>
2013-12-16 13:47:11 +0100

    [ISTL] Add new backend class with more efficient pattern construction
    The new construction method re-uses the ideas from the new implicit
    construction mode of the raw BCRSMatrix, but splits the process in two
    again by separating pattern creation and matrix assembly. There are two
    reasons for this split:
    
    - In parallel non-overlapping mode, we have to collect some extra
      information during pattern creation. By keeping pattern creation as a
      separate process, this data collection does not slow down standard
      jacobian assembly.
    
    - The split approach is more robust than the implicit mode of the
      BCRSMatrix, which can fail with an exception if the user
      underestimates the number of nonzero entries per row. Our approach
      only experiences a performance degradation during pattern creation in
      that case.

---
+++ dune/pdelab/backend/istl/bcrsmatrixbackend.hh
@@ -0,0 +1,306 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_BACKEND_ISTL_BCRSMATRIXBACKEND_HH
+#define DUNE_PDELAB_BACKEND_ISTL_BCRSMATRIXBACKEND_HH
+
+#include <dune/pdelab/backend/istlmatrixbackend.hh>
+#include <dune/pdelab/backend/istl/bcrspattern.hh>
+#include <dune/pdelab/backend/istl/patternstatistics.hh>
+
+namespace Dune {
+  namespace PDELab {
+    namespace istl {
+
+      // ********************************************************************************
+      // infrastructure for deducing the pattern type from row and column orderings
+      // ********************************************************************************
+
+      namespace {
+
+        template<typename M, typename RowOrdering, typename ColOrdering, bool pattern>
+        struct _build_bcrs_pattern_type
+        {
+          // we use void as a marker for a nonexisting subpattern
+          typedef void type;
+        };
+
+        // central TMP that is invoked recursively while traversing the ordering hierarchy
+        // and builds up the possibly nested pattern.
+        template<typename M, typename RowOrdering, typename ColOrdering>
+        struct _build_bcrs_pattern_type<M,RowOrdering,ColOrdering,true>
+        {
+
+          // descend into blocks
+          typedef typename _build_bcrs_pattern_type<
+            typename M::block_type,
+            RowOrdering,
+            ColOrdering,
+            requires_pattern<
+              typename M::block_type
+              >::value
+            >::type BlockOrdering;
+
+          // Leafs -> BCRSPattern, interior nodes -> NestedPattern
+          typedef typename conditional<
+            is_same<BlockOrdering,void>::value,
+            BCRSPattern<
+              RowOrdering,
+              ColOrdering
+              >,
+            NestedPattern<
+              RowOrdering,
+              ColOrdering,
+              BlockOrdering
+              >
+            >::type type;
+
+        };
+
+        // Wrapper TMP for constructing OrderingBase types from function spaces and for
+        // shielding user from recursive implementation
+        template<typename M, typename GFSV, typename GFSU, typename Tag>
+        struct build_bcrs_pattern_type
+        {
+
+          typedef OrderingBase<
+            typename GFSV::Ordering::Traits::DOFIndex,
+            typename GFSV::Ordering::Traits::ContainerIndex
+            > RowOrdering;
+
+          typedef OrderingBase<
+            typename GFSU::Ordering::Traits::DOFIndex,
+            typename GFSU::Ordering::Traits::ContainerIndex
+            > ColOrdering;
+
+          typedef typename _build_bcrs_pattern_type<M,RowOrdering,ColOrdering,requires_pattern<M>::value>::type type;
+        };
+
+        // Specialization for forcibly flat backends
+        template<typename M, typename GFSV, typename GFSU>
+        struct build_bcrs_pattern_type<M,GFSV,GFSU,FlatContainerAllocationTag>
+        {
+          typedef BCRSPattern<typename GFSV::Ordering, typename GFSU::Ordering> type;
+        };
+
+
+
+        // ********************************************************************************
+        // nested matrix allocation
+        // In contrast to the older implementation, this code still uses BCRSMatrix for nested matrices,
+        // but we do not attempt to keep the pattern of those interior matrices sparse and always allocate all
+        // blocks. That greatly simplifies the code, and as those interior matrices really shouldn't be very
+        // large, any performance impact is minimal.
+        // The code also collects statistics about the pattern of the leaf BCRSMatrices and collates those stats
+        // in a row-major ordering.
+        // ********************************************************************************
+
+        // interior BCRSMatrix
+        template<typename OrderingV, typename OrderingU, typename Pattern, typename Container, typename StatsVector>
+        typename enable_if<
+          !is_same<typename Pattern::SubPattern,void>::value &&
+           requires_pattern<Container>::value
+        >::type
+        allocate_bcrs_matrix(const OrderingV& ordering_v,
+                             const OrderingU& ordering_u,
+                             Pattern& p,
+                             Container& c,
+                             StatsVector& stats)
+        {
+          c.setSize(ordering_v.blockCount(),ordering_u.blockCount(),ordering_v.blockCount()*ordering_u.blockCount());
+          c.setBuildMode(Container::random);
+
+          for (std::size_t i = 0; i < c.N(); ++i)
+            c.setrowsize(i,ordering_u.blockCount());
+          c.endrowsizes();
+
+          for (std::size_t i = 0; i < c.N(); ++i)
+            for (std::size_t j = 0; j < c.M(); ++j)
+              c.addindex(i,j);
+          c.endindices();
+
+          for (std::size_t i = 0; i < c.N(); ++i)
+            for (std::size_t j = 0; j < c.M(); ++j)
+              {
+                allocate_matrix(ordering_v.childOrdering(i),
+                                ordering_u.childOrdering(j),
+                                p.subPattern(i,j),
+                                c[i][j],
+                                stats);
+              }
+        }
+
+        // leaf BCRSMatrix
+        template<typename OrderingV, typename OrderingU, typename Pattern, typename Container, typename StatsVector>
+        typename enable_if<
+          is_same<typename Pattern::SubPattern,void>::value
+          >::type
+        allocate_bcrs_matrix(const OrderingV& ordering_v,
+                             const OrderingU& ordering_u,
+                             Pattern& p,
+                             Container& c,
+                             StatsVector& stats)
+        {
+          c.setSize(ordering_v.blockCount(),ordering_u.blockCount(),0);
+          c.setBuildMode(Container::random);
+
+          std::vector<typename Pattern::size_type> row_sizes(p.sizes());
+
+          typename Pattern::size_type nnz = 0;
+          typename Pattern::size_type longest_row = 0;
+
+          for (typename Pattern::size_type i = 0; i < c.N(); ++i)
+            {
+              nnz += row_sizes[i];
+              longest_row = std::max(longest_row,row_sizes[i]);
+              c.setrowsize(i,row_sizes[i]);
+            }
+          c.endrowsizes();
+
+          stats.push_back(typename StatsVector::value_type(nnz,longest_row,p.overflowCount(),p.entriesPerRow(),ordering_v.blockCount()));
+
+          for (typename Pattern::size_type i = 0; i < c.N(); ++i)
+            c.setIndices(i,p.begin(i),p.end(i));
+
+          // free temporary index storage in pattern before allocating data array in matrix
+          p.clear();
+          // allocate data array
+          c.endindices();
+        }
+
+      } // anonymous namespace
+
+
+
+      //! Backend using (possibly nested) ISTL BCRSMatrices.
+      /**
+       * BCRSMatrixBackend is a matrix backend descriptor for ISTL matrices. It expects that
+       * both the ansatz and the test function space use ISTL vectors and automatically deduces
+       * the correct matrix type from those two vector backends.
+       *
+       * The backend uses an accelerated pattern construction scheme, which requires the average
+       * number of non-zero entries per matrix row as a priori information. In constrast to the
+       * older construction scheme, the improved version never requires more memory than the matrix
+       * does after pattern construction and runs a lot faster, as long as it is provided with a
+       * reasonable estimate for the number of non-zero entries per row.
+       *
+       */
+      template<typename EntriesPerRow = std::size_t>
+      struct BCRSMatrixBackend
+      {
+
+        //! The size type of the BCRSMatrix.
+        typedef std::size_t size_type;
+
+        //! The type of the object holding the statistics generated during pattern construction.
+        typedef PatternStatistics<size_type> Statistics;
+
+#if HAVE_TEMPLATE_ALIASES || DOXYGEN
+
+        //! The type of the pattern object passed to the GridOperator for pattern construction.
+        template<typename Matrix, typename GFSV, typename GFSU>
+        using Pattern = typename build_bcrs_pattern_type<
+          typename Matrix::Container,
+          GFSV,
+          GFSU,
+          typename GFSV::Ordering::ContainerAllocationTag
+          >::type;
+
+#else // HAVE_TEMPLATE_ALIASES
+
+        template<typename Matrix, typename GFSV, typename GFSU>
+        struct Pattern
+          : public build_bcrs_pattern_type<typename Matrix::Container,
+                                           GFSV,
+                                           GFSU,
+                                           typename GFSV::Ordering::ContainerAllocationTag
+                                           >::type
+        {
+
+          typedef OrderingBase<
+            typename GFSV::Ordering::Traits::DOFIndex,
+            typename GFSV::Ordering::Traits::ContainerIndex
+            > RowOrdering;
+
+          typedef OrderingBase<
+            typename GFSU::Ordering::Traits::DOFIndex,
+            typename GFSU::Ordering::Traits::ContainerIndex
+            > ColOrdering;
+
+          typedef typename build_bcrs_pattern_type<
+            typename Matrix::Container,
+            GFSV,
+            GFSU,
+            typename GFSV::Ordering::ContainerAllocationTag
+            >::type BaseT;
+
+          template<typename EntriesPerRow>
+          Pattern(const RowOrdering& row_ordering, const ColOrdering& col_ordering, const EntriesPerRow& entries_per_row)
+            : BaseT(row_ordering,col_ordering,entries_per_row)
+          {}
+
+        };
+
+#endif // HAVE_TEMPLATE_ALIASES
+
+        template<typename VV, typename VU, typename E>
+        struct MatrixHelper
+        {
+          typedef ISTLMatrixContainer<
+            typename VV::GridFunctionSpace,
+            typename VU::GridFunctionSpace,
+            typename build_matrix_type<
+              E,
+              typename VV::Container,
+              typename VU::Container
+              >::type,
+            Statistics
+            > type;
+        };
+
+        //! Builds the matrix pattern associated with grid_operator and initializes matrix with it.
+        /**
+         * \returns  a vector with statistics object for all leaf BCRSMatrices in row-major order.
+         */
+        template<typename GridOperator, typename Matrix>
+        std::vector<Statistics> buildPattern(const GridOperator& grid_operator, Matrix& matrix) const
+        {
+          Pattern<
+            Matrix,
+            typename GridOperator::Traits::TestGridFunctionSpace,
+            typename GridOperator::Traits::TrialGridFunctionSpace
+            > pattern(grid_operator.testGridFunctionSpace().ordering(),grid_operator.trialGridFunctionSpace().ordering(),_entries_per_row);
+          grid_operator.fill_pattern(pattern);
+          std::vector<Statistics> stats;
+          allocate_bcrs_matrix(grid_operator.testGridFunctionSpace().ordering(),
+                               grid_operator.trialGridFunctionSpace().ordering(),
+                               pattern,
+                               istl::raw(matrix),
+                               stats
+                               );
+#if HAVE_RVALUE_REFERENCES
+          return std::move(stats);
+#else
+          return stats;
+#endif
+        }
+
+        //! Constructs a BCRSMatrixBackend.
+        /**
+         * TODO: Document and flesh out the way this should work for nested matrices (use a nested array as entries_per_row).
+         *
+         * \param entries_per_row  The average number of nonzero entries per row in matrices created with this backend.
+         */
+        BCRSMatrixBackend(const EntriesPerRow& entries_per_row)
+          : _entries_per_row(entries_per_row)
+        {}
+
+      private:
+
+        EntriesPerRow _entries_per_row;
+
+      };
+
+    } // namespace istl
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_BACKEND_ISTL_BCRSMATRIXBACKEND_HH
---
+++ dune/pdelab/backend/istl/bcrspattern.hh
@@ -0,0 +1,411 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef DUNE_PDELAB_BACKEND_ISTL_BCRSPATTERN_HH
+#define DUNE_PDELAB_BACKEND_ISTL_BCRSPATTERN_HH
+
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#include <dune/common/iteratorfacades.hh>
+
+#include <dune/pdelab/backend/tags.hh>
+#include <dune/pdelab/backend/common/uncachedmatrixview.hh>
+#include <dune/pdelab/backend/istl/matrixhelpers.hh>
+#include <dune/pdelab/backend/istl/descriptors.hh>
+
+namespace Dune {
+  namespace PDELab {
+    namespace istl {
+
+      //! Pattern builder for generic BCRS-like sparse matrices.
+      /**
+       * BCRSPattern is a pattern builder for unstructured sparse matrices
+       * for operators mapping from a vector that conforms to RowOrdering to a vector
+       * that conforms to ColOrdering.
+       *
+       * BCRSPattern has much better runtime performance and requires far less memory
+       * than the older pattern constructon method in PDELab. By letting the user specify
+       * the average number of nonzeroes per row, it is possible to use a more efficient
+       * array-based storage scheme for the majority of the pattern entries, only using
+       * expensive map-like lookups for those entries that exceed that average.
+       *
+       * BCRSPattern requires a recent version of the BCRSMatrix with support for row-wise
+       * setting of column indices and split allocation of column index and data arrays.
+       *
+       * Note that unlike the implicit construction mode of the BCRSMatrix itself, this
+       * pattern builder will neither throw an exception if the number of nonzeroes was set
+       * too low nor retain excess memory if it was set too high after the pattern construction
+       * is complete. Performance will degrade if the user-provided estimate is too far away
+       * from the real value.
+       */
+      template<typename RowOrdering, typename ColOrdering>
+      class BCRSPattern
+      {
+
+      public:
+
+        //! size type used by BCRSPattern.
+        typedef typename RowOrdering::Traits::size_type size_type;
+
+        //! BCRSPattern cannot contain nested subpatterns. This entry is only required for TMP purposes.
+        typedef void SubPattern;
+
+      private:
+
+        //! Marker value indicating an empty array entry.
+        static const size_type empty = ~static_cast<size_type>(0);
+
+        //! Functor for looking up a column index within a row.
+        /**ss
+         * Looking up column indices requires a special comparison iterator,
+         * as we want to either return the position of the actual index if it
+         * has already been inserted or of the first empty matrix entry that we
+         * can use to store the index.
+         *
+         * \warning Only use for sequential search algorithms, binary searches
+         *          will not work correctly!
+         */
+        struct PaddedColumnCriterion
+        {
+
+          bool operator()(size_type k) const
+          {
+            return k == _j || k == empty;
+          }
+
+          PaddedColumnCriterion(size_type j)
+            : _j(j)
+          {}
+
+          const size_type _j;
+
+        };
+
+
+        typedef typename std::vector<size_type>::iterator IndicesIterator;
+        typedef typename std::set<std::pair<size_type,size_type> >::iterator OverflowIterator;
+
+        typedef typename std::vector<size_type>::const_iterator ConstIndicesIterator;
+        typedef typename std::set<std::pair<size_type,size_type> >::const_iterator ConstOverflowIterator;
+
+      public:
+
+        //! Add a link between the row indicated by ri and the column indicated by ci.
+        template<typename RI, typename CI>
+        void add_link(const RI& ri, const CI& ci)
+        {
+          // extract block indices for current level
+          size_type i = ri.back();
+          size_type j = ci.back();
+
+          IndicesIterator start = _indices.begin();
+          IndicesIterator begin = start + _entries_per_row*i;
+          IndicesIterator end = start + _entries_per_row*(i+1);
+
+          // Does the entry (i,j) already exist?
+          IndicesIterator it = std::find_if(begin,end,PaddedColumnCriterion(j));
+          if (it != end)
+            {
+              // Yes, just write out j. This does the right thing regardless of whether
+              // it points at the correct column value or at an empty entry.
+              *it = j;
+            }
+          else
+            {
+              // The row is already full -> spill into map
+              _overflow.insert(std::make_pair(i,j));
+            }
+        }
+
+#ifndef DOXYGEN
+
+        // implementation detail for nested patterns
+        template<typename RI, typename CI>
+        void recursive_add_entry(const RI& ri, const CI& ci)
+        {
+          add_link(ri,ci);
+        }
+
+#endif //DOXYGEN
+
+        //! Stream the sizes of all rows into the output iterator rit.
+        template<typename I>
+        void sizes(I rit) const
+        {
+          ConstIndicesIterator it = _indices.begin();
+          ConstIndicesIterator end = _indices.begin() + _entries_per_row;
+          ConstOverflowIterator oit = _overflow.begin();
+          ConstOverflowIterator oend = _overflow.end();
+          for (size_type i = 0; i < _row_ordering.blockCount(); ++i, ++rit, end+=_entries_per_row)
+            {
+              size_type s = 0;
+              // count non-empty column entries, break when first empty one is found.
+              for (; it != end; ++it)
+                {
+                  if (*it == empty)
+                    break;
+                  ++s;
+                }
+              it = end;
+              // add overflow entries
+              for (; oit != oend && oit->first == i; ++oit)
+                ++s;
+              *rit = s;
+            }
+        }
+
+        //! Returns a vector with the size of all rows in the pattern.
+        std::vector<size_type> sizes() const
+        {
+          std::vector<size_type> r(_row_ordering.blockCount());
+          sizes(r.begin());
+#if HAVE_RVALUE_REFERENCES
+          return std::move(r);
+#else
+          return r;
+#endif
+        }
+
+        //! Iterator over all column indices for a given row, unique but in arbitrary order.
+        struct iterator
+          : public ForwardIteratorFacade<iterator, const size_type>
+        {
+
+#ifndef DOXYGEN
+
+          const size_type& dereference() const
+          {
+            if (_in_overflow)
+              return _oit->second;
+            else
+              return *_it;
+          }
+
+          void increment()
+          {
+            if (_in_overflow)
+              {
+                if (_oit != _oend)
+                  ++_oit;
+                if (_oit->first == _row)
+                  return;
+                // we have exhausted the row, invalidate iterator
+                _at_end = true;
+              }
+            else
+              {
+                if (_it != _end)
+                  ++_it;
+                if (_it == _end || *_it == empty)
+                  {
+                    _in_overflow = true;
+                    // we have exhausted the row, invalidate iterator
+                    if (_oit == _oend || _oit->first > _row)
+                      _at_end = true;
+                  }
+              }
+          }
+
+          bool equals(const iterator& other) const
+          {
+            if (_row != other._row)
+              return false;
+            if (_at_end || other._at_end)
+              return _at_end && other._at_end;
+            if (_in_overflow)
+              return _oit == other._oit;
+            else
+              return _it == other._it;
+          }
+
+          iterator(const BCRSPattern& p, size_type row, bool at_end)
+            : _row(row)
+            , _in_overflow(false)
+            , _at_end(at_end)
+            , _it(p._indices.begin() + row * p._entries_per_row)
+            , _end(p._indices.begin() + (row+1) * p._entries_per_row)
+            , _oit(p._overflow.lower_bound(std::make_pair(row,0)))
+            , _oend(p._overflow.end())
+          {
+            // catch corner case with completely empty row
+            if ((!_at_end) && _it == _end)
+              _at_end = _oit != _oend && _oit->first == _row;
+          }
+
+          size_type _row;
+          bool _in_overflow;
+          bool _at_end;
+          typename std::vector<size_type>::const_iterator _it;
+          typename std::vector<size_type>::const_iterator _end;
+          typename std::set<std::pair<size_type,size_type> >::const_iterator _oit;
+          const typename std::set<std::pair<size_type,size_type> >::const_iterator _oend;
+
+#endif // DOXYGEN
+
+        };
+
+        //! Returns an iterator to the first column index of row i.
+        iterator begin(size_type i) const
+        {
+          return iterator(*this,i,false);
+        }
+
+        //! Returns an iterator past the last column index of row i.
+        iterator end(size_type i) const
+        {
+          return iterator(*this,i,true);
+        }
+
+        //! Constructs a BCRSPattern for the given pair of orderings and reserves space for the provided average number of entries per row.
+        /**
+         * \param row_ordering    Ordering describing the row structure
+         * \param col_ordering    Ordering describing the column structure
+         * \param entries_per_row An estimate of the average number of entries per row.
+         */
+        BCRSPattern(const RowOrdering& row_ordering, const ColOrdering& col_ordering, size_type entries_per_row)
+          : _row_ordering(row_ordering)
+          , _col_ordering(col_ordering)
+          , _entries_per_row(entries_per_row)
+          , _indices(row_ordering.blockCount()*entries_per_row,size_type(empty))
+        {}
+
+        const RowOrdering& rowOrdering() const
+        {
+          return _row_ordering;
+        }
+
+        const ColOrdering& colOrdering() const
+        {
+          return _row_ordering;
+        }
+
+        //! Discard all internal data.
+        /**
+         * The purpose of this method is to release all internal memory before calling
+         * BCRSMatrix::endindices(). That way, the matrix creation process never consumes
+         * substantially more memory as required by the matrix after construction, as the
+         * second copy of the column indices is about as large as the data array.
+         */
+        void clear()
+        {
+          _indices = std::vector<size_type>();
+          _overflow = std::set<std::pair<size_type,size_type> >();
+        }
+
+        size_type entriesPerRow() const
+        {
+          return _entries_per_row;
+        }
+
+        size_type overflowCount() const
+        {
+          return _overflow.size();
+        }
+
+      private:
+
+        const RowOrdering& _row_ordering;
+        const ColOrdering& _col_ordering;
+        const size_type _entries_per_row;
+
+        std::vector<size_type> _indices;
+        std::set<std::pair<size_type,size_type> > _overflow;
+
+      };
+
+
+      //! Pattern builder for nested hierarchies of generic BCRS-like sparse matrices.
+      /**
+       * NestedPattern contains a dense set of subpatterns for each matrix block. Those
+       * blocks can be nested (i.e. be NestedPatterns again) or BCRSPattern instances.
+       */
+
+      template<typename RowOrdering, typename ColOrdering, typename SubPattern_>
+      class NestedPattern
+      {
+
+      public:
+
+        //! The pattern type used for each block.
+        typedef SubPattern_ SubPattern;
+
+        //! size type used by NestedPattern.
+        typedef typename SubPattern::size_type size_type;
+
+        //! Add a link between the row indicated by ri and the column indicated by ci.
+        /**
+         * This method just forwards the call to the relevant block as indicated by the
+         * tail members of ri and ci.
+         */
+        template<typename RI, typename CI>
+        void add_link(const RI& ri, const CI& ci)
+        {
+          recursive_add_entry(ri.view(),ci.view());
+        }
+
+#ifndef DOXYGEN
+
+        template<typename RI, typename CI>
+        void recursive_add_entry(const RI& ri, const CI& ci)
+        {
+          _sub_patterns[ri.back() * _col_ordering.blockCount() + ci.back()].recursive_add_entry(ri.back_popped(),ci.back_popped());
+        }
+
+#endif // DOXYGEN
+
+        template<typename EntriesPerRow>
+        NestedPattern(const RowOrdering& row_ordering, const ColOrdering& col_ordering, const EntriesPerRow& entries_per_row)
+          : _row_ordering(row_ordering)
+          , _col_ordering(col_ordering)
+        {
+          size_type rows = row_ordering.blockCount();
+          size_type cols = col_ordering.blockCount();
+          for (size_type i = 0; i < rows; ++i)
+            for (size_type j = 0; j < cols; ++j)
+              _sub_patterns.push_back(
+                SubPattern(
+                  _row_ordering.childOrdering(i),
+                  _col_ordering.childOrdering(j),
+                  entries_per_row[i][j]
+                  )
+                );
+        }
+
+        NestedPattern(const RowOrdering& row_ordering, const ColOrdering& col_ordering, size_type entries_per_row)
+          : _row_ordering(row_ordering)
+          , _col_ordering(col_ordering)
+        {
+          size_type rows = row_ordering.blockCount();
+          size_type cols = col_ordering.blockCount();
+          for (size_type i = 0; i < rows; ++i)
+            for (size_type j = 0; j < cols; ++j)
+              _sub_patterns.push_back(
+                SubPattern(
+                  _row_ordering.childOrdering(i),
+                  _col_ordering.childOrdering(j),
+                  entries_per_row
+                  )
+                );
+        }
+
+        //! Returns the subpattern associated with block (i,j).
+        SubPattern& subPattern(size_type i, size_type j)
+        {
+          return _sub_patterns[i * _col_ordering.blockCount() + j];
+        }
+
+      private:
+
+        const RowOrdering& _row_ordering;
+        const ColOrdering& _col_ordering;
+        std::vector<SubPattern> _sub_patterns;
+
+      };
+
+
+    } // namespace istl
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_BACKEND_ISTL_BCRSPATTERN_HH
