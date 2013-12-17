1d602dea410c2028163af40e48aa428dd108c5da
Steffen Müthing <muething@dune-project.org>
2013-12-17 11:55:52 +0100

    [FiniteElementMap] Add generic QkLocalFiniteElementMap to replace the current zoo of variants

---
+++ dune/pdelab/finiteelementmap/qkfem.hh
@@ -0,0 +1,60 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_PDELAB_FINITEELEMENTMAP_QKFEM_HH
+#define DUNE_PDELAB_FINITEELEMENTMAP_QKFEM_HH
+
+#include <cstddef>
+
+#include <dune/localfunctions/lagrange/qk.hh>
+#include <dune/pdelab/finiteelementmap/finiteelementmap.hh>
+
+namespace Dune {
+  namespace PDELab {
+
+    //! wrap up element from local functions
+    //! \ingroup FiniteElementMap
+    template<typename GV, typename D, typename R, std::size_t k>
+    class QkLocalFiniteElementMap
+      : public SimpleLocalFiniteElementMap< Dune::QkLocalFiniteElement<D,R,GV::dimension,k> >
+    {
+
+    public:
+
+      bool fixedSize() const
+      {
+        return true;
+      }
+
+      std::size_t size(GeometryType gt) const
+      {
+        switch (k)
+          {
+          case 1:
+            return gt.isVertex() ? 1 : 0;
+          case 2:
+            {
+              if (GV::dimension != 2)
+                DUNE_THROW(NotImplemented,"QkLocalFiniteElementMap is only implemented for k <= 2");
+              // Q1 simply attaches a single DOF to each subentity
+              return 1;
+            }
+          default:
+            DUNE_THROW(NotImplemented,"QkLocalFiniteElementMap is only implemented for k <= 2");
+          }
+      }
+
+      std::size_t maxLocalSize() const
+      {
+        std::size_t r = 1;
+        for (std::size_t i = 0; i < GV::dimension; ++i)
+          r *= (k + 1);
+        return r;
+      }
+
+    };
+
+  }
+}
+
+#endif // DUNE_PDELAB_FINITEELEMENTMAP_QKFEM_HH
