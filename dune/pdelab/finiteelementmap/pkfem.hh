97fc1bea7be842dc6c52b1f2d6e27ffebe3633b9
Steffen Müthing <muething@dune-project.org>
2013-12-17 14:44:12 +0100

    [PkFEM] Deprecate redundant fifth template parameter for the grid dimension
    Just get it from the GridView...

--- dune/pdelab/finiteelementmap/pkfem.hh
+++ dune/pdelab/finiteelementmap/pkfem.hh
@@ -14,41 +14,75 @@
 namespace Dune {
   namespace PDELab {
 
+
+    template<unsigned int d>
+    struct DUNE_DEPRECATED_MSG("The fifth template parameter for PkLocalFiniteElementMap (the dimension) is deprecated") warn_deprecated_pk_interface
+    {
+      warn_deprecated_pk_interface() DUNE_DEPRECATED_MSG("The fifth template parameter for PkLocalFiniteElementMap (the dimension) is deprecated")
+        {}
+    };
+
+    template<>
+    struct warn_deprecated_pk_interface<0>
+    {
+      warn_deprecated_pk_interface()
+      {}
+    };
+
     //! wrap up element from local functions
     //! \ingroup FiniteElementMap
 
-    // dummy template; use only implementations for d=2, d=3
+
     template<typename GV, typename D, typename R, unsigned int k, unsigned int d>
-    class PkLocalFiniteElementMap
-    {
-    };
+    class PkFEMDimensionDispatch
+    {};
 
     // 1D version
     template<typename GV, typename D, typename R, unsigned int k>
-    class PkLocalFiniteElementMap<GV,D,R,k,1> : public Pk1dLocalFiniteElementMap<D,R>
+    class PkFEMDimensionDispatch<GV,D,R,k,1> : public Pk1dLocalFiniteElementMap<D,R>
     {
     public:
-      PkLocalFiniteElementMap (const GV& gv) : Pk1dLocalFiniteElementMap<D,R>(k)
+      PkFEMDimensionDispatch (const GV& gv) : Pk1dLocalFiniteElementMap<D,R>(k)
       {}
     };
 
     // 2D version
     template<typename GV, typename D, typename R, unsigned int k>
-    class PkLocalFiniteElementMap<GV,D,R,k,2> : public Pk2DLocalFiniteElementMap<GV,D,R,k>
+    class PkFEMDimensionDispatch<GV,D,R,k,2> : public Pk2DLocalFiniteElementMap<GV,D,R,k>
     {
     public:
-      PkLocalFiniteElementMap (const GV& gv) : Pk2DLocalFiniteElementMap<GV,D,R,k>(gv)
+      PkFEMDimensionDispatch (const GV& gv) : Pk2DLocalFiniteElementMap<GV,D,R,k>(gv)
       {}
     };
 
     // 3D version
     template<typename GV, typename D, typename R, unsigned int k>
-    class PkLocalFiniteElementMap<GV,D,R,k,3> : public Pk3DLocalFiniteElementMap<GV,D,R,k>
+    class PkFEMDimensionDispatch<GV,D,R,k,3> : public Pk3DLocalFiniteElementMap<GV,D,R,k>
     {
     public:
-      PkLocalFiniteElementMap (const GV& gv) : Pk3DLocalFiniteElementMap<GV,D,R,k>(gv)
+      PkFEMDimensionDispatch (const GV& gv) : Pk3DLocalFiniteElementMap<GV,D,R,k>(gv)
       {}
     };
+
+
+
+    // dummy template; use only implementations for d=2, d=3
+    template<typename GV, typename D, typename R, unsigned int k, unsigned int d = 0>
+    class PkLocalFiniteElementMap
+      : public PkFEMDimensionDispatch<GV,D,R,k,GV::dimension>
+      , public warn_deprecated_pk_interface<d>
+
+    {
+
+    public:
+
+      PkLocalFiniteElementMap(const GV& gv)
+        : PkFEMDimensionDispatch<GV,D,R,k,GV::dimension>(gv)
+        , warn_deprecated_pk_interface<d>()
+      {}
+
+    };
+
 
   }
 }
