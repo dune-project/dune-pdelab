e2c54bde7ef04207aef64083b9d1fb0b4df5c99d
Steffen Müthing <muething@dune-project.org>
2014-01-15 16:02:53 +0100

    [StationaryLinearProblemSolver] Code cleanup and performance improvement
    This patch adds an option to keep the allocated matrix across
    invocations of the solver to avoid rebuilding the pattern for the common
    case where it doesn't change between calls to the solver.
    The matrix is automatically created if it does not exist upon entering
    the apply() method. By default, the matrix is now kept across calls to
    apply(); it can be discarded manually by calling discardMatrix() and the
    old behavior can be restored with setKeepMatrix(false).
    
    The patch also cleans up the code (whitespace, formatting, variable
    naming conventions).

--- dune/pdelab/stationary/linearproblem.hh
+++ dune/pdelab/stationary/linearproblem.hh
@@ -1,12 +1,15 @@
 #ifndef DUNE_PDELAB_STATIONARYLINEARPROBLEM_HH
 #define DUNE_PDELAB_STATIONARYLINEARPROBLEM_HH
 
-#include<dune/common/timer.hh>
-#include<dune/pdelab/backend/backendselector.hh>
-#include<dune/pdelab/constraints/common/constraints.hh>
-#include<iostream>
+#include <iostream>
 
-#include "../backend/solver.hh"
+#include <dune/common/timer.hh>
+#include <dune/common/deprecated.hh>
+#include <dune/common/parametertree.hh>
+
+#include <dune/pdelab/backend/backendselector.hh>
+#include <dune/pdelab/constraints/common/constraints.hh>
+#include <dune/pdelab/backend/solver.hh>
 
 namespace Dune {
   namespace PDELab {
@@ -28,49 +31,91 @@ namespace Dune {
       double linear_solver_time; // Cumulative time for linear sovler
       int linear_solver_iterations; // Total number of linear iterations
 
-      StationaryLinearProblemSolverResult() :
-        first_defect(0.0), defect(0.0), assembler_time(0.0), linear_solver_time(0.0),
-        linear_solver_iterations(0) {}
+      StationaryLinearProblemSolverResult()
+        : first_defect(0.0)
+        , defect(0.0)
+        , assembler_time(0.0)
+        , linear_solver_time(0.0)
+        , linear_solver_iterations(0)
+      {}
+
     };
 
-    template<class GOS, class LS, class V> 
+    template<typename GO, typename LS, typename V>
     class StationaryLinearProblemSolver
     {
       typedef typename V::ElementType Real;
-      typedef typename GOS::Traits::Jacobian M;
-      typedef typename GOS::Traits::TrialGridFunctionSpace TrialGridFunctionSpace;
+      typedef typename GO::Traits::Jacobian M;
+      typedef typename GO::Traits::TrialGridFunctionSpace TrialGridFunctionSpace;
       typedef typename Dune::PDELab::BackendVectorSelector<TrialGridFunctionSpace,Real>::Type W;
-      
+      typedef GO GridOperator;
+
     public:
       typedef StationaryLinearProblemSolverResult<double> Result;
 
-      StationaryLinearProblemSolver (const GOS& gos_, V& x_, LS& ls_, typename V::ElementType reduction_, typename V::ElementType mindefect_ = 1e-99, int verb_=1)
-        : gos(gos_), ls(ls_), x(&x_), reduction(reduction_), mindefect(mindefect_), hangingNodeModifications(true), verbose(verb_)
+      StationaryLinearProblemSolver(const GO& go, V& x, LS& ls, typename V::ElementType reduction, typename V::ElementType min_defect = 1e-99, int verbose=1) DUNE_DEPRECATED_MSG("Use StationaryLinearProblemSolver(const GO&, LS&, V&, ...) instead.")
+        : _go(go)
+        , _ls(ls)
+        , _x(&x)
+        , _reduction(reduction)
+        , _min_defect(min_defect)
+        , _hanging_node_modifications(true)
+        , _keep_matrix(true)
+        , _verbose(verbose)
+      {}
+
+      StationaryLinearProblemSolver(const GO& go, LS& ls, V& x, typename V::ElementType reduction, typename V::ElementType min_defect = 1e-99, int verbose=1)
+        : _go(go)
+        , _ls(ls)
+        , _x(&x)
+        , _reduction(reduction)
+        , _min_defect(min_defect)
+        , _hanging_node_modifications(true)
+        , _keep_matrix(true)
+        , _verbose(verbose)
+      {}
+
+      StationaryLinearProblemSolver (const GO& go, LS& ls, typename V::ElementType reduction, typename V::ElementType min_defect = 1e-99, int verbose=1)
+        : _go(go)
+        , _ls(ls)
+        , _x(nullptr)
+        , _reduction(reduction)
+        , _min_defect(min_defect)
+        , _hanging_node_modifications(true)
+        , _keep_matrix(true)
+        , _verbose(verbose)
+      {}
+      //! Set whether the solver should apply the necessary transformations for calculations on hanging nodes.
+      void setHangingNodeModifications(bool b)
       {
+        _hanging_node_modifications=b;
       }
 
-      StationaryLinearProblemSolver (const GOS& gos_, LS& ls_, V& x_, typename V::ElementType reduction_, typename V::ElementType mindefect_ = 1e-99, int verb_=1)
-        : gos(gos_), ls(ls_), x(&x_), reduction(reduction_), mindefect(mindefect_), hangingNodeModifications(true), verbose(verb_)
+      //! Return whether the solver performs the necessary transformations for calculations on hanging nodes.
+      bool hangingNodeModifications() const
       {
+        return _hanging_node_modifications;
       }
 
-      StationaryLinearProblemSolver (const GOS& gos_, LS& ls_, typename V::ElementType reduction_, typename V::ElementType mindefect_ = 1e-99, int verb_=1)
-          : gos(gos_), ls(ls_), x(0), reduction(reduction_), mindefect(mindefect_), hangingNodeModifications(true), verbose(verb_)
+      //! Set whether the jacobian matrix should be kept across calls to apply().
+      void setKeepMatrix(bool b)
       {
+        _keep_matrix = b;
       }
 
-      void setHangingNodeModifications (bool b)
+      //! Return whether the jacobian matrix is kept across calls to apply().
+      bool keepMatrix() const
       {
-        hangingNodeModifications=b;
+        return _keep_matrix;
       }
 
       const Result& result() const
       {
-        return res;
+        return _res;
       }
 
-      void apply (V& x_) {
-        x = &x_;
+      void apply(V& x) {
+        _x = &x;
         apply();
       }
 
@@ -82,89 +127,104 @@ namespace Dune {
         // assemble matrix; optional: assemble only on demand!
         watch.reset();
 
-        M m(gos); 
-
-        timing = watch.elapsed();
-        // timing = gos.trialGridFunctionSpace().gridView().comm().max(timing);
-        if (gos.trialGridFunctionSpace().gridView().comm().rank()==0 && verbose>=1)
-          std::cout << "=== matrix setup (max) " << timing << " s" << std::endl;
-        watch.reset();
-        assembler_time += timing;
-
-        m = 0.0;
-        if (hangingNodeModifications)
+        if (!_jacobian)
           {
-            Dune::PDELab::set_shifted_dofs(gos.localAssembler().trialConstraints(),0.0,*x); // set hanging node DOFs to zero
-            gos.localAssembler().backtransform(*x); // interpolate hanging nodes adjacent to Dirichlet nodes
+            _jacobian = make_shared<M>(_go);
+            timing = watch.elapsed();
+            if (_go.trialGridFunctionSpace().gridView().comm().rank()==0 && _verbose>=1)
+              std::cout << "=== matrix setup (max) " << timing << " s" << std::endl;
+            watch.reset();
+            assembler_time += timing;
           }
-        gos.jacobian(*x,m);
+        else if (_go.trialGridFunctionSpace().gridView().comm().rank()==0 && _verbose>=1)
+          std::cout << "=== matrix setup skipped (matrix already allocated)" << std::endl;
+
+        (*_jacobian) = 0.0;
+        if (_hanging_node_modifications)
+          {
+            Dune::PDELab::set_shifted_dofs(_go.localAssembler().trialConstraints(),0.0,*_x); // set hanging node DOFs to zero
+            _go.localAssembler().backtransform(*_x); // interpolate hanging nodes adjacent to Dirichlet nodes
+          }
+        _go.jacobian(*_x,*_jacobian);
 
         timing = watch.elapsed();
         // timing = gos.trialGridFunctionSpace().gridView().comm().max(timing);
-        if (gos.trialGridFunctionSpace().gridView().comm().rank()==0 && verbose>=1)
+        if (_go.trialGridFunctionSpace().gridView().comm().rank()==0 && _verbose>=1)
           std::cout << "=== matrix assembly (max) " << timing << " s" << std::endl;
         assembler_time += timing;
 
         // assemble residual
         watch.reset();
 
-        W r(gos.testGridFunctionSpace(),0.0);
-        gos.residual(*x,r);  // residual is additive
+        W r(_go.testGridFunctionSpace(),0.0);
+        _go.residual(*_x,r);  // residual is additive
 
         timing = watch.elapsed();
         // timing = gos.trialGridFunctionSpace().gridView().comm().max(timing);
-        if (gos.trialGridFunctionSpace().gridView().comm().rank()==0 && verbose>=1)
+        if (_go.trialGridFunctionSpace().gridView().comm().rank()==0 && _verbose>=1)
           std::cout << "=== residual assembly (max) " << timing << " s" << std::endl;
         assembler_time += timing;
-        res.assembler_time = assembler_time;
+        _res.assembler_time = assembler_time;
 
-        typename V::ElementType defect = ls.norm(r);
+        typename V::ElementType defect = _ls.norm(r);
 
         // compute correction
         watch.reset();
-        V z(gos.trialGridFunctionSpace(),0.0);
-        typename V::ElementType red = std::min(reduction,defect/mindefect);
-        if (gos.trialGridFunctionSpace().gridView().comm().rank()==0)
+        V z(_go.trialGridFunctionSpace(),0.0);
+        typename V::ElementType red = std::min(_reduction,defect/_min_defect);
+        if (_go.trialGridFunctionSpace().gridView().comm().rank()==0)
           std::cout << "=== solving (reduction: " << red << ") ";
-        ls.apply(m,z,r,red); // solver makes right hand side consistent
-        linearsolverresult = ls.result();
+        _ls.apply(*_jacobian,z,r,red); // solver makes right hand side consistent
+        _linear_solver_result = _ls.result();
         timing = watch.elapsed();
         // timing = gos.trialGridFunctionSpace().gridView().comm().max(timing);
-        if (gos.trialGridFunctionSpace().gridView().comm().rank()==0 && verbose>=1)
+        if (_go.trialGridFunctionSpace().gridView().comm().rank()==0 && _verbose>=1)
           std::cout << timing << " s" << std::endl;
-        res.linear_solver_time = timing;
+        _res.linear_solver_time = timing;
 
-        res.converged = linearsolverresult.converged;
-        res.iterations = linearsolverresult.iterations;
-        res.elapsed = linearsolverresult.elapsed;
-        res.reduction = linearsolverresult.reduction;
-        res.conv_rate = linearsolverresult.conv_rate;
-        res.first_defect = defect;
-        res.defect = defect*reduction;
-        res.linear_solver_iterations = linearsolverresult.iterations;
+        _res.converged = _linear_solver_result.converged;
+        _res.iterations = _linear_solver_result.iterations;
+        _res.elapsed = _linear_solver_result.elapsed;
+        _res.reduction = _linear_solver_result.reduction;
+        _res.conv_rate = _linear_solver_result.conv_rate;
+        _res.first_defect = defect;
+        _res.defect = defect*_reduction;
+        _res.linear_solver_iterations = _linear_solver_result.iterations;
 
         // and update
-        if (hangingNodeModifications)
-          Dune::PDELab::set_shifted_dofs(gos.localAssembler().trialConstraints(),0.0,*x); // set hanging node DOFs to zero
-        *x -= z;
-        if (hangingNodeModifications)
-          gos.localAssembler().backtransform(*x); // interpolate hanging nodes adjacent to Dirichlet nodes
+        if (_hanging_node_modifications)
+          Dune::PDELab::set_shifted_dofs(_go.localAssembler().trialConstraints(),0.0,*_x); // set hanging node DOFs to zero
+        *_x -= z;
+        if (_hanging_node_modifications)
+          _go.localAssembler().backtransform(*_x); // interpolate hanging nodes adjacent to Dirichlet nodes
+
+        if (!_keep_matrix)
+          _jacobian = nullptr;
+      }
+
+      //! Discard the stored Jacobian matrix.
+      void discardMatrix()
+      {
+        if(_jacobian)
+          _jacobian = nullptr;
       }
 
       const Dune::PDELab::LinearSolverResult<double>& ls_result() const{
-        return linearsolverresult;
+        return _linear_solver_result;
       }
 
     private:
-      const GOS& gos;
-      LS& ls;
-      V* x;
-      typename V::ElementType reduction;
-      typename V::ElementType mindefect;
-      Dune::PDELab::LinearSolverResult<double> linearsolverresult;
-      Result res;
-      bool hangingNodeModifications;
-      int verbose;
+      const GO& _go;
+      LS& _ls;
+      V* _x;
+      shared_ptr<M> _jacobian;
+      typename V::ElementType _reduction;
+      typename V::ElementType _min_defect;
+      Dune::PDELab::LinearSolverResult<double> _linear_solver_result;
+      Result _res;
+      bool _hanging_node_modifications;
+      bool _keep_matrix;
+      int _verbose;
     };
 
   } // namespace PDELab
