3a26f7001e54d01cdfd98d7f1d501bcc8d09707f
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2014-05-15 11:24:42 +0200

    [GridOperator] Use new method scatter_jacobian() in JacobianEngine
    Use scatter_jacobian() to enable optimized scatter code if there are no
    non-Dirichlet constraints.
    
    The symmetric assembly mode is disabled for now.

--- dune/pdelab/gridoperator/default/jacobianengine.hh
+++ dune/pdelab/gridoperator/default/jacobianengine.hh
@@ -148,7 +148,7 @@ namespace Dune{
       //! @{
       template<typename EG, typename LFSUC, typename LFSVC>
       void onUnbindLFSUV(const EG & eg, const LFSUC & lfsu_cache, const LFSVC & lfsv_cache){
-        local_assembler.etadd(al,global_a_ss_view);
+        local_assembler.scatter_jacobian(al,global_a_ss_view,false);
       }
 
       template<typename IG, typename LFSUC, typename LFSVC>
@@ -156,9 +156,9 @@ namespace Dune{
                                 const LFSUC & lfsu_s_cache, const LFSVC & lfsv_s_cache,
                                 const LFSUC & lfsu_n_cache, const LFSVC & lfsv_n_cache)
       {
-        local_assembler.etadd(al_sn,global_a_sn_view);
-        local_assembler.etadd(al_ns,global_a_ns_view);
-        local_assembler.etadd(al_nn,global_a_nn_view);
+        local_assembler.scatter_jacobian(al_sn,global_a_sn_view,false);
+        local_assembler.scatter_jacobian(al_ns,global_a_ns_view,false);
+        local_assembler.scatter_jacobian(al_nn,global_a_nn_view,false);
       }
 
       //! @}
