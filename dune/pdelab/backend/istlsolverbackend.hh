daafa598721cd7e6ffc23d160b8c3ad78e8688de
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2015-06-24 11:02:05 +0200

    [Backend][ISTL] move istl-related files into istl subfolder
    the istl related files in the backend folders have been moved to the istl subfolder and a deprecation warning has been added to the old files.

--- dune/pdelab/backend/istlsolverbackend.hh
+++ dune/pdelab/backend/istlsolverbackend.hh
@@ -1,18 +1,6 @@
 // -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=8 sw=2 sts=2:
 #ifndef DUNE_ISTLSOLVERBACKEND_HH
-#define DUNE_ISTLSOLVERBACKEND_HH
-
-#include "seqistlsolverbackend.hh"
-#include "ovlpistlsolverbackend.hh"
-#include "novlpistlsolverbackend.hh"
-
-  /**
-   * @brief For better handling istlsolverbackend.hh is now divided into:
-   * parallelistlhelper.hh for creation of index sets (mainly for amg)
-   * seqistlsolverbackend.hh for sequential solvers
-   * ovlpistlsolverbackend.hh for overlapping solvers,operators,...
-   * novlpistlsolverbackend.hh with nonoverlapping solvers,operators,...
-   */
-
+#warning "The file dune/pdelab/backend/istlsolverbackend.hh is deprecated. Please use dune/pdelab/backend/istl/istlsolverbackend.hh instead."
+#include <dune/pdelab/backend/istl/istlsolverbackend.hh>
 #endif
