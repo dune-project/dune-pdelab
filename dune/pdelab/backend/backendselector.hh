5c0e9147acbb892f816392e775f637898653d5fc
Steffen Müthing <muething@dune-project.org>
2015-06-26 11:38:13 +0200

    [Backend] Rename backendselector.hh -> interface.hh
    This file contains a collection of interface classes to unify the usage
    of different LA backends. This will extend beyond just the vector and
    matrix selectors, so give the file a better name.

--- dune/pdelab/backend/backendselector.hh
+++ dune/pdelab/backend/backendselector.hh
@@ -1,70 +1,8 @@
 #ifndef DUNE_PDELAB_BACKENDSELECTOR_HH
 #define DUNE_PDELAB_BACKENDSELECTOR_HH
-namespace Dune {
-  namespace PDELab {
 
-    template<typename Backend, typename GridFunctionSpace, typename FieldType>
-    struct BackendVectorSelectorHelper
-    {};
+#warning This file has been deprecated and will be removed after the release of PDELab 2.4. Use dune/pdelab/backend/interface.hh instead
 
-    template<typename GridFunctionSpace, typename FieldType>
-    struct BackendVectorSelector
-    {
-      typedef typename GridFunctionSpace::Traits::Backend Backend;
-      typedef typename BackendVectorSelectorHelper<Backend, GridFunctionSpace, FieldType>::Type Type;
-    };
-
-    template<typename Backend, typename VU, typename VV, typename E>
-    struct BackendMatrixSelector
-    {
-      typedef typename Backend::template MatrixHelper<VV,VU,E>::type Type;
-    };
-
-    namespace Backend
-    {
-      /**
-       * \brief alias of the return type of BackendVectorSelector
-       *
-       * This alias can be used as a short hand for retrieving the vector type for
-       * a grid function space and a given field type. The typedef
-       * \code
-       * typedef typename Dune::PDELab::BackendVectorSelector<GFS,FT>::Type Vec;
-       * \endcode
-       * simplifies to
-       * \code
-       * typedef Dune::PDELab::Backend::Vector<GFS,FT> Vec;
-       * \endcode
-       * or
-       * \code
-       * using Vec = Dune::PDELab::Backend::Vector<GFS,FT>;
-       * \endcode
-       *
-       **/
-      template<typename GridFunctionSpace, typename FieldType>
-      using Vector = typename BackendVectorSelector<GridFunctionSpace, FieldType>::Type;
-
-      /**
-       * \brief alias of the return type of BackendMatrixSelector
-       *
-       * This alias can be used as a short hand for retrieving the matrix type for
-       * given matrix backend, domain, range and field type.
-       * \code
-       * typedef typename Dune::PDELab::BackendMatrixSelector<Backend,VU,VV,E>::Type Mat;
-       * \endcode
-       * simplifies to
-       * \code
-       * typedef Dune::PDELab::Backend::Matrix<Backend,VU,VV,E> Mat;
-       * \endcode
-       * or
-       * \code
-       * using Mat = Dune::PDELab::Backend::Matrix<Backend,VU,VV,E>;
-       * \endcode
-       *
-       **/
-      template<typename Backend, typename VU, typename VV, typename E>
-      using Matrix = typename BackendMatrixSelector<Backend, VU, VV, E>::Type;
-    }
-  }
-}
+#include <dune/pdelab/backend/interface.hh>
 
 #endif
---
+++ dune/pdelab/backend/interface.hh
@@ -0,0 +1,71 @@
+#ifndef DUNE_PDELAB_BACKEND_INTERFACE_HH
+#define DUNE_PDELAB_BACKEND_INTERFACE_HH
+
+namespace Dune {
+  namespace PDELab {
+
+    template<typename Backend, typename GridFunctionSpace, typename FieldType>
+    struct BackendVectorSelectorHelper
+    {};
+
+    template<typename GridFunctionSpace, typename FieldType>
+    struct BackendVectorSelector
+    {
+      typedef typename GridFunctionSpace::Traits::Backend Backend;
+      typedef typename BackendVectorSelectorHelper<Backend, GridFunctionSpace, FieldType>::Type Type;
+    };
+
+    template<typename Backend, typename VU, typename VV, typename E>
+    struct BackendMatrixSelector
+    {
+      typedef typename Backend::template MatrixHelper<VV,VU,E>::type Type;
+    };
+
+    namespace Backend
+    {
+      /**
+       * \brief alias of the return type of BackendVectorSelector
+       *
+       * This alias can be used as a short hand for retrieving the vector type for
+       * a grid function space and a given field type. The typedef
+       * \code
+       * typedef typename Dune::PDELab::BackendVectorSelector<GFS,FT>::Type Vec;
+       * \endcode
+       * simplifies to
+       * \code
+       * typedef Dune::PDELab::Backend::Vector<GFS,FT> Vec;
+       * \endcode
+       * or
+       * \code
+       * using Vec = Dune::PDELab::Backend::Vector<GFS,FT>;
+       * \endcode
+       *
+       **/
+      template<typename GridFunctionSpace, typename FieldType>
+      using Vector = typename BackendVectorSelector<GridFunctionSpace, FieldType>::Type;
+
+      /**
+       * \brief alias of the return type of BackendMatrixSelector
+       *
+       * This alias can be used as a short hand for retrieving the matrix type for
+       * given matrix backend, domain, range and field type.
+       * \code
+       * typedef typename Dune::PDELab::BackendMatrixSelector<Backend,VU,VV,E>::Type Mat;
+       * \endcode
+       * simplifies to
+       * \code
+       * typedef Dune::PDELab::Backend::Matrix<Backend,VU,VV,E> Mat;
+       * \endcode
+       * or
+       * \code
+       * using Mat = Dune::PDELab::Backend::Matrix<Backend,VU,VV,E>;
+       * \endcode
+       *
+       **/
+      template<typename Backend, typename VU, typename VV, typename E>
+      using Matrix = typename BackendMatrixSelector<Backend, VU, VV, E>::Type;
+    }
+  }
+}
+
+#endif // DUNE_PDELAB_BACKEND_INTERFACE_HH
