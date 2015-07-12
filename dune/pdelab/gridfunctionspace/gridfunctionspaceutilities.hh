76c9c35bbff82eddcbdefc889ab10b4e38226c4a
Marian Piatkowski <marian.piatkowski@iwr.uni-heidelberg.de>
2015-07-12 14:36:43 +0200

    remove TODO notes in "gridfunctionspaceutilities.hh"

--- dune/pdelab/gridfunctionspace/gridfunctionspaceutilities.hh
+++ dune/pdelab/gridfunctionspace/gridfunctionspaceutilities.hh
@@ -12,10 +12,6 @@
 #include <dune/localfunctions/common/interfaceswitch.hh>
 
 #include"../common/function.hh"
-//--------------------------------------------//
-// TODO
-// Is the header "jacobiantocurl.hh" really used?
-//--------------------------------------------//
 #include <dune/pdelab/common/jacobiantocurl.hh>
 #include"gridfunctionspace.hh"
 #include <dune/pdelab/gridfunctionspace/localfunctionspace.hh>
@@ -180,10 +176,6 @@ namespace Dune {
      * \tparam T Type of GridFunctionSpace
      * \tparam X Type of coefficients vector
      */
-    //--------------------------------------------//
-    // TODO
-    // Is the following class really used?
-    //--------------------------------------------//
     template<typename T, typename X>
     class DiscreteGridFunctionCurl :
       public GridFunctionInterface<
@@ -269,10 +261,6 @@ namespace Dune {
       std::shared_ptr<const X> px; // FIXME: dummy pointer to make sure we take ownership of X
     };
 
-    //--------------------------------------------//
-    // TODO
-    // Are the following traits really used?
-    //--------------------------------------------//
     //! Helper class to calculate the Traits of DiscreteGridFunctionCurl
     /**
      * \tparam GV              Type of the GridView.
@@ -358,10 +346,6 @@ namespace Dune {
      *           evaluateJacobianGlobal() method.
      * \tparam X Type of coefficients vector
      */
-    //--------------------------------------------//
-    // TODO
-    // Is the following class really used?
-    //--------------------------------------------//
     template<typename T, typename X>
     class DiscreteGridFunctionGlobalCurl
       : public GridFunctionInterface<
