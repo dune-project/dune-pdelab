bada52ae4bc8b90d18b8d03e6ac3745683624e3f
Steffen Müthing <muething@dune-project.org>
2015-10-06 12:13:28 +0500

    [Assembler] Upgrade IntersectionType helper for EntitySets
    The IntersectionType helper from the GridOperator utilities is used
    inside the GridOperator assembler to classify the type of an
    intersection (boundary, skeleton etc.).
    
    This patch updates the IntersectionType classifier to correctly handle
    the fact that an EntitySet might not span the whole underlying
    GridView. As this extended test will often require access to the outside
    entity, the new classifier function returns the outside entity along
    with the IntersectionType.
    
    The patch also moves the whole classifier machinery to a separate
    header, as the constraints assembler (which is pretty broken right now)
    will also have to use it in the future.

---
+++ dune/pdelab/common/intersectiontype.hh
@@ -0,0 +1,53 @@
+#ifndef DUNE_PDELAB_COMMON_INTERSECTIONTYPE_HH
+#define DUNE_PDELAB_COMMON_INTERSECTIONTYPE_HH
+
+namespace Dune {
+  namespace PDELab {
+
+    //! Enum describing the type of an intersection.
+    enum class IntersectionType
+    {
+
+      processor = 0, //!< processor boundary intersection (neighbor() == false && boundary() == false) or outside entity not in EntitySet
+      skeleton = 1,  //!< skeleton intersection (neighbor() == true && boundary() == false)
+      boundary = 2,  //!< domain boundary intersection (neighbor() == false && boundary() == true)
+      periodic = 3   //!< periodic boundary intersection (neighbor() == true && boundary() == true)
+
+    };
+
+    //! Classifies the type of an intersection wrt to the passed EntitySet.
+    /**
+     * This function classifies the neighborship type of an intersection. It mirrors
+     * the classification provided by the Intersection::boundary() and
+     * Intersection::neighbor() methods, but also takes into account an EntitySet that
+     * might not span all parallel PartitionTypes.
+     *
+     * As the classification often requires the function to obtain the outside entity, the function
+     * returns a tuple containing both the IntersectionType and the outside entity to avoid obtaining
+     * the outside entity twice. In case of a boundary intersection where an outside entity is not available,
+     * it will return a default-constructed (invalid) entity.
+     */
+    template<typename EntitySet, typename Intersection>
+    std::tuple<IntersectionType,typename EntitySet::Element> classifyIntersection(const EntitySet& entity_set, const Intersection& is)
+    {
+      auto type = static_cast<IntersectionType>(1* is.neighbor() + 2*is.boundary());
+      if (type == IntersectionType::skeleton)
+        if (entity_set.partitions() == Partitions::all)
+          return std::make_tuple(type,is.outside());
+        else
+          {
+            auto outside_entity = is.outside();
+            if (entity_set.partitions().contains(outside_entity.partitionType()))
+              return std::make_tuple(type,outside_entity);
+            else
+              return std::make_tuple(IntersectionType::processor,std::move(outside_entity));
+          }
+      else
+        return std::make_tuple(type,decltype(is.outside()){});
+    }
+
+
+  } // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_PDELAB_COMMON_INTERSECTIONTYPE_HH
