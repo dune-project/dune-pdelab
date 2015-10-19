1e53474f8666bf35ccbeacf997b6f78e2d8ee40d
Steffen Müthing <muething@dune-project.org>
2015-10-19 15:44:45 +0200

    [Cleanup] Remove deprecated files in backend directory

--- dune/pdelab/backend/istlvectorbackend.hh
+++
@@ -1,6 +0,0 @@
-// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_PDELAB_BACKEND_ISTLVECTORBACKEND_HH
-#warning "The file dune/pdelab/backend/istlvectorbackend.hh is deprecated. Please use dune/pdelab/backend/istl.hh instead."
-#include <dune/pdelab/backend/istl.hh>
-#endif // DUNE_PDELAB_BACKEND_ISTLVECTORBACKEND_HH
