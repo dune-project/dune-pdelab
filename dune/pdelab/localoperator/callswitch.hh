afc236fc79eba600f027a4425cf36f710371e00a
René Heß <rene.hess@iwr.uni-heidelberg.de>
2015-11-17 11:16:11 +0100

    [Bugfix] add Buffer to callswitch false methods

--- dune/pdelab/localoperator/callswitch.hh
+++ dune/pdelab/localoperator/callswitch.hh
@@ -164,16 +164,16 @@ namespace Dune {
         M & mat_ss)
       {
       }
-      template<typename IG, typename LFSU, typename X, typename LFSV, typename M>
+      template<typename IG, typename LFSU, typename X, typename LFSV, typename M, typename Buf>
       static void jacobian_process_boundary_gather (const LA& la, const IG& ig,
-        const LFSU& lfsu_s, const X& x_s, const LFSV& lfsv_s,
-        M & mat_ss)
+                                                    const LFSU& lfsu_s, const X& x_s, const LFSV& lfsv_s,
+                                                    M & mat_ss, Buf& buf)
       {
       }
-      template<typename IG, typename LFSU, typename X, typename LFSV, typename M>
+      template<typename IG, typename LFSU, typename X, typename LFSV, typename M, typename Buf>
       static void jacobian_process_boundary_scatter (const LA& la, const IG& ig,
-        const LFSU& lfsu_s, const X& x_s, const LFSV& lfsv_s,
-        M & mat_ss)
+                                                     const LFSU& lfsu_s, const X& x_s, const LFSV& lfsv_s,
+                                                     M & mat_ss, Buf& buf)
       {
       }
       static bool jacobianCommunicationFixedSize(const LA& la)
@@ -182,7 +182,7 @@ namespace Dune {
       }
       template <typename IG, typename LFSU, typename LFSV>
       static size_t jacobianCommunicationSize(const LA& la, const IG& ig,
-                                                   const LFSU& lfsu_s, const LFSV& lfsv_s)
+                                              const LFSU& lfsu_s, const LFSV& lfsv_s)
       {
         return 0.0;
       }
