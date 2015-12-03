6a403c8378fb113040af1074e7c2813f4aa0bfb2
René Heß <rene.hess@iwr.uni-heidelberg.de>
2015-12-03 16:02:48 +0100

    change header guard

--- dune/pdelab/finiteelementmap/qkdg.hh
+++ dune/pdelab/finiteelementmap/qkdg.hh
@@ -1,8 +1,8 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 
-#ifndef DUNE_QkDG_LOCALFINITEELEMENT_HH
-#define DUNE_QkDG_LOCALFINITEELEMENT_HH
+#ifndef DUNE_PDELAB_FINITEELEMENTMAP_QKDG_HH
+#define DUNE_PDELAB_FINITEELEMENTMAP_QKDG_HH
 
 #include <dune/common/fvector.hh>
 #include <dune/common/deprecated.hh>
