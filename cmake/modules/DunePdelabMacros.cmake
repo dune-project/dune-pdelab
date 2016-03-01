f3c8a092a2bb4d504a6b4cb42c29cc6e235b8496
Steffen Müthing <muething@dune-project.org>
2016-03-01 10:52:28 +0100

    [cmake][typo] Fix indentation

--- cmake/modules/DunePdelabMacros.cmake
+++ cmake/modules/DunePdelabMacros.cmake
@@ -32,7 +32,7 @@ endfunction(add_dune_petsc_flags)
 # will result in very subtle errors. We therefore issue a warning
 # and set a preprocessor variable to give meaningful error messages.
 if(MPI_FOUND AND UG_FOUND)
-    if(NOT (UG_PARALLEL STREQUAL "yes"))
+  if(NOT (UG_PARALLEL STREQUAL "yes"))
     message(WARNING "You are using a sequential UG in a parallel PDELab environment!")
     set(PDELAB_SEQUENTIAL_UG 1)
   endif()
