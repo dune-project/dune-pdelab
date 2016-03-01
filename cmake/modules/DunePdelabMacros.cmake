44d86a3121ea7fea4d1069ef7c9fca5546858de0
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2016-03-01 09:53:20 +0000

    Merge branch 'feature/fix-parallel-ug-assertion' into 'master'
    Fix parallel UG assertion
    
    Fix some problems with the new sequential UG warning feature.
    
    See merge request !136
    (cherry picked from commit fb1c56b16d193aca07dd03dcae44b90f0d764636)
    
    Signed-off-by: Steffen Müthing <muething@dune-project.org>

--- cmake/modules/DunePdelabMacros.cmake
+++ cmake/modules/DunePdelabMacros.cmake
@@ -33,8 +33,7 @@ endfunction(add_dune_petsc_flags)
 # will result in very subtle errors. We therefore issue a warning
 # and set a preprocessor variable to give meaningful error messages.
 if(MPI_FOUND AND UG_FOUND)
-  list(FIND CMAKE_CXX_FLAGS ModelP position)
-  if(${position} EQUAL -1)
+  if(NOT (UG_PARALLEL STREQUAL "yes"))
     message(WARNING "You are using a sequential UG in a parallel PDELab environment!")
     set(PDELAB_SEQUENTIAL_UG 1)
   endif()
