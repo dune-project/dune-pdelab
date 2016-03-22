7d3ba3bf3ba93f0db4f7c7591ff058ed565cf609
René Heß <rene.hess@iwr.uni-heidelberg.de>
2016-03-22 15:02:44 +0100

    Remove deprecations from gridfunctionspace/

--- dune/pdelab/gridfunctionspace/utility.hh
+++
@@ -1,9 +0,0 @@
-// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_PDELAB_GRIDFUNCTIONSPACE_UTILITY_HH
-#define DUNE_PDELAB_GRIDFUNCTIONSPACE_UTILITY_HH
-
-#warning dune/pdelab/gridfunctionspace/utility.hh is deprecated, please use dune/pdelab/constraints/noconstraints.hh instead
-#include <dune/pdelab/constraints/noconstraints.hh>
-
-#endif // DUNE_PDELAB_GRIDFUNCTIONSPACE_UTILITY_HH
