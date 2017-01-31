f61ef476ba79b03b2bc7c97f9154036be66c41ef
Jö Fahlke <jorrit@jorrit.de>
2017-01-31 17:04:46 +0100

    [testelectrodynamic] More modernization.

--- dune/pdelab/test/testelectrodynamic.cc
+++ dune/pdelab/test/testelectrodynamic.cc
@@ -2,6 +2,10 @@
  *  @brief Unit test for `Electrodymic_S` and `Electrodynamic_T` operators
  *
  * This was adapted from Andreas Buhr's circ_in_rect_2d test program.
+ *
+ * Despite the name, it does not actually do a time-domain simulation; instead
+ * it does a frequency-domain simulation.  But is does make use of the
+ * operators that were originally intended for time-domain simulations.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -9,13 +13,16 @@
 #endif
 
 #include <iostream>
-#include <memory>
+// #include <memory>
+#include <string>
+#include <type_traits>
 #include <vector>
 
-#include <dune/common/exceptions.hh>         // We use exceptions
-#include <dune/common/parallel/mpihelper.hh> // An initializer of MPI
+// #include <dune/common/exceptions.hh>
+#include <dune/common/fvector.hh>
+#include <dune/common/parallel/mpihelper.hh>
 
-#include <dune/localfunctions/common/interfaceswitch.hh>
+// #include <dune/localfunctions/common/interfaceswitch.hh>
 
 #include <dune/grid/common/gridinfo.hh>
 #include <dune/grid/io/file/gmshreader.hh>
@@ -24,31 +31,41 @@
 
 #include <dune/alugrid/grid.hh>
 
-#include <dune/istl/bvector.hh>
-#include <dune/istl/io.hh>
+// #include <dune/istl/bvector.hh>
+// #include <dune/istl/io.hh>
 #include <dune/istl/matrixmarket.hh>
-#include <dune/istl/operators.hh>
-#include <dune/istl/preconditioners.hh>
-#include <dune/istl/solvers.hh>
-#include <dune/istl/superlu.hh>
+// #include <dune/istl/operators.hh>
+// #include <dune/istl/preconditioners.hh>
+// #include <dune/istl/solvers.hh>
+// #include <dune/istl/superlu.hh>
 
+#include <dune/pdelab/backend/interface.hh>
 #include <dune/pdelab/backend/istl.hh>
 #include <dune/pdelab/common/quadraturerules.hh>
+#include <dune/pdelab/constraints/common/constraints.hh>
+#include <dune/pdelab/constraints/common/constraintsparameters.hh>
 #include <dune/pdelab/constraints/conforming.hh>
-#include <dune/pdelab/constraints/noconstraints.hh>
 #include <dune/pdelab/finiteelementmap/edges0.5fem.hh>
+#include <dune/pdelab/gridfunctionspace/gridfunctionspace.hh>
+#include <dune/pdelab/gridfunctionspace/vtk.hh>
 #include <dune/pdelab/gridoperator/gridoperator.hh>
 #include <dune/pdelab/localoperator/electrodynamic.hh>
+#include <dune/pdelab/localoperator/flags.hh>
+#include <dune/pdelab/localoperator/numericalresidual.hh>
+#include <dune/pdelab/localoperator/pattern.hh>
 #include <dune/pdelab/stationary/linearproblem.hh>
 
 // store physical entitiy information from GmshReader
-class MeshInfo {
-public:
+struct MeshInfo
+{
+  // note: we rely on grids where we can use the boundary segment index to
+  // subscript the array of physical entities for the boundary intersections.
   std::vector<int> boundary_id_to_physical_entity;
   std::vector<int> element_index_to_physical_entity;
 };
 
-const double pi = 3.14159265358979323846264338328;
+// no M_PI in C++ standard, and acos(-1.0) ain't constexpr (may set errno)
+constexpr double pi = 3.14159265358979323846264338328;
 
 template <typename Eps, typename Mu>
 class Electrodynamic_Full :
@@ -66,13 +83,13 @@ public:
     : edyn_s_lop_(mu, qorder)
     , edyn_t_lop_(eps, qorder)
     , meshinfo_(m)
-    , frequency_(frequency) {
-    // empty
-  }
+    , frequency_(frequency)
+  { /* empty */ }
 
   template <typename EG, typename LFS, typename X, typename M>
   void jacobian_volume(const EG& eg, const LFS& lfsu, const X& x,
-                       const LFS& lfsv, M& mat) const {
+                       const LFS& lfsv, M& mat) const
+  {
     // first assemble curl curl part:
     edyn_s_lop_.jacobian_volume(eg, lfsu, x, lfsv, mat);
 
@@ -91,9 +108,9 @@ public:
   void lambda_boundary(const IG& ig, const LFSV& lfsv_s, R& r_s) const {
     using BasisTraits =
       typename LFSV::Traits::FiniteElementType::Traits::Basis::Traits;
-
     using Range = typename BasisTraits::Range;
 
+    // see note in MeshInfo
     int boundarySegmentIndex = ig.intersection().boundarySegmentIndex();
     int physical_id =
         meshinfo_.boundary_id_to_physical_entity[boundarySegmentIndex];
@@ -113,17 +130,16 @@ public:
         lfsv_s.finiteElement().basis().evaluateFunction(
             geo.global(quadraturepoint.position()), phi);
 
+        auto factor = frequency_ * quadraturepoint.weight();
         for (unsigned i = 0; i < lfsv_s.size(); ++i) {
-          r_s.accumulate(lfsv_s, i, frequency_ *
-                                        (phi[i] * tangent) *
-                                        quadraturepoint.weight());
+          r_s.accumulate(lfsv_s, i, factor * (phi[i] * tangent));
         }
       }
     }
   }
 
 private:
-  Dune::PDELab::Electrodynamic_S<Mu> edyn_s_lop_;
+  Dune::PDELab::Electrodynamic_S<Mu>  edyn_s_lop_;
   Dune::PDELab::Electrodynamic_T<Eps> edyn_t_lop_;
 
   const MeshInfo& meshinfo_;
@@ -158,14 +174,18 @@ struct Eps {
   };
 };
 
-class BCTypeParam : public Dune::PDELab::DirichletConstraintsParameters {
+class BCTypeParam :
+  public Dune::PDELab::DirichletConstraintsParameters
+{
 public:
   BCTypeParam(const MeshInfo& m) : meshinfo_(m) {}
 
   template <typename IG>
   bool isDirichlet(const IG& intersection,
                    const Dune::FieldVector<typename IG::ctype,
-                                           IG::dimension - 1>&) const {
+                                           IG::dimension - 1>&) const
+  {
+    // see note in MeshInfo
     int boundarySegmentIndex =
         intersection.intersection().boundarySegmentIndex();
     int physical_id =
@@ -179,144 +199,125 @@ public:
 };
 
 int main(int argc, char** argv) {
-  try {
-    // Maybe initialize Mpi
-    Dune::MPIHelper::instance(argc, argv);
+  // Maybe initialize Mpi
+  Dune::MPIHelper::instance(argc, argv);
 
-    // here we start something real:
+  // here we start something real:
 
-    std::string nameprefix = "testelectrodynamic";
-    std::string meshfilename = "mesh.msh";
-    if(argc >= 2)
-      meshfilename = argv[1];
+  std::string nameprefix = "testelectrodynamic";
+  std::string meshfilename = "mesh.msh";
+  if(argc >= 2)
+    meshfilename = argv[1];
 
-    // create the grid
-    const int dim = 2;
-    using Grid = Dune::ALUGrid<dim, dim, Dune::simplex, Dune::conforming>;
+  // create the grid
+  const int dim = 2;
+  using Grid = Dune::ALUGrid<dim, dim, Dune::simplex, Dune::conforming>;
 
-    MeshInfo meshinfo;
-    Dune::shared_ptr<Grid> mygrid(Dune::GmshReader<Grid>::read(
-        meshfilename, meshinfo.boundary_id_to_physical_entity,
-        meshinfo.element_index_to_physical_entity));
-    Dune::gridinfo(*mygrid);
+  MeshInfo meshinfo;
+  auto mygrid(Dune::GmshReader<Grid>::read(
+      meshfilename, meshinfo.boundary_id_to_physical_entity,
+      meshinfo.element_index_to_physical_entity));
+  Dune::gridinfo(*mygrid);
 
-    BCTypeParam bctype(meshinfo);
+  BCTypeParam bctype(meshinfo);
 
-    // now we need a vertex order
-    using Geometry = Grid::Codim<0>::Geometry;
-    using VertexOrderFactory = Dune::VertexOrderByIdFactory<Grid::GlobalIdSet>;
-    VertexOrderFactory vertexOrderFactory(mygrid->globalIdSet());
-    using RF = double;
-    using FiniteElementMap =
-      Dune::PDELab::EdgeS0_5FiniteElementMap<Geometry, VertexOrderFactory, RF>;
-    FiniteElementMap finiteElementMap(vertexOrderFactory);
+  // now we need a vertex order
+  using Geometry = Grid::Codim<0>::Geometry;
+  using VertexOrderFactory = Dune::VertexOrderByIdFactory<Grid::GlobalIdSet>;
+  VertexOrderFactory vertexOrderFactory(mygrid->globalIdSet());
+  using RF = double;
+  using FiniteElementMap =
+    Dune::PDELab::EdgeS0_5FiniteElementMap<Geometry, VertexOrderFactory, RF>;
+  FiniteElementMap finiteElementMap(vertexOrderFactory);
 
-    using GV = Grid::LeafGridView;
-    using CON = Dune::PDELab::ConformingDirichletConstraints;
-    // using CON = Dune::PDELab::NoConstraints;
+  using GV = Grid::LeafGridView;
+  using CON = Dune::PDELab::ConformingDirichletConstraints;
+  using VBE = Dune::PDELab::ISTL::VectorBackend<>;
+  using GFS =
+    Dune::PDELab::GridFunctionSpace<GV, FiniteElementMap, CON, VBE>;
+  GFS gfs(mygrid->leafView(), finiteElementMap);
+  gfs.name("solution"); // name for vtkwriter
 
-    using VBE = Dune::PDELab::ISTL::VectorBackend<>;
-    using GFS =
-      Dune::PDELab::GridFunctionSpace<GV, FiniteElementMap, CON, VBE>;
+  using CC = GFS::ConstraintsContainer<double>::Type;
+  CC cc;
+  Dune::PDELab::constraints(bctype, gfs, cc);
+  std::cout << "constrained dofs: " << cc.size() << " of "
+            << gfs.globalSize() << std::endl;
 
-    GFS gfs(mygrid->leafView(), finiteElementMap);
-    using CC = GFS::ConstraintsContainer<double>::Type;
-    CC cc;
+  using MBE = Dune::PDELab::ISTL::BCRSMatrixBackend<>;
+  MBE mbe(5); // the edge itself + two adjacent elements * two other edges
 
-    Dune::PDELab::constraints(bctype, gfs, cc);
+  Mu<double> mu;
+  Eps<double> eps;
 
-    std::cout << "constrained dofs: " << cc.size() << " of "
-              << gfs.globalSize() << std::endl;
+  // build matrices and write them out for analysis
+  auto writeMatrix = [&](auto &&lo, const std::string &fname) {
+    using LO = std::decay_t<decltype(lo)>;
 
-    using LocaloperatorS = Dune::PDELab::Electrodynamic_S<Mu<double> >;
-    using LocaloperatorT = Dune::PDELab::Electrodynamic_T<Eps<double> >;
-    using LocaloperatorFull = Electrodynamic_Full<Eps<double>, Mu<double> >;
-    Mu<double> mu;
-    Eps<double> eps;
-    LocaloperatorS localoperatorS(mu);
-    LocaloperatorT localoperatorT(eps);
-    LocaloperatorFull localoperatorFull(eps, mu, 3e7, meshinfo);
-    using MBE = Dune::PDELab::ISTL::BCRSMatrixBackend<>;
-    using GOS = Dune::PDELab::GridOperator<GFS, GFS, LocaloperatorS, MBE,
-                                           double, double, double, CC, CC>;
-    using GOT = Dune::PDELab::GridOperator<GFS, GFS, LocaloperatorT, MBE,
-                                           double, double, double, CC, CC>;
-    using GOFull = Dune::PDELab::GridOperator<GFS, GFS, LocaloperatorFull, MBE,
-                                              double, double, double, CC, CC>;
-    MBE mbe(5); // the edge itself + two adjacent elements * two other edges
-    GOS goS(gfs, cc, gfs, cc, localoperatorS, mbe);
-    GOT goT(gfs, cc, gfs, cc, localoperatorT, mbe);
+    using GO = Dune::PDELab::GridOperator<GFS, GFS, LO, MBE,
+                                          double, double, double, CC, CC>;
+    GO go(gfs, cc, gfs, cc, lo, mbe);
+    using U = typename GO::Traits::Domain;
+    U u(gfs, 0);
+
+    typename GO::Jacobian j(go);
+    j = 0;
+    go.jacobian(u, j);
+
+    // get rhs
+    // (strictly speaking, this has no effect besides ensuring that the
+    // residual can be computed, we don't check the result in any way
+    // currently)
+    typename GO::Range rhs(gfs);
+    go.residual(u, rhs);
+
+    Dune::storeMatrixMarket(Dune::PDELab::Backend::native(j), fname);
+  };
+
+  using LocaloperatorS    = Dune::PDELab::Electrodynamic_S<Mu<double> >;
+  writeMatrix(LocaloperatorS(mu),  nameprefix + "_matrixS.mtx");
+
+  using LocaloperatorT    = Dune::PDELab::Electrodynamic_T<Eps<double> >;
+  writeMatrix(LocaloperatorT(eps), nameprefix + "_matrixT.mtx");
+
+  using LocaloperatorFull = Electrodynamic_Full<Eps<double>, Mu<double> >;
+  writeMatrix(LocaloperatorFull(eps, mu, 3e7, meshinfo),
+              nameprefix + "_matrix_" + std::to_string((int)3e7) + ".mtx");
+
+  using GOFull = Dune::PDELab::GridOperator<GFS, GFS, LocaloperatorFull, MBE,
+                                            double, double, double, CC, CC>;
+
+  // now solve a problem
+  using LS = Dune::PDELab::ISTLBackend_SEQ_SuperLU;
+  LS ls;
+  using U = GOFull::Traits::Domain;
+  using SLP = Dune::PDELab::StationaryLinearProblemSolver<GOFull, LS, U>;
+
+  // generate some vtk output
+  // TODO: the free leafGridView() function seems broken atm (returning a
+  // gridview of a weird type)
+  const auto& gv = mygrid->leafView();
+
+  auto dofreq = [&](double frequency) {
+    LocaloperatorFull localoperatorFull(eps, mu, frequency, meshinfo);
     GOFull goFull(gfs, cc, gfs, cc, localoperatorFull, mbe);
+    U u(gfs, 0);
+    SLP slp(goFull, ls, u, 1e-10);
+    slp.apply();
+    Dune::VTKWriter<GV> myvtkwriter(gv, Dune::VTK::nonconforming);
+    Dune::PDELab::addSolutionToVTKWriter(myvtkwriter, gfs, u);
+    myvtkwriter.write(nameprefix + "_" + std::to_string((int)frequency),
+                      Dune::VTK::appendedraw);
+  };
 
-    using US = GOS::Traits::Domain;
-    using UT = GOT::Traits::Domain;
-    using UFull = GOFull::Traits::Domain;
-    US uS(gfs, 0);
-    UT uT(gfs, 0);
-    UFull uFull(gfs, 0);
+  // scan through frequencies from 1MHz to 300MHz:
+  // for (double frequency = 1e6; frequency < 3e8; frequency *= 1.05)
+  //   dofreq(frequency);
 
-    GOS::Jacobian jS(goS);
-    GOT::Jacobian jT(goT);
-    GOFull::Jacobian jFull(goFull);
-    jS = 0;
-    jT = 0;
-    jFull = 0;
+  // for the unit test:
+  // do a single frequency (the one we have a screenshot for)
+  dofreq(30426425);
 
-    goS.jacobian(uS, jS);
-    goT.jacobian(uT, jT);
-    goFull.jacobian(uFull, jFull);
-
-    // get rhs:
-    GOFull::Range rhs(gfs);
-    goFull.residual(uFull, rhs);
-    auto matrixS = Dune::PDELab::Backend::native(jS);
-    auto matrixT = Dune::PDELab::Backend::native(jT);
-    auto matrixFull = Dune::PDELab::Backend::native(jFull);
-
-    Dune::storeMatrixMarket(matrixS,    nameprefix + "_matrixS.mtx");
-    Dune::storeMatrixMarket(matrixT,    nameprefix + "_matrixT.mtx");
-    Dune::storeMatrixMarket(matrixFull, nameprefix + "_matrixFull.mtx");
-
-    // now solve a problem
-    using LS = Dune::PDELab::ISTLBackend_SEQ_SuperLU;
-    LS ls;
-    using U = GOFull::Traits::Domain;
-    using SLP = Dune::PDELab::StationaryLinearProblemSolver<GOFull, LS, U>;
-    using DGF = Dune::PDELab::DiscreteGridFunction<GFS, U>;
-
-    // generate some vtk output
-    using GV = Grid::LeafGridView;
-    const GV& gv = mygrid->leafView();
-
-    auto dofreq = [&](double frequency) {
-      LocaloperatorFull localoperatorFull2(eps, mu, frequency, meshinfo);
-      GOFull goFull2(gfs, cc, gfs, cc, localoperatorFull2, mbe);
-      U u(gfs, 0);
-      SLP slp(goFull2, ls, u, 1e-10);
-      slp.apply();
-      DGF udgf(gfs, u);
-      Dune::VTKWriter<GV> myvtkwriter(gv, Dune::VTK::nonconforming);
-      myvtkwriter.addVertexData
-        (std::make_shared<Dune::PDELab::VTKGridFunctionAdapter<DGF> >
-         (udgf, "solution"));
-      myvtkwriter.write(nameprefix + "_" + std::to_string((int)frequency),
-                        Dune::VTK::appendedraw);
-    };
-
-    // scan through frequencies from 1MHz to 300MHz:
-    // for (double frequency = 1e6; frequency < 3e8; frequency *= 1.05)
-    //   dofreq(frequency);
-
-    // for the unit test:
-    // do a single frequency (the one we have a screenshot for)
-    dofreq(30426425);
-
-    return 0;
-  }
-  catch (Dune::Exception& e) {
-    std::cerr << "Dune reported error: " << e << std::endl;
-  }
-  catch (...) {
-    std::cerr << "Unknown exception thrown!" << std::endl;
-  }
+  // Don't handle exceptions, that would prevent typical C++ libraries from
+  // printing a meaningful backtrace
 }
