617d65dfd02336487e07068326bc54c16724b3a2
Marian Piatkowski <marian.piatkowski@iwr.uni-heidelberg.de>
2017-01-31 18:56:58 +0100

    [GridOperators,Engines] improve readability of code + consistent style

--- dune/pdelab/gridoperator/default/residualengine.hh
+++ dune/pdelab/gridoperator/default/residualengine.hh
@@ -61,7 +61,8 @@ namespace Dune{
          creates this engine
       */
       DefaultLocalResidualAssemblerEngine(const LocalAssembler & local_assembler_)
-        : local_assembler(local_assembler_), lop(local_assembler_.localOperator()),
+        : local_assembler(local_assembler_),
+          lop(local_assembler_.localOperator()),
           rl_view(rl,1.0),
           rn_view(rn,1.0)
       {}
@@ -91,7 +92,10 @@ namespace Dune{
       //! @}
 
       //! Public access to the wrapping local assembler
-      const LocalAssembler & localAssembler() const { return local_assembler; }
+      const LocalAssembler & localAssembler() const
+      {
+        return local_assembler;
+      }
 
       //! Trial space constraints
       const typename LocalAssembler::Traits::TrialGridFunctionSpaceConstraints& trialConstraints() const
@@ -107,14 +111,16 @@ namespace Dune{
 
       //! Set current residual vector. Should be called prior to
       //! assembling.
-      void setResidual(Residual & residual_){
+      void setResidual(Residual & residual_)
+      {
         global_rl_view.attach(residual_);
         global_rn_view.attach(residual_);
       }
 
       //! Set current solution vector. Should be called prior to
       //! assembling.
-      void setSolution(const Solution & solution_){
+      void setSolution(const Solution & solution_)
+      {
         global_sl_view.attach(solution_);
         global_sn_view.attach(solution_);
       }
@@ -123,19 +129,22 @@ namespace Dune{
       //! global assembler.
       //! @{
       template<typename EG, typename LFSUC, typename LFSVC>
-      void onBindLFSUV(const EG & eg, const LFSUC & lfsu_cache, const LFSVC & lfsv_cache){
+      void onBindLFSUV(const EG & eg, const LFSUC & lfsu_cache, const LFSVC & lfsv_cache)
+      {
         global_sl_view.bind(lfsu_cache);
         xl.resize(lfsu_cache.size());
       }
 
       template<typename EG, typename LFSVC>
-      void onBindLFSV(const EG & eg, const LFSVC & lfsv_cache){
+      void onBindLFSV(const EG & eg, const LFSVC & lfsv_cache)
+      {
         global_rl_view.bind(lfsv_cache);
         rl.assign(lfsv_cache.size(),0.0);
       }
 
       template<typename IG, typename LFSUC, typename LFSVC>
-      void onBindLFSUVInside(const IG & ig, const LFSUC & lfsu_cache, const LFSVC & lfsv_cache){
+      void onBindLFSUVInside(const IG & ig, const LFSUC & lfsu_cache, const LFSVC & lfsv_cache)
+      {
         global_sl_view.bind(lfsu_cache);
         xl.resize(lfsu_cache.size());
       }
@@ -150,7 +159,8 @@ namespace Dune{
       }
 
       template<typename IG, typename LFSVC>
-      void onBindLFSVInside(const IG & ig, const LFSVC & lfsv_cache){
+      void onBindLFSVInside(const IG & ig, const LFSVC & lfsv_cache)
+      {
         global_rl_view.bind(lfsv_cache);
         rl.assign(lfsv_cache.size(),0.0);
       }
@@ -170,13 +180,15 @@ namespace Dune{
       //! discarded
       //! @{
       template<typename EG, typename LFSVC>
-      void onUnbindLFSV(const EG & eg, const LFSVC & lfsv_cache){
+      void onUnbindLFSV(const EG & eg, const LFSVC & lfsv_cache)
+      {
         global_rl_view.add(rl);
         global_rl_view.commit();
       }
 
       template<typename IG, typename LFSVC>
-      void onUnbindLFSVInside(const IG & ig, const LFSVC & lfsv_cache){
+      void onUnbindLFSVInside(const IG & ig, const LFSVC & lfsv_cache)
+      {
         global_rl_view.add(rl);
         global_rl_view.commit();
       }
@@ -194,26 +206,30 @@ namespace Dune{
       //! Methods for loading of the local function's coefficients
       //! @{
       template<typename LFSUC>
-      void loadCoefficientsLFSUInside(const LFSUC & lfsu_s_cache){
+      void loadCoefficientsLFSUInside(const LFSUC & lfsu_s_cache)
+      {
         global_sl_view.read(xl);
       }
       template<typename LFSUC>
-      void loadCoefficientsLFSUOutside(const LFSUC & lfsu_n_cache){
+      void loadCoefficientsLFSUOutside(const LFSUC & lfsu_n_cache)
+      {
         global_sn_view.read(xn);
       }
       template<typename LFSUC>
       void loadCoefficientsLFSUCoupling(const LFSUC & lfsu_c_cache)
-      {DUNE_THROW(Dune::NotImplemented,"No coupling lfsu available for ");}
+      {
+        DUNE_THROW(Dune::NotImplemented,"No coupling lfsu available for ");
+      }
       //! @}
 
       //! Notifier functions, called immediately before and after assembling
       //! @{
 
-      void postAssembly(const GFSU& gfsu, const GFSV& gfsv){
+      void postAssembly(const GFSU& gfsu, const GFSV& gfsv)
+      {
         if(local_assembler.doPostProcessing())
-          {
-            Dune::PDELab::constrain_residual(local_assembler.testConstraints(),global_rl_view.container());
-          }
+          Dune::PDELab::constrain_residual(local_assembler.testConstraints(),global_rl_view.container());
+
       }
 
       //! @}
@@ -292,14 +308,18 @@ namespace Dune{
                                              const LFSUC & lfsu_s_cache, const LFSVC & lfsv_s_cache,
                                              const LFSUC & lfsu_n_cache, const LFSVC & lfsv_n_cache,
                                              const LFSUC & lfsu_coupling_cache, const LFSVC & lfsv_coupling_cache)
-      {DUNE_THROW(Dune::NotImplemented,"Assembling of coupling spaces is not implemented for ");}
+      {
+        DUNE_THROW(Dune::NotImplemented,"Assembling of coupling spaces is not implemented for ");
+      }
 
       template<typename IG, typename LFSVC>
       static void assembleVEnrichedCoupling(const IG & ig,
                                             const LFSVC & lfsv_s_cache,
                                             const LFSVC & lfsv_n_cache,
                                             const LFSVC & lfsv_coupling_cache)
-      {DUNE_THROW(Dune::NotImplemented,"Assembling of coupling spaces is not implemented for ");}
+      {
+        DUNE_THROW(Dune::NotImplemented,"Assembling of coupling spaces is not implemented for ");
+      }
 
       template<typename EG, typename LFSUC, typename LFSVC>
       void assembleUVVolumePostSkeleton(const EG & eg, const LFSUC & lfsu_cache, const LFSVC & lfsv_cache)
