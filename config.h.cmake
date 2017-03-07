8f7d16c21d04a49108fad0ba353346e201d2e00b
Felix Schindler <felix.schindler@wwu.de>
2017-03-07 20:50:44 +0100

    avoid warnings in config.h

--- config.h.cmake
+++ config.h.cmake
@@ -41,15 +41,23 @@
 
 /* This is only true if PETSc was found by configure _and_ if the application
    uses the UG_CPPFLAGS */
+#ifndef HAVE_PETSC
 #cmakedefine HAVE_PETSC ENABLE_PETSC
+#endif
 
 /* This is only true if Eigen3 was found by configure */
+#ifndef HAVE_EIGEN
 #cmakedefine HAVE_EIGEN ENABLE_EIGEN
+#endif
 
 /* Define to 1 if GCC's __typeof__ extension is supported. */
+#ifndef HAVE_GCC___TYPEOF__
 #cmakedefine HAVE_GCC___TYPEOF__ 1
+#endif
 
 /* Define to 1 if sequential UG has been found */
+#ifndef PDELAB_SEQUENTIAL_UG
 #cmakedefine PDELAB_SEQUENTIAL_UG 1
+#endif
 
 /* end dune-pdelab */
