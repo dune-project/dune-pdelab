d67f1fc81ee744a652fc4cc0c3eae8f296e71daf
Steffen Müthing <muething@dune-project.org>
2017-08-23 18:12:54 +0200

    [fem] Make Qk finite element map query methods constexpr
    Also clean up comparisons by using new GeometryType variables and turn
    the runtime check for a valid combination of dimension and degree into
    a compile time check.

--- dune/pdelab/finiteelementmap/qkfem.hh
+++ dune/pdelab/finiteelementmap/qkfem.hh
@@ -21,48 +21,52 @@ namespace Dune {
 
     public:
 
+      static_assert(
+        (k == 1) or (GV::dimension == 2 or GV::dimension == 3),
+        "QkLocalFiniteElementMap with k = 2 is only implemented for d = 2,3"
+        );
+
+      static_assert(
+        k == 1 or k == 2,
+        "QkLocalFiniteElementMap is only implemented for k = 1,2"
+        );
+
       QkLocalFiniteElementMap(const GV& gv)
       {}
 
-      bool fixedSize() const
+      static constexpr bool fixedSize()
       {
         return true;
       }
 
-      bool hasDOFs(int codim) const
+      static constexpr bool hasDOFs(int codim)
       {
         switch(k)
           {
           case 1:
             return codim == GV::dimension;
           case 2:
-            if (GV::dimension != 2 && GV::dimension != 3)
-              DUNE_THROW(NotImplemented,"QkLocalFiniteElementMap with k = 2 is only implemented for d = 2,3");
-            return 1;
+            return true;
           default:
-            DUNE_THROW(NotImplemented,"QkLocalFiniteElementMap is only implemented for k <= 2");
+            return false;
           }
       }
 
-      std::size_t size(GeometryType gt) const
+      static constexpr std::size_t size(GeometryType gt)
       {
         switch (k)
           {
           case 1:
-            return gt.isVertex() ? 1 : 0;
+            return gt == GeometryTypes::vertex ? 1 : 0;
           case 2:
-            {
-              if (GV::dimension != 2 && GV::dimension != 3)
-                DUNE_THROW(NotImplemented,"QkLocalFiniteElementMap with k = 2 is only implemented for d = 2,3");
-              // Q1 simply attaches a single DOF to each subentity
-              return 1;
-            }
+            // Q1 simply attaches a single DOF to each subentity
+            return 1;
           default:
-            DUNE_THROW(NotImplemented,"QkLocalFiniteElementMap is only implemented for k <= 2");
+            return 0;
           }
       }
 
-      std::size_t maxLocalSize() const
+      static constexpr std::size_t maxLocalSize()
       {
         std::size_t r = 1;
         for (std::size_t i = 0; i < GV::dimension; ++i)
