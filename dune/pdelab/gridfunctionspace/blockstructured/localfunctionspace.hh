4d341b9d485edfa6b50a15549fdea3b3fd5f051c
Marcel Koch <marcel.koch@uni-muenster.de>
2018-05-29 15:23:27 +0200

    editor hints

--- dune/pdelab/gridfunctionspace/blockstructured/localfunctionspace.hh
+++ dune/pdelab/gridfunctionspace/blockstructured/localfunctionspace.hh
@@ -1,6 +1,5 @@
-//
-// Created by marckoch on 09/05/18.
-//
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
 
 #ifndef DUNE_PDELAB_GRIDFUNCTIONSPACE_BLOCKSTRUCTURED_LOCALFUNCTIONSPACE_HH
 #define DUNE_PDELAB_GRIDFUNCTIONSPACE_BLOCKSTRUCTURED_LOCALFUNCTIONSPACE_HH
