0a3ee8b8f8eeca6f742fe5e00ba6cc7f0cfd5536
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2019-02-04 00:17:56 +0100

    Add lazy message items for localTime and the logger backend

---
+++ dune/pdelab/logging/sinkmessageitems.hh
@@ -0,0 +1,51 @@
+#ifndef DUNE_PDELAB_LOGGING_SINKMESSAGEITEMS_HH
+#define DUNE_PDELAB_LOGGING_SINKMESSAGEITEMS_HH
+
+#include <cassert>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <dune/pdelab/logging/fmt.hh>
+#include <dune/pdelab/logging/logger.hh>
+
+namespace Dune::PDELab {
+
+  namespace SinkMessageItems {
+
+    inline auto localTime(const LogMessage& msg)
+    {
+      return LazyFormatArgument([&]()
+        {
+          return msg.localTime();
+        });
+    }
+
+    template<typename Buffer>
+    inline auto backend(const LogMessage& msg, Buffer& buffer, std::size_t width)
+    {
+      return LazyFormatArgument([&,width]() -> std::string_view
+        {
+          auto logger = msg.logger().name();
+          auto size = logger.size();
+          if (size < width)
+          {
+            buffer.clear();
+            buffer.append(logger.data(),logger.data() + size);
+            buffer.resize(width);
+            std::fill_n(buffer.data() + size,width - size,' ');
+            return {buffer.data(),buffer.size()};
+          }
+          else
+          {
+            return logger;
+          }
+        });
+    }
+
+  } // end namespace SinkMessageItems
+
+} // end namespace Dune::PDELab
+
+
+#endif // DUNE_PDELAB_LOGGING_SINKMESSAGEITEMS_HH
