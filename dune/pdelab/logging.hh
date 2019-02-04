688aed5f2f175170cd6007d244e71754dbb72eb3
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2019-02-04 09:37:55 +0100

    Add an overview documentation of the logging system

--- dune/pdelab/logging.hh
+++ dune/pdelab/logging.hh
@@ -16,17 +16,239 @@
 
 namespace Dune::PDELab {
 
-  /**
-   * \addtogroup logging Logging Infrastructure
-   * \brief Logging of status and error messages to different targets with ParameterTree-based configuration.
-   *
-   * Some more weird text.
-   * \{
-   */
+/**
+ * \addtogroup logging Logging Infrastructure
+ * \brief Logging of status and error messages to different targets with
+ * ParameterTree-based configuration.
+ *
+ * # Overview
+ *
+ * PDELab has a powerful, flexible logging system for reporting the current
+ * status of a running program. The system revolves around a small number of
+ * components:
+ *
+ * - The Logger is the most important class for normal users. Messages are
+ * logged with the help of a Logger. Loggers are lightweight objects with value
+ * semantics, so you can (and should) copy them around. Loggers can also be
+ * default-constructed into an invalid state and then assigned a valid logger at
+ * a later time.
+ *
+ * - The logger backend is a mostly invisible component to which a Logger
+ * forwards its log messages. Logger backends are long-lived singletons stored
+ * in the logging system that carry information like the list of attached sinks
+ * and defaults for new Loggers. Many Loggers can share a single logger backend,
+ * and every Logger is attached to exactly one backend.
+ *
+ * - The Sink is a class that is responsible for actually storing a log message
+ * somewhere. It gets passed preprocessed log messages in the form of a
+ * LogMessage object and has to write them to the terminal, a file, over the
+ * network or whatever you come up with. Like backends, Sinks are managed by the
+ * logging system, but you can obtain a shared_ptr to a registered sink and
+ * interact with it, e.g. to change its configuration. Unlike backends, Sinks
+ * can also be removed from the logging system by calling Logging::retireSink().
+ * Even after retiring a sink, backends can keep using it safely until they
+ * don't it any longer. There are two default Sinks connected to stdout and
+ * stderr, which you can obtain via Logging::cout() and Logging::cerr().
+ *
+ * - The configuration of the logging system is handled by calling static
+ * functions on the central class Logging. This class contains the central
+ * registries for sinks and logger backends. It must be initialized by calling
+ * Logging::init() before using any part of the logging system. During
+ *     initialization, it parses a ParameterTree and creates sinks and backends
+ * according to the given configuration. It is also possible to register new
+ * types of sinks with this configuration system. Moreover, Logging also lets
+ * you centrally disable all console output, which is very convenient for
+ *     parallel programs. The system mutes itself by default for all parallel
+ * ranks except 0.
+ *
+ * # Usage
+ *
+ * - Before you can use the logging system, you first need to initialize it:
+ * ~~~
+ * auto& parallel_helper = ...;
+ * Dune::ParameterTree params = ...;
+ * Dune::PDELab::Logging::init(parallel_helper.getCollectiveCommunication(),params.sub("logging"));
+ * ~~~
+ *
+ * - In your normal program (outside of the namespace Dune), you need to import
+ * the namespace Dune::Literals to make the user-defined literal `_fmt`
+ * available:
+ * ~~~
+ * using namespace Dune::Literals;
+ * ~~~
+ *     This is not necessary if your code is in namespace Dune or a nested
+ * namespace.
+ *
+ * - Now you can obtain the default Logger and log some messages:
+ * ~~~
+ * auto log = Dune::PDELab::logger();
+ * log.notice("Hello, {}! The answer is {}."_fmt,"world",42);
+ * log(Dune::PDELab::LogLevel::warning,"This seems complicated"_fmt);
+ * ~~~
+ *     \note The logging system uses the [{fmt}](https://fmtlib.net) library for
+ * formatting the log message. This system works like printf(): You first supply
+ * a format string, and placeholders in the format string are then replaced by
+ * textual representions of the remaining arguments. {fmt} has a powerful syntax
+ * for controlling the exact formatting of the arguments, see
+ *           http://fmtlib.net/latest/syntax.html for details.
+ *     \note The format string must be specially marked with the user-defined
+ * literal `_fmt`, otherwise your code will not compile.
+ *
+ * - If you write a component that can be configured with a ParameterTree, you
+ * can let the logging system configure a logger for you according to some
+ * configuration keys (see Logging::logger(const Dune::ParameterTree&)):
+ * ~~~
+ * class MyComponent
+ * {
+ *   Dune::PDELab::Logger _log;
+ *   ...
+ * public:
+ *   MyComponent(const Dune::ParameterTree& config)
+ *     : _log(Dune::PDELab::logger(config))
+ *   {}
+ * };
+ * ~~~
+ *
+ * # Configuration
+ *
+ * While it is possible to configure the logging system using function calls,
+ * the recommended configuration style is via the ParameterTree, most likely
+ * driven by an INI file.
+ *
+ * Logging::init() expects to be passed a ParameterTree subtree that only
+ * contains logging-related configuration. You can configure sinks, backends and
+ * global settings through this ParameterTree.
+ *
+ * * Sinks can be configured by creating a subtree with the prefix "sink." and
+ * putting the sink-specific configuration into that subtree, e.g.:
+ * ~~~{.ini}
+ * [logging.sink.logfile]
+ * type = file
+ * file = production-{}.log
+ * ~~~
+ *     This configuration will create a sink named "logfile" that writes to the
+ * file "production-X.log", where X will be replaced by the MPI rank of the
+ * process, zero-padded on the left as necessary.
+ *
+ * * Backends can be configured by placing information under the prefix
+ *     "backend.":
+ *     * If you want to create a backend with the same configuration as the default
+ *         backend, just a different name, create a key with value "default". The new
+ *         backend will be named after the key:
+ * ~~~{.ini}
+ * [logging.backend]
+ * newton = default
+ * timestep = default
+ * ~~~
+ *     * If you want to apply custom configuration to the new backend, create a subtree
+ *         named after the new backend and place the configuration keys for the backend
+ *         in the subtree:
+ * ~~~{.ini}
+ * [logging.backend.timestep]
+ * level = debug   # default level of loggers attached to this backend
+ * sinks = logfile # only write to the logfile, not to the console
+ * ~~~
+ *     It is possible to combine these two approaches.
+ *
+ * * You can also configure the components that are automatically created by the system.
+ *     In order to change the configuration of one of the default sinks stdout or stderr,
+ *     place the parameters in the corresponding entries unter "sink.":
+ * ~~~{.ini}
+ * [logging.sink.stdout]
+ * level = info
+ *
+ * [logging.sink.stderr]
+ * level = waring
+ * pattern = "STDERR [{backend}] {msg}"
+ * ~~~
+ *     The configuration for the default backend is in the subtree "default" at the root of
+ *     the ParameterTree, not in "backend.default":
+ * ~~~{.ini}
+ * [logging]
+ * default.level = info
+ * default.sinks = stderr, logfile
+ * ~~~
+ *
+ * ## Sink Configuration
+ *
+ * All sinks support the following configuration keys:
+ *
+ * | Key         | Description                                                                      |
+ * |-------------|----------------------------------------------------------------------------------|
+ * | type        | The type of the sink, selects the factory used to create the sink.               |
+ * | level       | The maximum LogLevel processed, messages with a higher level are ignored.        |
+ *
+ * When configuring a sink, you **must** specify the "type" of the sink. Currently, supported
+ * types include
+ *
+ * | Name        | Description                                                                      |
+ * |-------------|----------------------------------------------------------------------------------|
+ * | file        | A FileSink that logs to a different file per rank.                               |
+ *
+ * See the documentation of the created class for supported type-specific configuration keys.
+ *
+ * Most sinks inherit from PatternFormatSink and support its pattern-based formatting of the log
+ * output. For these sinks, you can set the configuration key "pattern" to a {fmt} format string
+ * that supports the following named arguments:
+ *
+ * | Parameter   |Description                                                      |Notes         |
+ * |-------------|-----------------------------------------------------------------|--------------|
+ * | msg         | The message submitted by the user                               |              |
+ * | level       | The log level of the message                                    |              |
+ * | paddedlevel | The log level of the message, right-padded to the longest level |              |
+ * | reltime     | The relative time since program start                           |              |
+ * | reldays     | The number of full days since program start                     |              |
+ * | abstime     | The absolute system time                                        | expensive    |
+ * | backend     | The name of the backend used to log the message                 | right-padded |
+ * | sink        | The name of the sink currently processing the message           |              |
+ *
+ * All of these parameters have the types returned by the corresponding member functions of
+ * LogMessage, exept for the log levels, which are converted to a string representation, and the
+ * logger names, which are right-padded to the width of the longest logger name.
+ *
+ * If your format string has to contain literal "{" or "}", escape them by doubling to "{{" or
+ * "}}".
+ *
+ * You can employ additional formatting with the standard {fmt} format specification language. This
+ * is especially important for "reltime" and "abstime".
+ *
+ * If the input format string lacks a trailing newline character, the sink will append one.
+ *
+ * For example, the pattern "[{reldays:0>2}-{reltime:12%T}] [{logger}] {msg}" causes messages to be
+ * logged like
+ * ~~~{.txt}
+ * [02-08:32:51.941] [default] This is the actual message
+ * ~~~
+ *
+ * ## Backend Configuration
+ *
+ * Backends support the following configuration keys;
+ *
+ * | Key         | Description                                                                      |
+ * |-------------|----------------------------------------------------------------------------------|
+ * | level       | The default maximum LogLevel of attached Loggers.                                |
+ * | indent      | The default indentation of attached Loggers.                                     |
+ * | enabled     | Flag for globally enabling or disabling all Loggers attached to this backend.    |
+ * | sinks       | Comma-separated list of sinks for this backend. Replaces the default sinks.      |
+ * | extra_sinks | Comma-separated list of sinks for this backend in addition to the default sinks. |
+ *
+ * ## Logger Configuration
+ *
+ * See Logging::logger(const ParameterTree&) for a list of configuration keys supported by Loggers.
+ *
+ * ## Global Configuration
+ *
+ * The logging system itself supports the following configuration keys:
+ *
+ * | Key         | Description                                                                      |
+ * |-------------|----------------------------------------------------------------------------------|
+ * | muted       | Overrides the default behavior of muting all MPI ranks with `comm.rank() > 0`    |
+ *
+ * \{
+ */
 
-  //! Central configuration of the logging system.
-  class Logging
-  {
+//! Central configuration of the logging system.
+class Logging {
 
 #ifndef DOXYGEN
 
