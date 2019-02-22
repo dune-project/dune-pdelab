6f3a7689c29b5ec0d5c23fcf4034d5fbf9febad1
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2019-02-22 11:43:42 +0100

    Switch LocalMatrix to row-major for consistency with FastDG

--- dune/pdelab/gridoperator/common/localmatrix.hh
+++ dune/pdelab/gridoperator/common/localmatrix.hh
@@ -454,7 +454,7 @@ namespace Dune {
            */
           value_type& getEntry(size_type i, size_type j)
           {
-            return _container[j*_rows + i];
+            return _container[i*_cols + j];
           }
 
           //! Direct (unmapped) access to the (i,j)-th entry of the matrix (const version).
@@ -468,7 +468,7 @@ namespace Dune {
            */
           const value_type& getEntry(size_type i, size_type j) const
           {
-            return _container[j*_rows + i];
+            return _container[i*_cols + j];
           }
 
         private:
