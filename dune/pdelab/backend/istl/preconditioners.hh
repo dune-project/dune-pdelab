15551c60568ab7ca712bb7b2d379419cf3ca0bcc
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2019-06-12 17:17:51 +0200

    Comment out unused typedefs

--- dune/pdelab/backend/istl/preconditioners.hh
+++ dune/pdelab/backend/istl/preconditioners.hh
@@ -264,10 +264,10 @@ namespace Dune::PDELab::ISTL::Experimental {
 
       using Matrix       = Native<typename Provider::Matrix>;
       using Vector       = Native<typename Provider::Domain>;
-      using Operator     = Dune::MatrixAdapter<Matrix,Vector,Vector>;
-      using Smoother     = Dune::SeqSSOR<Matrix,Vector,Vector,1>;
-      using SmootherArgs = typename Dune::Amg::SmootherTraits<Smoother>::Arguments;
-      using AMG          = Dune::Amg::AMG<Operator,Vector,Smoother>;
+      //using Operator     = Dune::MatrixAdapter<Matrix,Vector,Vector>;
+      //using Smoother     = Dune::SeqSSOR<Matrix,Vector,Vector,1>;
+      //using SmootherArgs = typename Dune::Amg::SmootherTraits<Smoother>::Arguments;
+      //using AMG          = Dune::Amg::AMG<Operator,Vector,Smoother>;
       using Parameters   = Dune::Amg::Parameters;
 
       static_assert(std::is_same_v<Vector,Native<typename Provider::Range>>,"range and domain must be the same");
