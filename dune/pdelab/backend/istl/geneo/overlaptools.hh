99afff13b00306be8df809d9ff0cf35f66213bc3
Peter Bastian <peter.bastian@iwr.uni-heidelberg.de>
2019-07-10 15:16:16 +0200

    make indices that are originally in the overlap also public

--- dune/pdelab/backend/istl/geneo/overlaptools.hh
+++ dune/pdelab/backend/istl/geneo/overlaptools.hh
@@ -562,9 +562,10 @@ namespace Dune {
               continue; // skip ghosts
             }
           EPISAttribute attr=partitiontype[i];
+          bool pub = (partitiontype[i]==EPISAttribute::overlap)||(dist[i]<=overlapsize);
           new2old_localindex.push_back(i);
           old2new_localindex[i] = new2old_localindex.size()-1;
-          pis->add(globalid[i],AttributedLocalIndex(new2old_localindex.size()-1,attr,dist[i]<=overlapsize));
+          pis->add(globalid[i],AttributedLocalIndex(new2old_localindex.size()-1,attr,pub));
         }
       pis->endResize();
       if (verbose)
