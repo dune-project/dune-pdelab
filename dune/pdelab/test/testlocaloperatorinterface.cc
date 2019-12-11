bcf499b4a65074f9d31de3c0656b462dc37cab22
René Heß <rene.hess@iwr.uni-heidelberg.de>
2019-12-11 15:13:37 +0100

    Improve localoperator test and prepare for testing fast DG operator

--- dune/pdelab/test/testlocaloperatorinterface.cc
+++ dune/pdelab/test/testlocaloperatorinterface.cc
@@ -37,6 +37,53 @@ public:
   static const bool isLinear = false;
 };
 
+
+template <typename GO, typename GFS>
+bool test_linear(const GO& go, const GFS& gfs)
+{
+    // Initialize vectors and matrices for gridoperator calls
+    typename GO::Traits::Domain u(gfs,0.0);
+    typename GO::Traits::Range r(gfs);
+    typename GO::Traits::Jacobian jac(go);
+
+    // Call gridoperator methods
+    go.residual(u,r);
+    go.jacobian(u,jac);
+    go.jacobian_apply(u,r);
+
+    // For linear problems these methods should throw errors
+    bool jacobian_apply_error = false;
+    bool nonlinear_jacobian_apply_error = false;
+    try{ go.jacobian_apply(u,u,r); } catch (...) { jacobian_apply_error = true; }
+    try{ go.nonlinear_jacobian_apply(u,u,r); } catch (...) { nonlinear_jacobian_apply_error = true; }
+
+    // If both are true we hit exceptions where expected -> true is a sucess
+    return (jacobian_apply_error and nonlinear_jacobian_apply_error);
+}
+
+template <typename GO, typename GFS>
+bool test_nonlinear(const GO& go, const GFS& gfs)
+{
+    // Initialize vectors and matrices for gridoperator calls
+    typename GO::Traits::Domain u(gfs,0.0);
+    typename GO::Traits::Range r(gfs);
+    typename GO::Traits::Jacobian jac(go);
+
+    // Call gridoperator methods
+    go.residual(u,r);
+    go.jacobian(u,jac);
+    go.jacobian_apply(u,u,r);
+    go.nonlinear_jacobian_apply(u,u,r);
+
+
+    // For non linear problems this methods should throw errors
+    bool jacobian_apply_error = false;
+    try{ go.jacobian_apply(u,r); } catch (...) { jacobian_apply_error = true; }
+
+    return jacobian_apply_error;
+}
+
+
 int main(int argc, char** argv)
 {
   try{
@@ -72,77 +119,56 @@ int main(int argc, char** argv)
     GFS gfs(gv,fem);
     gfs.name("x_h");
 
-    //=========================================
-    // Test interface for linear local operator
-    //=========================================
-
-    // Make local operator
-    using LOP = Dune::PDELab::LocalOperatorInterface;
-    LOP lop;
-
-    // Make grid operator
+    // Constraints
     using MBE = Dune::PDELab::ISTL::BCRSMatrixBackend<>;
     MBE mbe(9); // number of nonzeroes per row can be cross-checked by patternStatistics().
     using CC = typename GFS::template ConstraintsContainer<Real>::Type;
     CC cc;
+
+    // Make linear local operator
+    using LOP = Dune::PDELab::LocalOperatorInterface;
+    LOP lop;
+
+    // Make non linear local operator
+    using NLLOP = NonLinearLocalOperatorInterface;
+    NLLOP nllop;
+
+    //=========================================
+    // Test interface for linear local operator
+    //=========================================
     using GO = Dune::PDELab::GridOperator<GFS,GFS,LOP,MBE,Real,Real,Real,CC,CC>;
     GO go(gfs,cc,gfs,cc,lop,mbe);
-
-    // Initialize vectors and matrices for gridoperator calls
-    typedef typename GO::Traits::Domain U;
-    U u(gfs,0.0);
-    using R = typename GO::Traits::Range;
-    R r(gfs);
-    using J = typename GO::Traits::Jacobian;
-    J jac(go);
-
-    // Call gridoperator methods
-    go.residual(u,r);
-    go.jacobian(u,jac);
-    go.jacobian_apply(u,r);
-
-    // For linear problems these methods should throw errors
-    bool jacobian_apply_error = false;
-    bool nonlinear_jacobian_apply_error = false;
-    try{ go.jacobian_apply(u,u,r); } catch (...) { jacobian_apply_error = true; }
-    try{ go.nonlinear_jacobian_apply(u,u,r); } catch (...) { nonlinear_jacobian_apply_error = true; }
+    auto linear_test = test_linear(go, gfs);
 
     //==============================================
     // Test interface for non linear local operators
     //==============================================
-
-    // Make local operator
-    using NLLOP = NonLinearLocalOperatorInterface;
-    NLLOP nllop;
-
-    // Make grid operator
     using NLGO = Dune::PDELab::GridOperator<GFS,GFS,NLLOP,MBE,Real,Real,Real,CC,CC>;
     NLGO nlgo(gfs,cc,gfs,cc,nllop,mbe);
+    auto nonlinear_test = test_nonlinear(nlgo, gfs);
 
-    // Initialize vectors and matrices for gridoperator calls
-    typedef typename NLGO::Traits::Domain NLU;
-    NLU nlu(gfs,0.0);
-    using NLR = typename NLGO::Traits::Range;
-    NLR nlr(gfs);
-    using NLJ = typename NLGO::Traits::Jacobian;
-    NLJ nljac(nlgo);
+    // //===============================================
+    // // Test FastDGOperator with linear local operator
+    // //===============================================
+    // using FastGO = Dune::PDELab::FastDGGridOperator<GFS,GFS,LOP,MBE,Real,Real,Real,CC,CC>;
+    // FastGO fast_go(gfs,cc,gfs,cc,lop,mbe);
+    // auto fast_linear_test = test_linear(fast_go, gfs);
 
-    // Call gridoperator methods
-    nlgo.residual(nlu,nlr);
-    nlgo.jacobian(nlu,nljac);
-    nlgo.jacobian_apply(nlu,nlu,nlr);
-    nlgo.nonlinear_jacobian_apply(nlu,nlu,nlr);
+    // //===================================================
+    // // Test FastDGOperator with non linear local operator
+    // //===================================================
+    // using NLFastGO = Dune::PDELab::FastDGGridOperator<GFS,GFS,NLLOP,MBE,Real,Real,Real,CC,CC>;
+    // FastGO nl_fast_go(gfs,cc,gfs,cc,nllop,mbe);
+    // auto fast_nonlinear_test = test_nonlinear(nl_fast_go, gfs);
 
-    // For non linear problems this methods should throw errors
-    bool nl_jacobian_apply_error = false;
-    try{ nlgo.jacobian_apply(u,r); } catch (...) { nl_jacobian_apply_error = true; }
-
-    // If all methods that should throw an error really did so, the test
-    // succeeds (does not fail)
+    //==================
+    // Return testresult
+    //==================
     bool testfail = true;
-    if (jacobian_apply_error && nonlinear_jacobian_apply_error && nl_jacobian_apply_error)
+    // if (linear_test && nonlinear_test && fast_linear_test && fast_nonlinear_test)
+    //   testfail = false;
+    if (linear_test && nonlinear_test)
       testfail = false;
-
     return testfail;
   }
   catch (Dune::Exception &e){
