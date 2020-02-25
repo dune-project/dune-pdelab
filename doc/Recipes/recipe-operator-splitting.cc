8f050c07f5f67195459f0a9b111d87af3983eeb5
Carsten Gräser <graeser@mi.fu-berlin.de>
2020-02-25 08:36:41 +0100

    Replace TreePath<i...> by HybridTreePath<Dune::index_constant<i>...>

--- doc/Recipes/recipe-operator-splitting.cc
+++ doc/Recipes/recipe-operator-splitting.cc
@@ -928,7 +928,7 @@ void driver (const GV& gv, const FEM& fem, Param& param)
   osmd.setVerbosityLevel(2);
 
   // [Define objects for measuring splitting correction]
-  using GFSC_Sub0 = Dune::PDELab::GridFunctionSubSpace<GFSC,Dune::TypeTree::TreePath<0>>;
+  using GFSC_Sub0 = Dune::PDELab::GridFunctionSubSpace<GFSC,Dune::TypeTree::HybridTreePath<Dune::index_constant<0>>>;
   GFSC_Sub0 gfsc_sub0(gfsc);
   using SubC0 = Dune::PDELab::DiscreteGridFunction<GFSC_Sub0,ZC>;
   SubC0 subC0(gfsc_sub0,zcstep);
@@ -936,7 +936,7 @@ void driver (const GV& gv, const FEM& fem, Param& param)
   using ErrC0 = Dune::PDELab::DifferenceSquaredAdapter<SubC0,SubC0>;
   ErrC0 errC0(subC0,subC0new);
   //! [Define objects for measuring splitting correction]
-  using GFSC_Sub1 = Dune::PDELab::GridFunctionSubSpace<GFSC,Dune::TypeTree::TreePath<1>>;
+  using GFSC_Sub1 = Dune::PDELab::GridFunctionSubSpace<GFSC,Dune::TypeTree::HybridTreePath<Dune::index_constant<1>>>;
   GFSC_Sub1 gfsc_sub1(gfsc);
   using SubC1 = Dune::PDELab::DiscreteGridFunction<GFSC_Sub1,ZC>;
   SubC1 subC1(gfsc_sub1,zcstep);
