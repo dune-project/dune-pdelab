6de51ac7724acb37c915a07694b5dab36e24ca7d
rhess <rene.hess@iwr.uni-heidelberg.de>
2020-03-23 15:02:40 +0100

    Rename variblse in blocksorpreconditioner and improve documentation

--- dune/pdelab/backend/istl/matrixfree/blocksorpreconditioner.hh
+++ dune/pdelab/backend/istl/matrixfree/blocksorpreconditioner.hh
@@ -15,13 +15,16 @@ namespace Dune {
      * Given a linear system of equations Ax=b, a preconditioner step solves
      * Wv=d for a given vector d and an approximation $W \approx A$.
      *
-     * Using the block decomposition $A=D+L+U$ block-SOR uses
-     * $W=\omega^{-1}D+L$. And is implemented in the following way:
+     * Using the block decomposition $A=D+L+U$ block-SOR can be implemented in
+     * the following way (approximately solving Av=d).
      *
      * for element T_i, i=1,...,m do:
-     *   (1) a_i = d_i - \sum_{i<j} A_{ij} v_i^{(k)} - \sum_{i>j} A_{ij} v_i^{(k-1)}
+     *   (1) a_i = d_i - \sum_{j<i} A_{ij} v_j^{(k)} - \sum_{j>i} A_{ij} v_j^{(k-1)}
      *   (2) Solve D_i b_i = a_i
      *   (3) Update v_i^{(k)} = (1-\omega) v_i^{(k-1)} + \omega b_i
+     *
+     * Here d_i, a_i, ... denote a local vector from the global block vector
+     * and A_{ij} is the block (i,j) of the global block matrix A.
      *
      * See the artice "Matrix-free multigrid block-preconditioners for higher
      * order discontinuous Galerkin discretisations" by P. Bastian, E. Mueller,
@@ -53,12 +56,12 @@ namespace Dune {
       BlockSORPreconditionerLocalOperator(JacobianLOP& jacobianlop,
                                           BlockOffDiagonalLOP& blockOffDiagonalLOP,
                                           const GridFunctionSpace& gridFunctionSpace,
-                                          const double omega)
+                                          const double omega=1.0)
         : _jacobianlop(jacobianlop)
         , _blockOffDiagonalLOP(blockOffDiagonalLOP)
         , _omega(omega)
-        , _r_tmp(gridFunctionSpace.ordering().maxLocalSize())
-        , _dr_tmp(gridFunctionSpace.ordering().maxLocalSize())
+        , _a_i(gridFunctionSpace.ordering().maxLocalSize())
+        , _b_i(gridFunctionSpace.ordering().maxLocalSize())
       {}
 
       bool requireSetup()
@@ -92,12 +95,14 @@ namespace Dune {
       {}
 
       //! Linear operator application, volume terms
-      template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
+      template<typename EG, typename LFSU, typename X, typename LFSV, typename Y>
       void jacobian_apply_volume(const EG& eg,
                                  const LFSU& lfsu, const X& x,
-                                 const LFSV& lfsv, R& r) const
+                                 const LFSV& lfsv, Y& y) const
       {
-        // How does this preconditioner work:
+        //====================================
+        // How does this preconditioner work
+        //====================================
         //
         // When jacobian_apply is called on the grid operator the assembly
         // machine will iterate over the grid. Since we have set twosided to
@@ -107,46 +112,58 @@ namespace Dune {
         // jacobian_apply_skeleton for each intersection
         // jacobian_apply_volume_post_skeleton
         //
-        // In the volume part we set _r_tmp to zero. During the skeleton part
-        // we apply the off-diagonal blocks to the old and new solution (step
-        // (1) of the algorithm description at the top of this class). In the
+        // In the volume part we set _a_i to zero. During the skeleton part we
+        // apply the off-diagonal blocks to the old and new solution, step (1)
+        // of the algorithm description at the top of this class. In the
         // post_skeleton section we will do steps (2) and (3) of the algorithm.
-        _r_tmp = 0.0;
+        _a_i = 0.0;
       }
 
       //! linearized operator application, volume terms
       template<typename EG, typename LFSU, typename X, typename LFSV, typename Y>
       void jacobian_apply_volume(const EG& eg, const LFSU& lfsu, const X& x, const X& z, const LFSV& lfsv, Y& y) const
       {
-        _r_tmp = 0.0;
+        _a_i = 0.0;
       }
 
       //! Gather off-block-diagonals in Gauss-Seidel process of linear operator
-      template<typename IG, typename LFSU, typename X, typename LFSV, typename Y>
+      template<typename IG, typename LFSU, typename Z, typename LFSV, typename Y>
       void jacobian_apply_skeleton(const IG& ig,
-                                   const LFSU& lfsu_s, const X& x_s, const LFSV& lfsv_s,
-                                   const LFSU& lfsu_n, const X& x_n, const LFSV& lfsv_n,
+                                   const LFSU& lfsu_s, const Z& z_s, const LFSV& lfsv_s,
+                                   const LFSU& lfsu_n, const Z& z_n, const LFSV& lfsv_n,
                                    Y& y_s, Y& y_n) const
       {
-        // This step is bit tricky. r_s will always be the solution of the
-        // previous step. That is $v_i^{(k-1)} in the above algorithm. This
-        // will usually be zero since that is the value you get from ISTL.
+        // This step is bit tricky.
         //
-        // For cells you have not yet visited r_n will also be $v_i^{(k-1)}. If
-        // you have already visited the neigbor cell it will instead be the new
-        // solution $v_i^{(k)}$.
+        // In the apply method of the GridOperatorPreconditioner class
+        // jacobian_apply(d, v) is called on the preconditioner grid
+        // operator.
         //
-        // For this reason we pass the local vectors of r_s and r_n as
+        // z_s and z_n will be the local entries of the block vector d.
+        //
+        // y_s will always be the solution of the previous step, e.i
+        // $v_i^{(k-1)} in the above algorithm. This will be zero since that is
+        // the value you get from ISTL for v^{(0)} and we do only one
+        // preconditioner step.
+        //
+        // For cells you have not yet visited y_n will also be the old solution
+        // $v_j^{(k-1)}. If you have already visited the neigbor cell it will
+        // instead be the new solution $v_j^{(k)}$.
+        //
+        // For this reason we pass the local vectors of y_s and y_n as
         // coefficients to the block off-diagonal local operator. The result
-        // will be accumulated in _r_tmp. Following the above algorithm we will
+        // will be accumulated in _a_i. Following the above algorithm we will
         // have
         //
-        // _r_tmp = \sum_{j<i} A_ij v_i^{(k)} + \sum_{j>i} A_ij v_i^{(k-1)}
+        // _a_i = \sum_{j<i} A_ij v_j^{(k)} + \sum_{j>i} A_ij v_j^{(k-1)}
         //
-        // after visiting all the intersetions.
-        W _r_tmp_view(_r_tmp, y_s.weight());
+        // after visiting all the intersections.
+        W _a_i_view(_a_i, y_s.weight());
         if (ig.inside().partitionType() == Dune::InteriorEntity){
-          _blockOffDiagonalLOP.jacobian_apply_skeleton(ig, lfsu_s, y_s.container(), lfsv_s, lfsu_n, y_n.container(), lfsv_s, _r_tmp_view, _r_tmp_view);
+          // Note: Since the block off diagonal local operator works two sided
+          // we can pass _a_i_view two times to the jacobian_apply_skeleton but
+          // will only accumulate once.
+          _blockOffDiagonalLOP.jacobian_apply_skeleton(ig, lfsu_s, y_s.container(), lfsv_s, lfsu_n, y_n.container(), lfsv_s, _a_i_view, _a_i_view);
         }
       }
 
@@ -157,39 +174,40 @@ namespace Dune {
                                    const LFSU& lfsu_n, const X& x_n, const Z& z_n, const LFSV& lfsv_n,
                                    Y& y_s, Y& y_n) const
       {
-        W _r_tmp_view(_r_tmp, y_s.weight());
+        W _a_i_view(_a_i, y_s.weight());
         if (ig.inside().partitionType() == Dune::InteriorEntity)
-          _blockOffDiagonalLOP.jacobian_apply_skeleton(ig, lfsu_s, x_s, y_s.container(), lfsv_s, lfsu_n, x_n, y_n.container(), lfsv_s, _r_tmp_view, _r_tmp_view);
+          _blockOffDiagonalLOP.jacobian_apply_skeleton(ig, lfsu_s, x_s, y_s.container(), lfsv_s, lfsu_n, x_n, y_n.container(), lfsv_s, _a_i_view, _a_i_view);
       }
 
       //! Apply preconditioner after skeleton terms, linear version
-      template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
+      template<typename EG, typename LFSU, typename X, typename LFSV, typename Y>
       void jacobian_apply_volume_post_skeleton(const EG& eg,
                                                const LFSU& lfsu, const X& x,
-                                               const LFSV& lfsv, R& r) const
+                                               const LFSV& lfsv, Y& y) const
       {
         // Looking at the algorithm above we now need to finish step (1) by
 
-        // Subtract x: _r_tmp -= x_e. Looking at the algorithm above this
+        // Subtract x: _a_i -= x_e. Looking at the algorithm above this
         // finishes step (1) since the coefficient x is the vector d
         // above. After this we have r_tmp = - a_i
-        std::transform(_r_tmp.data(),
-                       _r_tmp.data() + _r_tmp.size(),
+        std::transform(_a_i.data(),
+                       _a_i.data() + _a_i.size(),
                        x.data(),
-                       _r_tmp.data(),
+                       _a_i.data(),
                        std::minus<value_type>{});
 
         // Solve the block diagonal system. This is step (2) of the
-        // algorithm. After this we have _dr_tmp = -b_i.
-        _dr_tmp = 0.0;
-        W _dr_tmp_view(_dr_tmp, r.weight());
-        _jacobianlop.jacobian_apply_volume(eg, lfsu, _r_tmp, lfsv, _dr_tmp_view);
+        // algorithm. After this we have _b_i = -b_i.
+        _b_i = 0.0;
+        W _b_i_view(_b_i, y.weight());
+        _jacobianlop.jacobian_apply_volume(eg, lfsu, _a_i, lfsv, _b_i_view);
+
 
         // Do the Update step (3) of the algorithm
-        std::transform(r.data(),
-                       r.data() + r.size(),
-                       _dr_tmp.data(),
-                       r.data(),
+        std::transform(y.data(),
+                       y.data() + y.size(),
+                       _b_i.data(),
+                       y.data(),
                        [=](const double &a,
                            const double& b)
                        {
@@ -203,27 +221,29 @@ namespace Dune {
       {
         // static checks
         //----------------------
-        // static_assert(std::is_same<decltype(x),decltype(_r_tmp)>::value,"Both types have to be the same for nonlinear Jacobian apply");
-        // static_assert(not std::is_same<decltype(x),decltype(_r_tmp)>::value,"Both types have to be the same for nonlinear Jacobian apply");
-
+        // static_assert(std::is_same<decltype(x),decltype(_a_i)>::value,"Both types have to be the same for nonlinear Jacobian apply");
+        // static_assert(not std::is_same<decltype(x),decltype(_a_i)>::value,"Both types have to be the same for nonlinear Jacobian apply");
+        //
         // dynamic crashes
         //----------------------
         // x.bar(); // type ConstAliasedVectorView
-        // _r_tmp.bar(); // type LocalVector<double, >
+        // _a_i.bar(); // type LocalVector<double, >
 
-        // Subtract x_e: _r_tmp -= x_e
-        std::transform(_r_tmp.data(),_r_tmp.data()+_r_tmp.size(),
+        // Subtract x_e: _a_i -= x_e
+        std::transform(_a_i.data(),_a_i.data()+_a_i.size(),
                        z.data(),
-                       _r_tmp.data(),
+                       _a_i.data(),
                        std::minus<value_type>{});
-        // Divide by block diagonal, _dr_tmp = D_{ee}^{-1} _r_tmp
-        _dr_tmp = 0.0;
-        W _dr_tmp_view(_dr_tmp,y.weight());
-        _jacobianlop.jacobian_apply_volume(eg,lfsu,x,_r_tmp,lfsv,_dr_tmp_view);
-        // Update vector r_e -= omega*_dr_tmp
+        // Divide by block diagonal, _b_i = D_{ee}^{-1} _a_i
+        _b_i = 0.0;
+        W _b_i_view(_b_i,y.weight());
+        _jacobianlop.jacobian_apply_volume(eg, lfsu, x, _a_i, lfsv, _b_i_view);
+        // Update vector r_e -= omega*_b_i
         // <=> r_e = (1-\omega)r_e + \omega D_{ee}^{-1}(x_e - \sum_{e'}A_{e,e'}r_{e'})
-        std::transform(y.data(),y.data()+y.size(),
-                       _dr_tmp.data(),y.data(),
+        std::transform(y.data(),
+                       y.data()+y.size(),
+                       _b_i.data(),
+                       y.data(),
                        [=](const double& a,
                            const double& b)
                        {
@@ -236,8 +256,8 @@ namespace Dune {
       JacobianLOP& _jacobianlop;
       BlockOffDiagonalLOP& _blockOffDiagonalLOP;
       const double _omega;
-      mutable LocalVector _r_tmp;
-      mutable LocalVector _dr_tmp;
+      mutable LocalVector _a_i;
+      mutable LocalVector _b_i;
     }; // end class BlockSORPreconditionerLocalOperator
 
   } // namespace PDELab
