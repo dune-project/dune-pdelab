6a8011fb0a7f3952bfa69af2c02672259dd843e2
Christian Engwer <christi@dune-project.org>
2020-06-02 22:52:18 +0200

    Revert hybrid tree path changes, use StaticTreePath

--- dune/pdelab/gridfunctionspace/subspacelocalfunctionspace.hh
+++ dune/pdelab/gridfunctionspace/subspacelocalfunctionspace.hh
@@ -22,7 +22,7 @@ namespace Dune {
       // ********************************************************************************
 
       template<typename It>
-      void extract_tree_path_elements(TypeTree::HybridTreePath<>, It it)
+      void extract_tree_path_elements(TypeTree::StaticTreePath<>, It it)
       {
         // end of recursion
       }
