4b05343613c0011c61a05f3077ffe770c430fa6e
Christian Engwer <christi@dune-project.org>
2020-06-08 00:07:33 +0200

    [FEM] add missing header for makeFunctionWithCallOperator

--- dune/pdelab/finiteelement/qkdglegendre.hh
+++ dune/pdelab/finiteelement/qkdglegendre.hh
@@ -17,6 +17,7 @@
 
 #include <dune/localfunctions/common/localbasis.hh>
 #include <dune/localfunctions/common/localfiniteelementtraits.hh>
+#include <dune/localfunctions/common/localinterpolation.hh>
 #include <dune/localfunctions/common/localkey.hh>
 #include <dune/localfunctions/common/localtoglobaladaptors.hh>
 
