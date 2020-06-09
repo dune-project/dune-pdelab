5c80b8e8985013bf26652e77937c5fc114f93059
rhess <rene.hess@iwr.uni-heidelberg.de>
2020-06-09 19:29:20 +0200

    Adjust test to backward compatible parameter names

--- dune/pdelab/test/testnewton.cc
+++ dune/pdelab/test/testnewton.cc
@@ -157,9 +157,9 @@ int main(int argc, char** argv)
 
     // Set some parameters without loading parameter tree from ini file (just to show that it works)
     Dune::ParameterTree ptree;
-    ptree["verbosity"] = "4";
-    ptree["line_search.line_search_damping_factor"] = "0.3";
-    ptree["use_max_norm"] = "1";
+    ptree["Verbosity"] = "4";
+    ptree["LineSearchDampingFactor"] = "0.3";
+    ptree["UseMaxNorm"] = "1";
     solver.setParameters(ptree);
 
     // Solve PDE
