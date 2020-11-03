67c183d212bf5270794f5d35c19265460f385823
Robert Kutri <r.kutri@gmail.com>
2020-11-03 13:44:23 +0100

    Add matrix comparison

--- dune/pdelab/test/geneo_unittests/utility/fuzzycompare.hh
+++ dune/pdelab/test/geneo_unittests/utility/fuzzycompare.hh
@@ -35,6 +35,42 @@ namespace Utility
     return (diff <= (std::max(std::abs(x1), std::abs(x2)) * rEps));
   }
 
+
+  // comparison of two istl block matrices of block size 1
+  template<class Matrix>
+  bool matrices_equal(const Matrix& m1, const Matrix& m2, double aEps=1e-12,
+                      double rEps=1e-8)
+  {
+    const std::size_t rows1{ m1.N() };
+    const std::size_t cols1{ m1.M() };
+
+    const std::size_t rows2{ m2.N() };
+    const std::size_t cols2{ m2.M() };
+
+    // check if matrices have the same size
+    if (rows1 != rows2 || cols1 != cols2)
+      return false;
+
+    bool same(false);
+
+    for (int i{ 0 }; i < rows1; ++i)
+      for (int j{ 0 }; j < cols1; ++j)
+        if (m1.exists(i, j))
+        {
+          // check if matrices have the same pattern
+          if (!m2.exists(i, j))
+            return false;
+
+          // check if matrices have the same entries
+          same = Utility::is_equal(m1[i][j], m2[i][j], aEps, rEps);
+
+          if (!same)
+            return false;
+        }
+
+    return true;
+  }
+
 } // namespace Utility
 
 #endif
