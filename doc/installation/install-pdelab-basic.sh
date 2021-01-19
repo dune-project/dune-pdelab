268342de7a2b237c7f9bed5f7bcb8148a543ae9e
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2021-01-19 18:03:37 +0100

    Add installation instruction script

---
+++ doc/installation/install-pdelab-basic.sh
@@ -0,0 +1,36 @@
+# Update package manager
+apt update
+
+# Install required dependencies
+apt install -y git cmake pkg-config g++ gfortran
+
+# Install some frequently used optional dependencies
+apt install -y libsuitesparse-dev openmpi-bin libopenmpi-dev libparmetis-dev libarpack++2-dev libsuperlu-dev
+
+
+# Create new directory to build in
+mkdir my-dune
+cd my-dune
+
+# Get DUNE modules via git
+git clone https://gitlab.dune-project.org/core/dune-common.git
+git clone https://gitlab.dune-project.org/core/dune-geometry.git
+git clone https://gitlab.dune-project.org/staging/dune-uggrid.git
+git clone https://gitlab.dune-project.org/core/dune-grid.git
+git clone https://gitlab.dune-project.org/core/dune-localfunctions.git
+git clone https://gitlab.dune-project.org/staging/dune-typetree.git
+git clone https://gitlab.dune-project.org/core/dune-istl.git
+git clone https://gitlab.dune-project.org/staging/dune-functions.git
+git clone https://gitlab.dune-project.org/pdelab/dune-pdelab.git
+
+# Choose a release version for all modules
+./dune-common/bin/dunecontrol git checkout releases/2.7
+
+# Build dune-pdelab and its dependencies
+./dune-common/bin/dunecontrol --builddir=$PWD/build --module=dune-pdelab all
+
+
+# Let's verify our build was successful by building and running a simple test
+cd build/dune-pdelab/dune/pdelab/test
+make testpoisson
+./testpoisson
