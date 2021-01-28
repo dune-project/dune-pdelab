846e100efee68691222ac819e6c25fda5ccd82f6
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2021-01-28 10:45:53 +0100

    Add sudo to apt in instal instructions

--- doc/installation/install-pdelab-basic.sh
+++ doc/installation/install-pdelab-basic.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Update package manager
-apt update
+sudo apt update
 
 # Install required dependencies
-apt install -y git cmake pkg-config g++ gfortran
+sudo apt install -y git cmake pkg-config g++ gfortran
 
 # Install some frequently used optional dependencies
-apt install -y libsuitesparse-dev openmpi-bin libopenmpi-dev libparmetis-dev libarpack++2-dev libsuperlu-dev
+sudo apt install -y libsuitesparse-dev openmpi-bin libopenmpi-dev libparmetis-dev libarpack++2-dev libsuperlu-dev
 
 
 # Create new directory to build in
