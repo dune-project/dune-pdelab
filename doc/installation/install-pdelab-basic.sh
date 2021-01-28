288ff48ca3e43e06701ed7fcf5adf2d133295517
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-01-28 11:59:41 +0100

    Noninteractive mode

--- doc/installation/install-pdelab-basic.sh
+++ doc/installation/install-pdelab-basic.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # Update package manager
-sudo apt update
+apt update
 
 # Install required dependencies
-sudo apt install -y git cmake pkg-config g++ gfortran
+apt install -y git cmake pkg-config g++ gfortran
 
 # Install some frequently used optional dependencies
-sudo apt install -y libsuitesparse-dev openmpi-bin libopenmpi-dev libparmetis-dev libarpack++2-dev libsuperlu-dev
-
+apt install -y libsuitesparse-dev openmpi-bin libopenmpi-dev libparmetis-dev libarpack++2-dev libsuperlu-dev
 
 # Create new directory to build in
 mkdir my-dune
@@ -29,10 +28,11 @@ git clone https://gitlab.dune-project.org/pdelab/dune-pdelab.git
 ./dune-common/bin/dunecontrol git checkout releases/2.7
 
 # Build dune-pdelab and its dependencies
-./dune-common/bin/dunecontrol --builddir=$PWD/build --module=dune-pdelab all
+echo "PWD: $(pwd)"
+./dune-common/bin/dunecontrol --builddir=$(pwd)/build --module=dune-pdelab all
 
 
 # Let's verify our build was successful by building and running a simple test
-cd build/dune-pdelab/dune/pdelab/test
+cd ./build/dune-pdelab/dune/pdelab/test
 make testpoisson
 ./testpoisson
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo $SUDO_USER
