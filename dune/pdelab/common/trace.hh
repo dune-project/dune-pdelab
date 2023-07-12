1209dc0e51a5f8d2792448f306f7b28bdc6b37ab
Santiago Ospina De Los Rios <sospinar@gmail.com>
2023-07-12 15:04:13 +0200

    Add perffeto helper factory

--- dune/pdelab/common/trace.hh
+++ dune/pdelab/common/trace.hh
@@ -6,6 +6,10 @@
 
 #if HAVE_PERFETTO
 #include <perfetto.h>
+#include <filesystem>
+#include <thread>
+#include <fstream>
+#include <iostream>
 
 #define PERFETTO_CATEGORIES
 
@@ -13,6 +17,64 @@ PERFETTO_DEFINE_CATEGORIES(
     perfetto::Category("dune")
         .SetDescription("Events from DUNE")
 );
+
+namespace Dune::PDELab::inline Experimental {
+
+struct [[nodiscard]] TracingSession {
+
+  TracingSession(std::filesystem::path filename)
+  : _filename{filename} {
+
+    perfetto::TracingInitArgs args;
+    args.backends |= perfetto::kInProcessBackend;
+    perfetto::Tracing::Initialize(args);
+    perfetto::TrackEvent::Register();
+
+    perfetto::TraceConfig cfg;
+    cfg.add_buffers()->set_size_kb(1024*100);
+    auto* ds_cfg = cfg.add_data_sources()->mutable_config();
+    ds_cfg->set_name("track_event");
+    _trace = perfetto::Tracing::NewTrace();
+    _trace->Setup(cfg);
+    _trace->StartBlocking();
+
+    // Give a custom name for the traced process.
+    perfetto::ProcessTrack process_track = perfetto::ProcessTrack::Current();
+    perfetto::protos::gen::TrackDescriptor desc = process_track.Serialize();
+    desc.mutable_process()->set_process_name("Main");
+    perfetto::TrackEvent::SetTrackDescriptor(process_track, desc);
+
+    TRACE_EVENT_BEGIN("dune", "Main");
+  }
+
+  ~TracingSession() {
+    TRACE_EVENT_END("dune");
+
+    // Make sure the last event is closed for this example.
+    perfetto::TrackEvent::Flush();
+
+    // Stop tracing and read the trace data.
+    _trace->StopBlocking();
+    std::vector<char> trace_data(_trace->ReadTraceBlocking());
+
+    // Write the result into a file.
+    // Note: To save memory with longer traces, you can tell Perfetto to write
+    // directly into a file by passing a file descriptor into Setup() above.
+    std::ostringstream ss; ss << "." << std::this_thread::get_id();
+    _filename.replace_extension("trace" + ss.str());
+    std::ofstream output;
+    output.open(_filename.c_str(), std::ios::out | std::ios::binary);
+    output.write(&trace_data[0], std::streamsize(trace_data.size()));
+    output.close();
+    std::cout << "Trace written in `"<< _filename.c_str() << "`." << std::endl;
+  }
+
+private:
+  std::unique_ptr<perfetto::TracingSession> _trace;
+  std::filesystem::path _filename;
+};
+
+} // namespace Dune::PDELab::inline Experimental
 
 #else
 
