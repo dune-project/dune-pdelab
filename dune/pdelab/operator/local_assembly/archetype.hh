2aaffd4f9ba1f08d896cf852cbd57d465fa9f65a
Santiago Ospina De Los Rios <sospinar@gmail.com>
2023-07-12 14:54:31 +0200

    Add missing arguments to jacobian skeleton

--- dune/pdelab/operator/local_assembly/archetype.hh
+++ dune/pdelab/operator/local_assembly/archetype.hh
@@ -72,16 +72,18 @@ struct Archetype {
     PDELab::Concept::LocalMutableContainer             auto& lresidual_out) noexcept;
 
   void localAssembleJacobianSkeleton(
-    const Dune::Concept::Intersection                  auto& intersection,
-                                                       auto  time_point,
-    const PDELab::Concept::LocalBasis                  auto& ltrial_in,
-    const PDELab::Concept::LocalConstContainer         auto& llin_point_in,
-    const PDELab::Concept::LocalBasis                  auto& ltest_in,
-    const PDELab::Concept::LocalBasis                  auto& ltrial_out,
-    const PDELab::Concept::LocalConstContainer         auto& llin_point_out,
-    const PDELab::Concept::LocalBasis                  auto& ltest_out,
-    PDELab::Concept::LocalMutableContainer             auto& ljacobian_in,
-    PDELab::Concept::LocalMutableContainer             auto& ljacobian_out);
+    const Dune::Concept::Intersection               auto& intersection,
+                                                    auto  time,
+    const PDELab::Concept::LocalBasis               auto& ltrial_in,
+    const PDELab::Concept::LocalConstContainer      auto& llin_point_in,
+    const PDELab::Concept::LocalBasis               auto& ltest_in,
+    const PDELab::Concept::LocalBasis               auto& ltrial_out,
+    const PDELab::Concept::LocalConstContainer      auto& llin_point_out,
+    const PDELab::Concept::LocalBasis               auto& ltest_out,
+    PDELab::Concept::LocalMutableMatrix             auto& ljacobian_in_in,
+    PDELab::Concept::LocalMutableMatrix             auto& ljacobian_in_out,
+    PDELab::Concept::LocalMutableMatrix             auto& ljacobian_out_in,
+    PDELab::Concept::LocalMutableMatrix             auto& ljacobian_out_out) noexcept;
 
   void localAssembleJacobianSkeletonApply(
     const Dune::Concept::Intersection                  auto& intersection,
